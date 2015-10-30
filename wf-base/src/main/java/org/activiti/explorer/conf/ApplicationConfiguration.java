c07b0526042350dbc976ea250936d55711745dec
Dmytro Tsapko <tsapko.dmitry@gmail.com>
2015-10-30 02:01:46 +0200

    [#915] Test fix.

--- wf-base/src/main/java/org/activiti/explorer/conf/ApplicationConfiguration.java
+++ wf-base/src/main/java/org/activiti/explorer/conf/ApplicationConfiguration.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.*;
 @ComponentScan(basePackages = { "org.activiti.explorer.conf",
         "org.egov.web.controller", "org.activiti.rest.controller",
         "org.activiti.rest.conf", "org.activiti.rest.service.api",
-        "org.wf.dp.dniprorada.engine.task", "org.wf.dp.dniprorada.util" })
+        "org.wf.dp.dniprorada.engine.task" })
 @ImportResource({ "classpath:context.xml" })
 /*@ImportResource({ "classpath:activiti-ui-context.xml",
         "classpath:activiti-login-context.xml",
