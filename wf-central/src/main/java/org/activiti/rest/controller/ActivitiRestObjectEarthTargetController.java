5165fb0fd94530ceea08a75b8e9442d3990ab2e9
grigoriy-romanenko <grigoriy-romanenko@rambler.ru>
2015-12-05 16:02:57 +0200

    #985 изменил вид сгенерированной документации

--- wf-central/src/main/java/org/activiti/rest/controller/ActivitiRestObjectEarthTargetController.java
+++ wf-central/src/main/java/org/activiti/rest/controller/ActivitiRestObjectEarthTargetController.java
@@ -25,15 +25,11 @@ public class ActivitiRestObjectEarthTargetController {
      * возвращает список целевых назначений земель, подпадающих под параметры
      <br>пример запроса: https://test.igov.org.ua/wf/service/services/getObjectEarthTargets?sID_UA=01.01
      <br>пример ответа:
-     <pre>
-     {@code
-         {
-             "sID_UA"   : "01.01",
-             "sName_UA" : "Для ведення товарного сільськогосподарського виробництва",
-             "nID"      : 1
-         }
+     {
+        "sID_UA"   : "01.01",
+        "sName_UA" : "Для ведення товарного сільськогосподарського виробництва",
+        "nID"      : 1
      }
-     </pre>
 
      <a href="http://www.neruhomist.biz.ua/classification.html">источник данных</a>
      *
