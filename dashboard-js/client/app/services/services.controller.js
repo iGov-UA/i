347812a997801b26c0f8d1884c9dbcf2bd6355a2
Maxim Shekhovets <maxymshg@gmail.com>
2015-12-06 17:38:54 +0200

    changes were made: - new function to load department list added into schedule - new dropdown for department added under bp dropdown

--- dashboard-js/client/app/services/services.controller.js
+++ dashboard-js/client/app/services/services.controller.js
@@ -4,8 +4,15 @@ angular.module('dashboardJsApp')
   .controller('ServicesCtrl', function ($scope, $modal, schedule, bpForSchedule) {
 
     $scope.bp = bpForSchedule.bp;
+    $scope.departments = [];
+
     $scope.bp.onChangeCallback = function () {
       $scope.$broadcast('bpChangedEvent');
+      console.log($scope.bp.chosenBp);
+      schedule.getFlowSlotDepartments($scope.bp.chosenBp.sID)
+        .then(function (data) {
+          $scope.departments = data;
+        });
     };
 
     $scope.workHours = {
