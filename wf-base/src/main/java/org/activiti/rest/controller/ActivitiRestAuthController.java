75495e636a6226de816ce8a92dc2adcb03f2ac39
Aleksandr Kuts <kr110666kai@gmail.com>
2015-12-09 14:29:20 +0200

    Add description to springfox
    Добавелены описания сервисов для контроллеров:
      wf-base/org.activiti.rest.controller/ActivitiRestAccessController.java
      wf-base/org.activiti.rest.controller/ActivitiRestAuthController.java

--- wf-base/src/main/java/org/activiti/rest/controller/ActivitiRestAuthController.java
+++ wf-base/src/main/java/org/activiti/rest/controller/ActivitiRestAuthController.java
@@ -11,12 +11,33 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 @Controller
+@Api(tags = { "wf-central", "Безопасность" }, description = "#")
 @RequestMapping(value = "/auth")
 public class ActivitiRestAuthController {
+    private final String noteController ="<strong>[wf-central] Аутентификация пользователя</strong><br /><br />";
+
+    private final String noteLogin = noteController
+    		+ "Логин пользователя в систему. Возращает признак успеха/неудачи входа.<br /><br />"
+            + "Response 200:<br />" 
+    		+ " {\"session\":\"true\"} - Пользователь авторизирован<br />"
+    		+ " {\"session\":\"false\"} - Имя пользователя или пароль не корректны<br /><br />"
+    		+ "Пример:<br />"
+            + "<a href=\"https://test.region.igov.org.ua/wf/auth/login?sLogin=kermit&sPassword=kermit\" target=\"_blank\">"
+            + "https://test.region.igov.org.ua/wf/auth/login?sLogin=kermit&sPassword=kermit</a>";
+
+    private final String noteLogout = noteController 
+    		+ "Логаут пользователя (наличие cookie JSESSIONID)<br /><br />"
+            + "Response 200:<br />" 
+            + "{\"session\":\"97AE7CA414A5DA85749FE379CC843796\"}";
+
     /**
      * Логин пользователя в систему. Возращает признак успеха/неудачи входа.
      * true - Пользователь авторизирован
@@ -28,11 +49,13 @@ public class ActivitiRestAuthController {
      * OR  {"session":"false"}- Имя пользователя или пароль не корректны
      * @throws ActivitiAuthException
      */
+    @ApiOperation(value = "Авторизация пользователя", notes = noteLogin)
     @RequestMapping(value = { "/login", "/login-v2" }, method = RequestMethod.POST)
     public
     @ResponseBody
-    LoginResponseI login(@RequestParam(value = "sLogin") String login,
-            @RequestParam(value = "sPassword") String password, HttpServletRequest request)
+    LoginResponseI login(
+    		@ApiParam(value = "Логин пользователя", required = true) @RequestParam(value = "sLogin") String login,
+    		@ApiParam(value = "Пароль пользователя", required = true) @RequestParam(value = "sPassword") String password, HttpServletRequest request)
             throws ActivitiAuthException {
         if (ProcessEngines.getDefaultProcessEngine().getIdentityService().checkPassword(login, password)) {
             request.getSession(true);
@@ -45,6 +68,7 @@ public class ActivitiRestAuthController {
     /**
      * Логаут пользователя (наличие cookie JSESSIONID):
      */
+    @ApiOperation(value = "Выход из системы", notes = noteLogout)
     @RequestMapping(value = "/logout", method = { RequestMethod.DELETE, RequestMethod.POST })
     public
     @ResponseBody
