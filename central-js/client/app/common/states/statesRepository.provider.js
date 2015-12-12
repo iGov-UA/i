b40ac3a3bea5e284b7fc5e5021ea1e6e489dfe1e
sybase <bw@pb.ua>
2015-12-12 23:22:36 +0200

    mvd profile prod+

--- central-js/client/app/common/states/statesRepository.provider.js
+++ central-js/client/app/common/states/statesRepository.provider.js
@@ -19,6 +19,11 @@ angular.module('appBoilerPlate').provider('statesRepository', function StatesRep
       "header" : "kharkiv.header.html",
       "footer" : "kharkiv.footer.html",
       "placesID" : ['6310100000','6300000000']
+    },
+    "mvd" : {
+      "header" : "mvd.header.html",
+      "footer" : "mvd.footer.html",
+      "placesID" : ['3200000000','8000000000']
     }
   };
 
@@ -28,7 +33,8 @@ angular.module('appBoilerPlate').provider('statesRepository', function StatesRep
     "kyiv" : modeModel.kyiv,
     "kiev" : modeModel.kyiv,
     "kharkiv" : modeModel.kharkiv,
-    "kharkov" : modeModel.kharkiv
+    "kharkov" : modeModel.kharkiv,
+    "mvd" : modeModel.mvd
   };
   this.init = function (domen) {
     //test.kiev.igov.org.ua
@@ -40,6 +46,10 @@ angular.module('appBoilerPlate').provider('statesRepository', function StatesRep
       if (domen.indexOf('kievcity')>=0) {
         //https://es.kievcity.gov.ua
         this.mode = 'kyiv';
+        //this.mode = modes.kyiv;
+      }else if (domen.indexOf('mvs')>=0) {
+        //https://es.kievcity.gov.ua
+        this.mode = 'mvd';
         //this.mode = modes.kyiv;
       }else{
         var matches = findModeRegexp.exec(domen);
