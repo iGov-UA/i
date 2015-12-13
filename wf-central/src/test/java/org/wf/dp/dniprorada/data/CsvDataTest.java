78235aac7f20cee9288137fc0a4145fa7c0c96e2
brunneng <good.guy.finish.first@gmail.com>
2015-12-13 16:11:34 +0200

    [issue #996]: CsvDataTest implemented.

---
+++ wf-central/src/test/java/org/wf/dp/dniprorada/data/CsvDataTest.java
@@ -0,0 +1,52 @@
+package org.wf.dp.dniprorada.data;
+
+import org.junit.*;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.io.IOException;
+
+/**
+ * User: goodg_000
+ * Date: 13.12.2015
+ * Time: 14:39
+ */
+public class CsvDataTest {
+
+    private static final String ACTIVE_SPRING_PROFILES = "spring.profiles.active";
+
+    private String savedSpringProfiles = null;
+
+    @Before
+    public void onBefore() {
+        savedSpringProfiles = System.getProperty(ACTIVE_SPRING_PROFILES);
+    }
+
+    @After
+    public void onAfter() {
+        if (savedSpringProfiles != null) {
+            System.setProperty(ACTIVE_SPRING_PROFILES, savedSpringProfiles);
+        }
+    }
+
+    @Test
+    public void testAlphaCsv() throws IOException {
+
+        System.setProperty(ACTIVE_SPRING_PROFILES, "alpha");
+        testApplicationContextStartedWithoutErrors();
+    }
+
+    @Test
+    public void testProdCsv() throws IOException {
+
+        System.setProperty(ACTIVE_SPRING_PROFILES, "prod");
+        testApplicationContextStartedWithoutErrors();
+    }
+
+    private void testApplicationContextStartedWithoutErrors() {
+        try (ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext(
+                "classpath:/csv-test-context.xml")) {
+            Assert.assertTrue(applicationContext.isActive());
+            Assert.assertTrue(applicationContext.isRunning());
+        }
+    }
+}
