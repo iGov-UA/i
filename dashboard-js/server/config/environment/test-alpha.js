8bc19eec55e54e65e0bba6a8cd52befc83fb0217
sybase <bw@pb.ua>
2015-12-13 14:22:24 +0200

    test-*.js fix

---
+++ dashboard-js/server/config/environment/test-alpha.js
@@ -0,0 +1,18 @@
+'use strict';
+
+// Test specific configuration
+// ===========================
+module.exports = {
+  /*activiti: {
+    prot: 'https',
+    host: 'test.region.igov.org.ua',
+    port: '',
+    rest: 'wf/service',
+    session: {
+      sessionIdle: 3000, //sec show warning
+      timeOut: 3000, //sec close session after warning
+      interval: 1000 //sec update session
+    }
+  },*/
+  bTest: true
+};
