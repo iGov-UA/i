66956b81d97ce2aa7d56381cbe30ec22fac38a73
LamronNu <olga.prylypko@gmail.com>
2015-12-15 13:00:46 +0200

    #981.

--- wf-base/src/main/java/org/activiti/rest/interceptor/RequestProcessingInterceptor.java
+++ wf-base/src/main/java/org/activiti/rest/interceptor/RequestProcessingInterceptor.java
@@ -25,6 +25,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+import org.wf.dp.dniprorada.base.model.EscalationHistory;
 import org.wf.dp.dniprorada.base.service.notification.NotificationService;
 import org.wf.dp.dniprorada.rest.HttpRequester;
 import org.wf.dp.dniprorada.util.GeneralConfig;
@@ -268,14 +269,12 @@ public class RequestProcessingInterceptor extends HandlerInterceptorAdapter {
             LOG.info("   >>> put nID_Proccess_Feedback=" + params.get("nID_Proccess_Feedback"));
         }
         try {
-            LOG.info("processName.indexOf(BpHandler.PROCESS_ESCALATION)=" + processName
-                    .indexOf(BpHandler.PROCESS_ESCALATION));
             if (processName.indexOf(BpHandler.PROCESS_ESCALATION) == 0) {//issue 981
-                LOG.info("begin update escalation history");
-                escalationHistoryService.updateStatus(Long.valueOf(sID_Process),
+                EscalationHistory escalationHistory = escalationHistoryService.updateStatus(Long.valueOf(sID_Process),
                         isProcessClosed ?
                                 EscalationHistoryService.STATUS_CLOSED :
                                 EscalationHistoryService.STATUS_IN_WORK);
+                LOG.info("update escalation history: " + escalationHistory);
             }
         } catch (Exception e) {
             LOG.error("ex!", e);
