446be7cdcf2eda1d1b81b233561373b19c284309
olya <olya@olya-comp>
2015-12-15 13:36:48 +0200

    игнор тестов

--- wf-base/src/main/java/org/activiti/rest/interceptor/RequestProcessingInterceptor.java
+++ wf-base/src/main/java/org/activiti/rest/interceptor/RequestProcessingInterceptor.java
@@ -235,8 +235,10 @@ public class RequestProcessingInterceptor extends HandlerInterceptorAdapter {
         historyEventService.addHistoryEvent(sID_Process, taskName, params);
 
         String taskCreatorEmail = JsonRequestDataResolver.getEmail(jsonObjectRequest);
+        LOG.info("sendTaskCreatedInfoEmail... taskCreatorEmail = " + taskCreatorEmail);
         if (taskCreatorEmail != null) {
             String processDefinitionId = (String)jsonObjectRequest.get("processDefinitionId");
+            LOG.info("processDefinitionId..." + processDefinitionId);
             if(processDefinitionId != null && processDefinitionId.indexOf("common_mreo_2") > 0){
                 LOG.info("skip send email for common_mreo_2 proccess");
                 return;
@@ -244,6 +246,7 @@ public class RequestProcessingInterceptor extends HandlerInterceptorAdapter {
             Long nID_Protected = AlgorithmLuna.getProtectedNumber(Long.parseLong(sID_Process));
             notificationService.sendTaskCreatedInfoEmail(taskCreatorEmail, nID_Protected);
         }
+        LOG.info("sendTaskCreatedInfoEmail ok!");
     }
 
     private void saveClosedTaskInfo(String sRequestBody) throws Exception {
