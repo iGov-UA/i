caddb023bdfe08c73b59213bfa11a6d75a5d11b2
olya <olya@olya-comp>
2015-12-15 16:29:04 +0200

    set charset

--- wf-base/src/main/java/org/wf/dp/dniprorada/util/unisender/UniSender.java
+++ wf-base/src/main/java/org/wf/dp/dniprorada/util/unisender/UniSender.java
@@ -250,7 +250,7 @@ public class UniSender {
 
         RestTemplate restTemplate = new RestTemplate(
                 Arrays.asList(stringConverter, resource, formHttpMessageConverter));
-        restTemplate.getMessageConverters().add(0, new StringHttpMessageConverter(Charset.forName("UTF-8"))); //._HeaderItem("charset", "utf-8")
+        //restTemplate.getMessageConverters().add(0, new StringHttpMessageConverter(Charset.forName("UTF-8"))); //._HeaderItem("charset", "utf-8")
         //let's construct main HTTP entity
         HttpHeaders httpHeaders = new HttpHeaders();
         httpHeaders.setContentType(MediaType.MULTIPART_FORM_DATA);
@@ -262,7 +262,8 @@ public class UniSender {
             for (int i = 0; iterator.hasNext(); i++) {
                 String fileName = iterator.next();
                 HttpHeaders partHeaders = new HttpHeaders();
-                partHeaders.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+                partHeaders.setContentType(new MediaType("application", "octet-stream", Charset.forName("UTF-8")));
+                //partHeaders.setContentType(MediaType.APPLICATION_OCTET_STREAM);
                 List<ByteArrayResource> bars = parametersFiles.get(fileName);
                 HttpEntity<ByteArrayResource> bytesPart = new HttpEntity<ByteArrayResource>(bars.get(i), partHeaders);
                 parametersMap.add(fileName, bytesPart);
