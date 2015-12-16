edac0d8f2c57398dba54249cfbccd357232123ee
bw <bw@bw-pc>
2015-12-16 20:23:50 +0200

    console.log('toEval=' + toEval);

--- central-js/client/app/common/services/field.motion.service.js
+++ central-js/client/app/common/services/field.motion.service.js
@@ -72,8 +72,8 @@ function FieldMotionService(MarkersFactory) {
     if (!_.contains(entry.aField_ID, fieldId)) return false;
     var toEval = entry.sCondition.replace(/\[(\w+)]/g, function(str, alias) {
       var fId = entry.asID_Field[alias];
-      console.log('fId =' + fId );
-      console.log('alias =' + alias );
+      console.log('fId=' + fId );
+      console.log('alias=' + alias );
       if (!fId) console.log('Cant resolve original fieldId by alias:' + alias);
       var result = '';
       console.log('formData[fId].value =' + formData[fId].value );
@@ -88,7 +88,10 @@ function FieldMotionService(MarkersFactory) {
       return result;
     });
     try {
-      return eval(toEval);
+      console.log('toEval=' + toEval);
+      var bResult = eval(toEval);
+      console.log('fId=' + fId + ', alias=' + alias + ', result=' + result + ', bResult=' + bResult);
+      return bResult;
     } catch (e) {
       console.log('OnCondition expression error\n' + e.name + '\n' + e.message
         + '\nexpression:' + entry.sCondition
