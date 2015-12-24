f54f3a71bb69ba5ad1abe29793f46e6c75c3c135
askosyr <askosyr@gmail.com>
2015-12-24 19:07:36 +0200

    https://github.com/e-government-ua/i/issues/1020 Added import of HttpServletResponse variable

--- wf-central/src/main/java/org/activiti/rest/controller/ActivitiRestSubjectMessageController.java
+++ wf-central/src/main/java/org/activiti/rest/controller/ActivitiRestSubjectMessageController.java
@@ -36,6 +36,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletResponse;
+
 @Controller
 @RequestMapping(value = "/messages")
 public class ActivitiRestSubjectMessageController {
