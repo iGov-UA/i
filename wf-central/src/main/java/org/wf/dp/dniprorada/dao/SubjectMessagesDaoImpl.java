442accbfde1460541b9744f02205779e67853796
sybase <bw@pb.ua>
2015-12-27 21:58:46 +0200

    https://github.com/e-government-ua/i/issues/1043

--- wf-central/src/main/java/org/wf/dp/dniprorada/dao/SubjectMessagesDaoImpl.java
+++ wf-central/src/main/java/org/wf/dp/dniprorada/dao/SubjectMessagesDaoImpl.java
@@ -1,10 +1,14 @@
 package org.wf.dp.dniprorada.dao;
 
+import java.util.LinkedList;
 import org.springframework.stereotype.Repository;
 import org.wf.dp.dniprorada.base.dao.GenericEntityDao;
 import org.wf.dp.dniprorada.model.SubjectMessage;
 
 import java.util.List;
+import org.hibernate.Criteria;
+import org.hibernate.criterion.Order;
+import org.hibernate.criterion.Restrictions;
 
 @Repository
 public class SubjectMessagesDaoImpl extends GenericEntityDao<SubjectMessage> implements SubjectMessagesDao {
@@ -21,6 +25,19 @@ public class SubjectMessagesDaoImpl extends GenericEntityDao<SubjectMessage> imp
     @Override
     public List<SubjectMessage> getMessages() {
         return findAll();
+    }
+
+    @Override
+    public List<SubjectMessage> getMessages(Long nID_HistoryEvent_Service) {
+        Criteria oCriteria = getSession().createCriteria(SubjectMessage.class);
+        oCriteria.add(Restrictions.eq("nID_HistoryEvent_Service", nID_HistoryEvent_Service));
+        //oCriteria.addOrder(Order.desc("id"));
+        oCriteria.setMaxResults(100);
+        List<SubjectMessage> aSubjectMessage = (List<SubjectMessage>) oCriteria.list();
+        if (aSubjectMessage == null) {
+            aSubjectMessage = new LinkedList();
+        }
+        return aSubjectMessage;
     }
 
     @Override
