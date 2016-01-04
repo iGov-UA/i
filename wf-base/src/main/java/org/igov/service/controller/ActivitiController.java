5830b776adf164e215651968984d47d137d65408
Oleksii Khalikov <oleksii.khalikov@gmail.com>
2016-01-05 01:36:19 +0200

    https://github.com/e-government-ua/i/issues/1011 add getTaskData into ActivitiController

--- wf-base/src/main/java/org/igov/service/controller/ActivitiController.java
+++ wf-base/src/main/java/org/igov/service/controller/ActivitiController.java
@@ -1185,7 +1185,7 @@ public class ActivitiController extends ExecutionBaseResource {
         oLog.info("id процесса nID = " + nID.toString());
 
         ProcessDTO oProcess = new ProcessDTO(sName, sBP, nID, sDateCreate);
-        return JsonRestUtils.toJsonResponse(oProcess);
+        return JsonRestUtils.toJsonResponse(sName);
     }
 
     private class ProcessDTO {
