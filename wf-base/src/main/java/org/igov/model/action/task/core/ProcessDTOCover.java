c8775fb84d40ecc278a8e4be8c7639b7109d1631
Oleksii Khalikov <oleksii.khalikov@gmail.com>
2016-01-12 01:55:26 +0200

    https://github.com/e-government-ua/i/issues/1011 cleaning temporary comments in ProcessDTOCover

--- wf-base/src/main/java/org/igov/model/action/task/core/ProcessDTOCover.java
+++ wf-base/src/main/java/org/igov/model/action/task/core/ProcessDTOCover.java
@@ -1,44 +1,23 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package org.igov.model.action.task.core;
 
 /**
+ * @author Khalikov
  *
- * @author Belyavtsev Vladimir Vladimirovich (BW)
+ *класс для сериализации JSON-объекта
  */
 public class ProcessDTOCover {
-    
-        private String sName;
-        private String sBP;
-        private Long nID;
-        private String sDateCreate;
 
-        public ProcessDTOCover(String sName, String sBP, Long nID, String sDateCreate) {
-            this.sName = sName;
-            this.sBP = sBP;
-            this.nID = nID;
-            this.sDateCreate = sDateCreate;
-        }
-/*
-        public String getName() {
-            return sName;
-        }
+    private String sName;
+    private String sBP;
+    private Long nID;
+    private String sDateCreate;
 
-        public String getBP() {
-            return sBP;
-        }
-
-        public Long getID() {
-            return nID;
-        }
-
-        public String getDateCreate() {
-            return sDateCreate;
-        }
-        */
+    public ProcessDTOCover(String sName, String sBP, Long nID, String sDateCreate) {
+        this.sName = sName;
+        this.sBP = sBP;
+        this.nID = nID;
+        this.sDateCreate = sDateCreate;
+    }
 
     public String getsName() {
         return sName;
