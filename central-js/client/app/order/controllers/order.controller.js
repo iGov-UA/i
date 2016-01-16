904a4ce2d5e0b309e0115577ace135c8f0a18099
glebzhebrakov <glebzhebrakov@gmail.com>
2016-01-16 15:16:58 +0200

    #1045 отображать список истории мессаджей заявки и давать возможность оставлять комментарии #1045

--- central-js/client/app/order/controllers/order.controller.js
+++ central-js/client/app/order/controllers/order.controller.js
@@ -2,15 +2,29 @@ angular.module('order', []).controller('OrderController', function($scope, $stat
     //if ($state.is('index.order')) {
     //    return $state.go('index.order.search');
     //}
+  $scope.loadMessages = function(order){
+    if (!!order){
+      MessagesService.getServiceMessages(order).then(function(data){
+        $scope.serviceMessages = data;
+        console.log(data);
+      }, function (error){
+        console.log(error);
+      });
+    }
+  } ;
+
+  $scope.postComment = function(){
+    $scope.comment = "";
+    $scope.loadMessages(order);
+  };
+
+
+
+  var order = $location.search().sID_Order;
 
   $scope.serviceMessages = [];
-  var order = $location.search().sID_Order;
-  if (!!order){
-    MessagesService.getServiceMessages(order).then(function(data){
-      $scope.serviceMessages = data;
-      console.log(data);
-    }, function (error){
-      console.log(error);
-    });
-  }
+
+  $scope.comment = "";
+
+  $scope.loadMessages(order);
 });
