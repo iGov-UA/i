18f26f5db8e59a9d2d951a040042f54b45883a6d
marbug <marbuga@gmail.com>
2016-01-17 12:35:29 +0200

    [1069] fix Vagrant install
    + add central-js/install/ubuntu.sh

---
+++ central-js/install/ubuntu.sh
@@ -0,0 +1,7 @@
+echo "Trying to become a root ..."
+sudo apt-get -y install gcc
+sudo apt-get -y install npm nodejs
+sudo apt-get -y install gem
+sudo npm install -g grunt-cli
+sudo npm install -g bower
+sudo npm install -g gem
