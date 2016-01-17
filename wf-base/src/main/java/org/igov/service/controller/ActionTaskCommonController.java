6a4669c9e27e158d8de79dce28a3c332ced7f09c
sybase <bw@pb.ua>
2016-01-17 18:33:49 +0200

    for(Map.Entry<String,Object> oHistoricProcess : oHistoricProcessInstance.getProcessVariables().entrySet()){                 mReturn.put(oHistoricProcess.getKey(), oHistoricProcess.getValue());             }

--- wf-base/src/main/java/org/igov/service/controller/ActionTaskCommonController.java
+++ wf-base/src/main/java/org/igov/service/controller/ActionTaskCommonController.java
@@ -55,6 +55,7 @@ import java.io.IOException;
 import java.nio.charset.Charset;
 import java.text.SimpleDateFormat;
 import java.util.*;
+import java.util.Map.Entry;
 import org.activiti.engine.form.FormData;
 import org.activiti.engine.history.HistoricProcessInstance;
 import org.igov.io.mail.NotificationPatterns;
@@ -423,14 +424,19 @@ public class ActionTaskCommonController {//extends ExecutionBaseResource
             if(oHistoricProcessInstance==null){
                 throw new RecordNotFoundException("oHistoricProcessInstance");
             }
-            FormData oFormData = formService.getStartFormData(oHistoricProcessInstance.getProcessDefinitionId());
+            
+            for(Map.Entry<String,Object> oHistoricProcess : oHistoricProcessInstance.getProcessVariables().entrySet()){
+                mReturn.put(oHistoricProcess.getKey(), oHistoricProcess.getValue());
+            }
+            
+            /*FormData oFormData = formService.getStartFormData(oHistoricProcessInstance.getProcessDefinitionId());
             if(oFormData==null){
                 throw new RecordNotFoundException("oFormData");
             }
             List<FormProperty> aFormProperty = oFormData.getFormProperties();
             for (FormProperty oFormProperty : aFormProperty) {
                 mReturn.put(oFormProperty.getId(), oFormProperty.getValue());
-            }
+            }*/
             //Task oTask = oActionTaskService.findBasicTask(nID_Task.toString());
         }
         return JSONValue.toJSONString(mReturn);
