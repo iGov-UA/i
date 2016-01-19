4f77275e03793fb1b90724ee26875045c90e0fe7
bw <bw@bw-pc>
2016-01-19 19:11:52 +0200

    oActionEventService.setHistoryEvent(HistoryEventType.GET_SERVICE, nID_Subject, mParamMessage);         oActionEventService.createHistoryEventForTaskQuestions(HistoryEventType.SET_TASK_QUESTIONS, soData, soData,
    add try catch

--- wf-central/src/main/java/org/igov/service/business/action/ActionEventService.java
+++ wf-central/src/main/java/org/igov/service/business/action/ActionEventService.java
@@ -263,7 +263,7 @@ public class ActionEventService {
                     eventType, mParamMessage);
             historyEventDao.setHistoryEvent(nID_Subject, eventType.getnID(),
                     eventMessage, eventMessage);
-        } catch (IOException e) {
+        } catch (Exception e) {
             LOG.error("error during creating HistoryEvent", e);
         }
     }
@@ -284,13 +284,18 @@ public class ActionEventService {
 
     public void createHistoryEventForTaskQuestions(HistoryEventType eventType, String soData, String data,
             String sID_Order, Long nID_Subject) {
-        Map<String, String> mParamMessage = new HashMap<>();
-        if (soData != null && !"[]".equals(soData)) {
-            mParamMessage.put(HistoryEventMessage.TASK_NUMBER, sID_Order);
-            mParamMessage.put(HistoryEventMessage.S_BODY, data == null ? "" : data);
-            mParamMessage.put(HistoryEventMessage.TABLE_BODY, createTable_TaskProperties(soData));
-            setHistoryEvent(eventType, nID_Subject, mParamMessage);
+        try {
+            Map<String, String> mParamMessage = new HashMap<>();
+            if (soData != null && !"[]".equals(soData)) {
+                mParamMessage.put(HistoryEventMessage.TASK_NUMBER, sID_Order);
+                mParamMessage.put(HistoryEventMessage.S_BODY, data == null ? "" : data);
+                mParamMessage.put(HistoryEventMessage.TABLE_BODY, createTable_TaskProperties(soData));
+                setHistoryEvent(eventType, nID_Subject, mParamMessage);
+            }
+        } catch (Exception e) {
+            LOG.error("FAIL:", e);
         }
+        
     }
 
 }
