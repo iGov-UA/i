c530241aba8cb1a8c825c2e3e7811c456dfdeeba
bw <bw@bw-pc>
2016-01-22 19:06:59 +0200

    - Полный рефакторинг фронта комплекса работы с сообщениями по заявкам FIX+

--- central-js/client/app/order/controllers/ordersearch.controller.js
+++ central-js/client/app/order/controllers/ordersearch.controller.js
@@ -15,13 +15,10 @@ angular.module('order').controller('OrderSearchController', function($rootScope,
     $scope.bOrderQuestion = false;
 
     var asErrorMessages = function(asMessageDefault, oData, onCheckMessage){
-        /*
-        var oData = {"s":"asasas"};
+        /*var oData = {"s":"asasas"};
         $.extend(oData,{sDTat:"dddddd"});
         var a=[];
-        a=a.concat(["1"]);
-        */
-        //{"code":"SYSTEM_ERR","message":null}
+        a=a.concat(["1"]);*/ //{"code":"SYSTEM_ERR","message":null}
         if(!asMessageDefault || asMessageDefault===null){
             asMessageDefault=[];
         }
@@ -51,87 +48,28 @@ angular.module('order').controller('OrderSearchController', function($rootScope,
         }
         if(asMessage.length>0){
             asMessage=asMessageDefault.concat(asMessage);
+            console.log('[asErrorMessages]:asMessage='+asMessage);
         }
-        console.log('[asErrorMessages]:asMessage='+asMessage);
+        return asMessage;
     }
-    
-    
-    $scope.loadMessages = function(sID_Order, sToken){
-        $scope.asServerReturnOnRequest = [];
-        $scope.aOrderMessage = [];
-        if($scope.bOrderOwner){
-            BankIDService.isLoggedIn().then(function() {
-                $scope.bAuthenticated = true;
-                if ($scope.bOrderOwner){
-                    if (!!sID_Order ){
-                      MessagesService.getServiceMessages(sID_Order, sToken).then(function(oData){
-                        $scope.asServerReturnOnRequest = asErrorMessages(["Помилка при отримані коментарів", 'Function: loadMessages'],oData);
-                        if($scope.asServerReturnOnRequest.length===0){
-                            if(oData.messages && oData.messages!==null){
-                                $scope.aOrderMessage = oData.messages;
-                            }else{
-                                $scope.asServerReturnOnRequest=['Невідома помилка при отримані коментарів!', 'Function: loadMessages','oData: '+oData,'sID_Order: '+sID_Order,'sToken: '+sToken];
-                            }
-                        }
-                      }, function (sError){
-                        $scope.asServerReturnOnRequest=['Невідома помилка при отримані коментарів!', 'Function: loadMessages','sError: '+sError,'sID_Order: '+sID_Order,'sToken: '+sToken];
-                      });
-                      if($scope.asServerReturnOnRequest.length>0){
-                          console.log("asServerReturnOnRequest="+$scope.asServerReturnOnRequest);
-                      }        
-                    }
-                }
-            }).catch(function(sError) {
-                $scope.bAuthenticated = false;
-                $scope.asServerReturnOnRequest=['Невідома помилка при отримані коментарів!', 'Function: loadMessages','sError: '+sError,'sID_Order: '+sID_Order,'sToken: '+sToken];
-                console.log("asServerReturnOnRequest="+$scope.asServerReturnOnRequest);
-            });            
-        }else{
-            console.log("[loadMessages](sID_Order="+sID_Order+",sToken="+sToken+"):bOrderOwner="+$scope.bOrderOwner);
-        }
-  } ;
-
-
-  $scope.postComment = function(){
-    if (!!$scope.sOrderCommentNew){
-        $scope.asServerReturnOnRequest = [];
-        var sID_Order = $scope.sID_Order;
-        var sToken = $scope.sToken;
-        if($scope.bOrderOwner){
-            //var oOrder = $scope.orders && $scope.orders!=null && $scope.orders.length > 0 ? $scope.orders[0] : null;
-            if(sID_Order && sID_Order!==null && sID_Order !== ''){
-              //MessagesService.postServiceMessage($scope.orders[0].sID_Order, $scope.comment);
-              try{
-                MessagesService.postServiceMessage(sID_Order, $scope.sOrderCommentNew, sToken);//$scope.orders[0].sID_Order
-                $scope.sOrderCommentNew = "";
-                $scope.loadMessages(sID_Order);//$scope.orders[0].sID_Order
-              }catch(sError){
-                $scope.asServerReturnOnRequest=['Невідома помилка при підправці коментаря!', 'Function: postComment','sError: '+sError,'sID_Order: '+sID_Order,'sToken: '+sToken,'sOrderCommentNew: '+$scope.sOrderCommentNew];
-              }
-            }else{
-              $scope.asServerReturnOnRequest=['Не задані параметри при підправці коментаря!', 'Function: postComment','sID_Order: '+sID_Order,'sToken: '+sToken,'sOrderCommentNew: ',$scope.sOrderCommentNew];
-            }
-            if($scope.asServerReturnOnRequest.length>0){
-                console.log("asServerReturnOnRequest="+$scope.asServerReturnOnRequest);
-            }
-        }else{
-            $scope.asServerReturnOnRequest=['Немає доступу!', 'Function: postComment', 'sID_Order: '+sID_Order,'sToken: '+sToken,'bOrderOwner: '+$scope.bOrderOwner];
-            console.log("asServerReturnOnRequest="+$scope.asServerReturnOnRequest);
-        }
+   
+    var bExist = function(oValue){
+        return oValue && oValue !== null && oValue !== undefined && !!oValue;
     }
-  };
-
-
-    if(order != null) {
+    var bExistNotSpace = function(oValue){
+        return bExist(oValue) && oValue.trim()!=="";
+    }
+   
+    if(order !== null) {
        $scope.searchOrder(
-               $stateParams.sID_Order !== null ? $stateParams.sID_Order : "0-" + $stateParams.nID
-               , $stateParams.sToken !== null ? $stateParams.sToken : $scope.sToken
+               bExist($stateParams.sID_Order) ? $stateParams.sID_Order : bExist($stateParams.nID) ? "0-" + $stateParams.nID : $scope.sID_Order
+               , bExist($stateParams.sToken) ? $stateParams.sToken : $scope.sToken
             );
     }
 
     $scope.searchOrder = function(sID_Order_New, sToken_New) {
-        var sID_Order = sID_Order_New !== null ? sID_Order_New : $scope.sID_Order;
-        var sToken = sToken_New !== null ? sToken_New : $scope.sToken;
+        var sID_Order = bExist(sID_Order_New) ? sID_Order_New : $scope.sID_Order;
+        var sToken = bExist(sToken_New) ? sToken_New : $scope.sToken;
         $scope.sID_Order = sID_Order;
         $scope.sToken = sToken;
         var aOrder = [];
@@ -141,74 +79,132 @@ angular.module('order').controller('OrderSearchController', function($rootScope,
         $scope.bOrderQuestion = false;
         $scope.sServerReturnOnAnswer = '';
         $scope.asServerReturnOnRequest = [];
-        ServiceService.searchOrder(sID_Order, sToken)
-            .then(function(oData) {
-                $scope.asServerReturnOnRequest = asErrorMessages(["Помилка при отримані заявки!", 'Function: searchOrder'],oData,function(sErrorMessage){
-                    if (sErrorMessage && sErrorMessage !== null && sErrorMessage.indexOf('CRC Error') > -1) {
-                        return ['Невірний номер!','sID_Order: '+sID_Order];
-                    } else if (sErrorMessage && sErrorMessage !== null && sErrorMessage.indexOf('Record not found') > -1) {
-                        return ['Заявку не знайдено!','sID_Order: '+sID_Order];
-                    } else {
-                        return ['Невідома помилка!','sErrorMessage: '+sErrorMessage,'sID_Order: '+sID_Order];
-                    }                    
-                });
-                if($scope.asServerReturnOnRequest.length===0){
-                    if (typeof oData === 'object') {
-                        if (oData.soData){
-                            try{
-                                oData.soData = JSON.parse(oData.soData.replace(/'/g,'"'));
-                            }catch(_){
-                              $scope.asServerReturnOnRequest=['Помилка парсингу об`єкту з полями, у яких заууваження при отримані заявки!', 'Function: searchOrder', 'sID_Order: '+sID_Order,'sToken: '+sToken,'oData.soData: '+oData.soData];
+        if(bExistNotSpace(sID_Order)){
+            ServiceService.searchOrder(sID_Order, sToken)
+                .then(function(oData) {
+                    $scope.asServerReturnOnRequest = asErrorMessages(["Помилка при отримані заявки!", 'Function: searchOrder'],oData,function(sErrorMessage){
+                        if (sErrorMessage && sErrorMessage !== null && sErrorMessage.indexOf('CRC Error') > -1) {
+                            return ['Невірний номер!','sID_Order: '+sID_Order];
+                        } else if (sErrorMessage && sErrorMessage !== null && sErrorMessage.indexOf('Record not found') > -1) {
+                            return ['Заявку не знайдено!','sID_Order: '+sID_Order];
+                        } else {
+                            return ['Невідома помилка!','sErrorMessage: '+sErrorMessage,'sID_Order: '+sID_Order];
+                        }                    
+                    });
+                    if($scope.asServerReturnOnRequest.length===0){
+                        if (typeof oData === 'object') {
+                            if (oData.soData){
+                                try{
+                                    oData.soData = JSON.parse(oData.soData.replace(/'/g,'"'));
+                                }catch(sError){
+                                  $scope.asServerReturnOnRequest=['Помилка десереалізації об`єкту з полями, у яких зауваження при отримані заявки!', 'Function: searchOrder','sError: '+sError, 'sID_Order: '+sID_Order,'sToken: '+sToken,'oData.soData: '+oData.soData];
+                                }
                             }
+                            aOrder = [oData];
+                        }else{
+                            $scope.asServerReturnOnRequest=['Помилка - повернено не об`єкт при отримані заявки!', 'Function: searchOrder', 'sID_Order: '+sID_Order,'sToken: '+sToken,'oData.soData: '+oData.soData];
                         }
-                        aOrder = [oData];
-                    }else{
-                        $scope.asServerReturnOnRequest=['Помилка - повернено не об`єкт при отримані заявки!', 'Function: searchOrder', 'sID_Order: '+sID_Order,'sToken: '+sToken,'oData.soData: '+oData.soData];
                     }
-                }
-                if($scope.asServerReturnOnRequest.length>0){
-                    console.log("asServerReturnOnRequest="+$scope.asServerReturnOnRequest);
-                }else{
-                    $scope.aOrder = aOrder;
-                    $scope.bOrder = $scope.aOrder && $scope.aOrder !== null && $scope.aOrder.length > 0;
-                    $scope.bOrderOwner = $scope.bOrder && $scope.aOrder[0].nID_Subject === $scope.aOrder[0].nID_Subject_Auth;
-                    $scope.bOrderQuestion = $scope.bOrder && $scope.aOrder[0].soData && $scope.aOrder[0].soData!==null && $scope.aOrder[0].soData.length > 0;
-                    $scope.loadMessages($scope.sID_Order, $scope.sToken);
-                }
-                return aOrder;
-            }, function (sError){
-              $scope.asServerReturnOnRequest=['Невідома помилка при отримані заявки!', 'Function: searchOrder','sError: '+sError, 'sID_Order: '+sID_Order,'sToken: '+sToken,'$scope.aOrder: '+$scope.aOrder];
-              console.log("asServerReturnOnRequest="+$scope.asServerReturnOnRequest);
-            });
+                    if($scope.asServerReturnOnRequest.length>0){
+                        console.log("asServerReturnOnRequest="+$scope.asServerReturnOnRequest);
+                    }else{
+                        $scope.aOrder = aOrder;
+                        $scope.bOrder = bExist($scope.aOrder) && $scope.aOrder.length > 0;
+                        $scope.bOrderOwner = $scope.bOrder && bExist($scope.aOrder[0].nID_Subject) && $scope.aOrder[0].nID_Subject === $scope.aOrder[0].nID_Subject_Auth;
+                        $scope.bOrderQuestion = $scope.bOrder && bExist($scope.aOrder[0].soData) && $scope.aOrder[0].soData.length > 0;
+                        $scope.loadMessages($scope.sID_Order, $scope.sToken);
+                    }
+                    return aOrder;
+                }, function (sError){
+                  $scope.asServerReturnOnRequest=['Невідома помилка при пошуку заявки!', 'Function: searchOrder','sError: '+sError, 'sID_Order: '+sID_Order,'sToken: '+sToken,'$scope.aOrder: '+$scope.aOrder];
+                  console.log("asServerReturnOnRequest="+$scope.asServerReturnOnRequest);
+                });            
+        }else{
+            console.log(['Не задані параметри при пошуку заявки!', 'Function: searchOrder','sID_Order: '+sID_Order,'sToken: '+sToken]);
+        }
     };
+    
+    $scope.loadMessages = function(sID_Order, sToken){
+        $scope.asServerReturnOnRequest = [];
+        $scope.aOrderMessage = [];
+        BankIDService.isLoggedIn().then(function() {
+            $scope.bAuthenticated = true;
+            if ($scope.bOrderOwner){
+                MessagesService.getServiceMessages(sID_Order, sToken).then(function(oData){
+                  $scope.asServerReturnOnRequest = asErrorMessages(["Помилка при отримані коментарів", 'Function: loadMessages'],oData);
+                  if($scope.asServerReturnOnRequest.length===0){
+                      if(bExist(oData.messages)){
+                          $scope.aOrderMessage = oData.messages;
+                      }else{
+                          $scope.asServerReturnOnRequest=['Невідома помилка при отримані коментарів!', 'Function: loadMessages','oData: '+oData,'sID_Order: '+sID_Order,'sToken: '+sToken];
+                      }
+                  }
+                }, function (sError){
+                  $scope.asServerReturnOnRequest=['Невідома помилка при отримані коментарів!', 'Function: loadMessages','sError: '+sError,'sID_Order: '+sID_Order,'sToken: '+sToken];
+                });
+            }else{
+                console.log(['Заборонено отримувати чужу історию або без авторизації!!', 'Function: loadMessages','sID_Order: '+sID_Order,'sToken: '+sToken,'bOrderOwner: '+$scope.bOrderOwner]);
+            }
+            if($scope.asServerReturnOnRequest.length>0){
+                console.log("asServerReturnOnRequest="+$scope.asServerReturnOnRequest);
+            }        
+        }).catch(function(sError) {
+            $scope.bAuthenticated = false;
+            $scope.asServerReturnOnRequest=['Невідома помилка при отримані коментарів!', 'Function: loadMessages','sError: '+sError,'sID_Order: '+sID_Order,'sToken: '+sToken];
+            console.log("asServerReturnOnRequest="+$scope.asServerReturnOnRequest);
+        });            
+  } ;
 
-
+  $scope.postComment = function(){
+    if (bExistNotSpace($scope.sOrderCommentNew)){
+        $scope.asServerReturnOnRequest = [];
+        var sID_Order = $scope.sID_Order;
+        var sToken = $scope.sToken;
+        if($scope.bOrderOwner){
+            if(bExistNotSpace(sID_Order)){
+              try{
+                MessagesService.postServiceMessage(sID_Order, $scope.sOrderCommentNew, sToken);//$scope.orders[0].sID_Order
+                $scope.sOrderCommentNew = "";
+                $scope.loadMessages(sID_Order, sToken);
+              }catch(sError){
+                $scope.asServerReturnOnRequest=['Невідома помилка при підправці коментаря!', 'Function: postComment','sError: '+sError,'sID_Order: '+sID_Order,'sToken: '+sToken,'sOrderCommentNew: '+$scope.sOrderCommentNew];
+              }
+            }else{
+              $scope.asServerReturnOnRequest=['Не задані параметри при підправці коментаря!', 'Function: postComment','sID_Order: '+sID_Order,'sToken: '+sToken,'sOrderCommentNew: ',$scope.sOrderCommentNew];
+            }
+        }else{
+            $scope.asServerReturnOnRequest=['Немає доступу!', 'Function: postComment', 'sID_Order: '+sID_Order,'sToken: '+sToken,'bOrderOwner: '+$scope.bOrderOwner];
+        }
+        if($scope.asServerReturnOnRequest.length>0){
+            console.log("asServerReturnOnRequest="+$scope.asServerReturnOnRequest);
+        }
+    }else{
+        console.log(['Пустій коментар!', 'Function: postComment', 'sID_Order: '+sID_Order,'sToken: '+sToken]);
+    }
+  };
 
   $scope.sendAnswer = function () {
-    //var sID_Order = $scope.orders[0].sID_Order;
-    //var sID_Order = $scope.sID_Order !== null ? $scope.sID_Order : $stateParams.sID_Order;
-    //var sToken = $scope.sToken !== null ? $scope.sToken : $stateParams.sToken;
     $scope.asServerReturnOnRequest = [];
     var sID_Order = $scope.sID_Order;
     var sToken = $scope.sToken;
-    var oOrder = $scope.aOrder && $scope.aOrder!=null && $scope.aOrder.length > 0 ? $scope.aOrder[0] : null;
+    var oOrder = bExist($scope.aOrder) && $scope.aOrder.length > 0 ? $scope.aOrder[0] : null;
     if($scope.bOrderOwner){
-        if(oOrder && oOrder!=null && sID_Order && sID_Order!==null && sID_Order !== ''){
+        if(bExistNotSpace(sID_Order) && bExist(oOrder)){
             try{
                 var oData = {
-        //              sToken: sToken,//$stateParams.sToken
                   sID_Order: sID_Order,
-                   //nID_Protected: $scope.orders[0].nID_Protected,
-                   //nID_Process: $scope.orders[0].nID_Process,
-                   //nID_Server: $scope.orders[0].nID_Server,
-                  //sHead: oOrder.sHead //$scope.orders[0]
-                  sBody: $scope.sOrderCommentNew //oOrder.sBody, //$scope.orders[0]
+                  sBody: $scope.sOrderCommentNew
                 };
                 if(sToken!==null){
                     oData = $.extend(oData,{sToken: sToken});
                 }
-
-                oData['saField'] = JSON.stringify(oOrder.soData);//$scope.orders[0]
+                if (oOrder.soData){
+                    try{
+                        oData.saField = JSON.stringify(oOrder.soData);
+                    }catch(sError){
+                        $scope.asServerReturnOnRequest=['Помилка сереалізації об`єкту з полями, у яких відповіді на зауваження при отримані заявки!', 'Function: sendAnswer','sError: '+sError, 'sID_Order: '+sID_Order,'sToken: '+sToken,'oData.soData: '+oData.soData];
+                    }
+                }
                 $http.post('/api/order/setTaskAnswer', oData).success(function() {
                   $scope.sServerReturnOnAnswer = 'Ваша відповідь успішно збережена';
                 });
@@ -218,11 +214,10 @@ angular.module('order').controller('OrderSearchController', function($rootScope,
         }else{
           $scope.asServerReturnOnRequest=['Не задані параметри для запиту при відсилці відповіді!', 'Function: sendAnswer', 'sID_Order: '+sID_Order,'sToken: '+sToken,'oOrder: '+oOrder];
         }
-        if($scope.asServerReturnOnRequest.length>0){
-            console.log("asServerReturnOnRequest="+$scope.asServerReturnOnRequest);
-        }
     }else{
         $scope.asServerReturnOnRequest=['Немає доступу!', 'Function: sendAnswer', 'sID_Order: '+sID_Order,'sToken: '+sToken,'bOrderOwner: '+$scope.bOrderOwner];
+    }
+    if($scope.asServerReturnOnRequest.length>0){
         console.log("asServerReturnOnRequest="+$scope.asServerReturnOnRequest);
     }
   };
