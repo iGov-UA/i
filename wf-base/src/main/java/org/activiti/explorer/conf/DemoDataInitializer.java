1684ec552937db5df499432e2d6900014c0ccd70
maxline <sergml@mail.ru>
2016-01-24 16:11:45 +0200

    Removal unused import http://sonar.igov.org.ua/issues/search#issues=beca65b6-efb3-4e2a-8d87-5d16dffbb5e1 http://sonar.igov.org.ua/issues/search#issues=a411d0a0-7dcd-4bd3-9b3f-de972af10e75 http://sonar.igov.org.ua/issues/search#issues=e0d62585-a022-4461-bc73-cdcee2cd6323 http://sonar.igov.org.ua/issues/search#issues=a1f572b4-aa28-46bf-bbe4-5c01998fc81d

--- wf-base/src/main/java/org/activiti/explorer/conf/DemoDataInitializer.java
+++ wf-base/src/main/java/org/activiti/explorer/conf/DemoDataInitializer.java
@@ -29,7 +29,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
