b7afe68e5ead9e1c4ba018803de6ffdbcc9b0c33
bw <bw@bw-pc>
2016-01-25 16:27:50 +0200

    ErrorsFactory.logWarn({sBody:'Ви використовуєте старий формат номеру заявки!<br>У майбутньому необхідно перед номером доповнити префікс "0-". (тобто "0-'+sID_Order+'", замість "'+sID_Order+'")'},{asParam:['sID_Order: '+sID_Order,'sToken: '+sToken]});

--- central-js/client/app/order/controllers/ordersearch.controller.js
+++ central-js/client/app/order/controllers/ordersearch.controller.js
@@ -68,7 +68,7 @@ angular.module('order').controller('OrderSearchController', function($rootScope,
         $scope.sServerReturnOnAnswer = '';
         if(bExistNotSpace(sID_Order)){
             if(sID_Order.indexOf("-")<0){
-                ErrorsFactory.logDebug({sBody:'Ви використовуєте старий формат номеру заявки!<br>У майбутньому необхідно перед номером доповнити префікс "0-". (тобто "0-'+sID_Order+'", замість "'+sID_Order+'")'},{asParam:['sID_Order: '+sID_Order,'sToken: '+sToken]});
+                ErrorsFactory.logWarn({sBody:'Ви використовуєте старий формат номеру заявки!<br>У майбутньому необхідно перед номером доповнити префікс "0-". (тобто "0-'+sID_Order+'", замість "'+sID_Order+'")'},{asParam:['sID_Order: '+sID_Order,'sToken: '+sToken]});
                 sID_Order="0-"-sID_Order;
                 $scope.sID_Order = sID_Order;
                 //return;
