b6488244110fd78bda607b116e44e69e5dd00a05
bw <bw@bw-pc>
2016-01-27 15:48:43 +0200

    fixes of log-dialog

--- central-js/client/app/common/components/errors/modal/errorsModal.controller.js
+++ central-js/client/app/common/components/errors/modal/errorsModal.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('app')
-  .controller('ErrorsModelController', function ($scope, $modalInstance, error) {
+  .controller('ErrorsModelController', function ($scope, $modalInstance, error, $filter) {
     $scope.error = error;
 
     $scope.close = function (el) {
