62d423302a0fd4e4247e8f900d41c85bd4bfa90f
bw <bw@bw-pc>
2016-01-27 17:15:44 +0200

    fixes data log

--- central-js/client/app/common/components/errors/modal/errorsModal.controller.js
+++ central-js/client/app/common/components/errors/modal/errorsModal.controller.js
@@ -1,8 +1,10 @@
 'use strict';
 
 angular.module('app')
-  .controller('ErrorsModelController', function ($scope, $modalInstance, error, $filter) {
+  .controller('ErrorsModelController', function ($scope, $modalInstance, error) {
     $scope.error = error;
+    //$scope.$filter = $filter;
+    //, $filter
 
     $scope.close = function (el) {
       $modalInstance.close(el);
