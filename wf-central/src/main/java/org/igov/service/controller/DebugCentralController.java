1488f643ff5cf698e59a296418112581796d17cf
sybase <bw@pb.ua>
2016-01-31 20:12:14 +0200

    add threads to logs and refactoring

--- wf-central/src/main/java/org/igov/service/controller/DebugCentralController.java
+++ wf-central/src/main/java/org/igov/service/controller/DebugCentralController.java
@@ -40,7 +40,7 @@ public class DebugCentralController {
     
     private static final Logger LOG_MIN = LoggerFactory.getLogger("Log_External");
     private static final Logger LOG_MID = LoggerFactory.getLogger("Log_External_Mid");
-    private static final Logger LOG_BIG = LoggerFactory.getLogger("Log_External_Big");
+    private static final Logger LOG_MAX = LoggerFactory.getLogger("Log_External_Big");
 
     
     
@@ -250,9 +250,9 @@ public class DebugCentralController {
             //LOG_MIN.info("sType={},nID_Subject={},sFunction={},sHead={},sBody={},sError={}",sType,nID_Subject,sFunction,sHead,sBody,sError);
             List<String> asParam = new LinkedList();
             Map<String,Object> moResponse = new HashMap();
-            String sMessage=null;
-            String sCode=null;
-            String soData=null;
+            String sResponseMessage=null;
+            String sResponseCode=null;
+            String soResponseData=null;
             String sDate=null;
             List<String> mParamResponse = new LinkedList();
             if(smData!=null){
@@ -272,13 +272,13 @@ public class DebugCentralController {
                         moResponse = (Map<String,Object>) moData.get("oResponse");
                         if (moResponse != null) {
                             if(moResponse.containsKey("sMessage")){
-                                sMessage=(String) moResponse.get("sMessage");
+                                sResponseMessage=(String) moResponse.get("sMessage");
                             }
                             if(moResponse.containsKey("sCode")){
-                                sCode=(String) moResponse.get("sCode");
+                                sResponseCode=(String) moResponse.get("sCode");
                             }
                             if(moResponse.containsKey("soData")){
-                                soData=(String) moResponse.get("soData");
+                                soResponseData=(String) moResponse.get("soData");
                             }
                         }
                     }
@@ -287,10 +287,10 @@ public class DebugCentralController {
                     }
                 }
             }
-            LOG_MIN.info("{}|{}|{}|{}|{}|{}|{}|{}|{}|",sType,nID_Subject,sFunction,sHead,sBody,sError,sMessage,sCode,soData);
-            LOG_MID.info("{}|{}|{}|{}|{}|{}|{}|{}|{}|{}|",sType,nID_Subject,sFunction,sHead,sBody,sError,sMessage,sCode,soData,asParam);
+            LOG_MIN.info("{}|{}|{}|{}|{}|{}|{}|{}|{}|",sType,nID_Subject,sFunction,sHead,sBody,sError,sResponseMessage,sResponseCode,soResponseData);
+            LOG_MID.info("{}|{}|{}|{}|{}|{}|{}|{}|{}|{}|",sType,nID_Subject,sFunction,sHead,sBody,sError,sResponseMessage,sResponseCode,soResponseData,asParam);
             //LOG_BIG.debug("sType={},nID_Subject={},nID_Server={},sFunction={},sHead={},sBody={},sError={},smData={}",sType,nID_Subject,nID_Server,sFunction,sHead,sBody,sError,smData);
-            LOG_BIG.debug("sType={},nID_Subject={},nID_Server={},sFunction={},sHead={},sBody={},sError={},smData={},sMessage={},sCode={},soData={},asParam={},sDate={}",sType,nID_Subject,nID_Server,sFunction,sHead,sBody,sError,sMessage,sCode,soData,asParam,sDate);
+            LOG_MAX.debug("sType={}|nID_Subject={}|nID_Server={}|sFunction={}|sHead={}|sBody={}|sError={}|smData={}|sResponseMessage={}|sResponseCode={}|soResponseData={}|asParam={}|sDate={}",sType,nID_Subject,nID_Server,sFunction,sHead,sBody,sError,sResponseMessage,sResponseCode,soResponseData,asParam,sDate);
             //subjectMessages = subjectMessagesDao.tranferDataFromMailToSubjectMail();
         } catch (Exception e) {
         	LOG.trace("FAIL:", e);
