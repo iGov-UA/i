ce6f33a3d5a78d9315aefe7b62ccde1f50b78013
Ольга <olga.kuzminova87@gmail.com>
2016-02-06 20:34:24 +0400

    set group

--- wf-base/src/main/java/org/igov/service/business/action/task/listener/AssignGroupListener.java
+++ wf-base/src/main/java/org/igov/service/business/action/task/listener/AssignGroupListener.java
@@ -6,10 +6,12 @@
 package org.igov.service.business.action.task.listener;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
 import org.activiti.engine.delegate.DelegateExecution;
 import org.activiti.engine.delegate.DelegateTask;
 import org.activiti.engine.delegate.TaskListener;
-import org.activiti.engine.impl.el.FixedValue;
+import org.activiti.engine.task.IdentityLink;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -17,18 +19,23 @@ import org.slf4j.LoggerFactory;
  *
  * @author Ольга
  */
-public class AssignGroupListener implements TaskListener{
-    
+public class AssignGroupListener implements TaskListener {
+
     private static final transient Logger LOG = LoggerFactory.getLogger(AssignGroupListener.class);
 
     @Override
     public void notify(DelegateTask task) {
         DelegateExecution execution = task.getExecution();
-        String organ = (String)execution.getVariable("act_id_group.id_");
+        String organ = (String) execution.getVariable("act_id_group.id_");
         LOG.info("organ: " + organ);
-        if(organ != null && !"".equals(organ)){
-            task.addCandidateGroups(Arrays.asList(organ.split(";")));
+        if (organ != null && !"".equals(organ)) {
+            List<String> groups = Arrays.asList(organ.split(","));
+            Set<IdentityLink> groupOld = task.getCandidates();
+            for (IdentityLink group : groupOld) {
+                groups.add(group.getGroupId());
+            }
+            task.addCandidateGroups(groups);
         }
     }
-    
+
 }
