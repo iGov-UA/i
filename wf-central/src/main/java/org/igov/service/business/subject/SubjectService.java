097a2ccd0a9a0ff10311098e9b140bb1abc0a456
lesha1980 <lesha.ievenko@yandex.ua>
2016-02-21 18:16:43 +0200

    #1116 - test сервис syncContacts

--- wf-central/src/main/java/org/igov/service/business/subject/SubjectService.java
+++ wf-central/src/main/java/org/igov/service/business/subject/SubjectService.java
@@ -18,11 +18,13 @@ import org.igov.model.subject.organ.SubjectOrganDao;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 /**
  *
  * @author Belyavtsev Vladimir Vladimirovich (BW)
  */
+@Service
 public class SubjectService {
     
      private static final Logger LOG = LoggerFactory.getLogger(SubjectService.class);
