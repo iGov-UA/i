9d2bf72196f5f180fea03afa345f86b82780872a
jamy23 <jamy023@gmail.com>
2016-03-11 18:53:59 +0200

    Fix Gruntfile.js to open right address when server started

--- central-js/Gruntfile.js
+++ central-js/Gruntfile.js
@@ -51,7 +51,7 @@ module.exports = function (grunt) {
     },
     open: {
       server: {
-        url: 'http://localhost:<%= express.options.port %>'
+        url: localConfig.SERVER_PROTOCOL + '://localhost:<%= express.options.port %>'
       }
     },
     watch: {
