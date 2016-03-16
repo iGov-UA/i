b78ee13a00c09be239c7170e1346fd929a4f8030
olya <olga.kuzminova87@gmail.com>
2016-03-16 16:34:54 +0200

    fix

--- wf-base/src/main/java/org/igov/service/controller/interceptor/RequestProcessingInterceptor.java
+++ wf-base/src/main/java/org/igov/service/controller/interceptor/RequestProcessingInterceptor.java
@@ -252,7 +252,7 @@ public class RequestProcessingInterceptor extends HandlerInterceptorAdapter {
         JSONObject omResponseBody = (JSONObject) oJSONParser.parse(sResponseBody);
         mParam.put("nID_StatusType", HistoryEvent_Service_StatusType.CREATED.getnID().toString());
 
-        String snID_Process = (String) omResponseBody.get("id");
+        String snID_Process = String.valueOf(omResponseBody.get("id"));
         Long nID_Process = Long.valueOf(snID_Process);
         String sID_Order = generalConfig.sID_Order_ByProcess(nID_Process);
         String snID_Subject = String.valueOf(omRequestBody.get("nID_Subject"));
@@ -294,21 +294,10 @@ public class RequestProcessingInterceptor extends HandlerInterceptorAdapter {
         String sUserTaskName = bProcessClosed ? "" : aTask.get(0).getName();//"(нет назви)"
 
         String sMailTo = JsonRequestDataResolver.getEmail(omRequestBody);
-        String sPhone = JsonRequestDataResolver.getPhone(omRequestBody);
-        //LOG.info("Check if ned sendTaskCreatedInfoEmail... (sMailTo={})", sMailTo);
+        String sPhone = String.valueOf(JsonRequestDataResolver.getPhone(omRequestBody));
         if (sMailTo != null) {
             LOG.info("Send notification mail... (sMailTo={})", sMailTo);
-            /*
-            String processDefinitionId = (String)jsonObjectRequest.get("processDefinitionId");
-            if(processDefinitionId != null && processDefinitionId.indexOf("common_mreo_2") > -1){
-                LOG.info("skip send email for common_mreo_2 proccess");
-                return;
-            }
-            */
-            
-         
             oNotificationPatterns.sendTaskCreatedInfoEmail(sMailTo, sID_Order);
-            //LOG.info("Sent Email ok!");
         }
         
         if(sMailTo != null || sPhone != null)
