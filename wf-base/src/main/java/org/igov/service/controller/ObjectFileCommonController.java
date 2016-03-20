74003a5df0b3fb1b7912a1c4b7ea893228fafcf2
askosyr <askosyr@gmail.com>
2016-03-21 01:22:26 +0200

    https://github.com/e-government-ua/i/issues/1190

--- wf-base/src/main/java/org/igov/service/controller/ObjectFileCommonController.java
+++ wf-base/src/main/java/org/igov/service/controller/ObjectFileCommonController.java
@@ -299,7 +299,7 @@ public class ObjectFileCommonController {
     }
 
     @ApiOperation(value = "Загрузки прикрепленного к заявке файла из постоянной базы", notes = "##### Пример:\n "
-            + "https://test.igov.org.ua/wf/service/object/file/getAttachmentByID?nId=111111&sFileName=111.txt&sType=text&description=aaaa\n")
+            + "https://test.igov.org.ua/wf/service/object/file/download_file_from_storage_static?nId=111111&sFileName=111.txt&sType=text&description=aaaa\n")
     @RequestMapping(value = "/getAttachmentByID", method = RequestMethod.GET)
     @Transactional
     public
