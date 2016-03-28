0a6cb0b72018d8ee557300e92df56d42cfa77526
jamy23 <jamy023@gmail.com>
2016-03-28 16:23:19 +0300

    Node.js part of task - optimize time of /api/object-customs

--- central-js/server/api/object-customs/index.controller.js
+++ central-js/server/api/object-customs/index.controller.js
@@ -2,7 +2,8 @@
 var zlib = require('zlib');
 var activiti = require('../../components/activiti');
 var cache = require('../../components/cache');
-
+var JSONStream = require('JSONStream');
+var etag = require('etag');
 var sURL = '/object/getObjectCustoms';
 
 function noop () {
@@ -10,27 +11,67 @@ function noop () {
 }
 
 function prepareResponse (res) {
-  return (/gzip|deflate/.test(res.headers['content-encoding']) ? res.pipe(zlib.createUnzip()) : res);
+  return (/gzip|deflate/.test(res.headers['content-encoding']) ?
+    res.pipe(zlib.createUnzip()) :
+    res).pipe(JSONStream.parse('*'));
 }
 
+function escapeRegExp (text) {
+  return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
+};
+
 module.exports.getObjectCustomsList = function (req, res) {
-  cache(
-    function id () {
-      return sURL + '?sName_UA=' + req.query.sName_UA;
-    },
-    function hit (response) {
-      return response;
-    },
-    function fault (output) {
+  var resourceId = req.query.sName_UA ? req.query.sName_UA.split('') : [];
+  var outputStream = JSONStream.stringify('[', ',', ']');
+  var cachedResource = cache('nID', function () {
+    return sURL + '?sName_UA=' + resourceId.join('');
+  });
+  res.setHeader('Content-Type', 'application/json;charset=utf-8');
+  var skip = Math.abs(parseInt(req.query.skip, 10)) || 0;
+  var stop = Math.abs(parseInt(req.query.count, 10)) + skip || 5 + skip;
+  outputStream.pipe(res);
+
+  cachedResource.on('hit', function (response, ids) {
+    var resTag = etag(JSON.stringify(ids));
+    var reqTag = req.headers['if-none-match'];
+    if (resTag === reqTag) {
+      res.status(304);
+      res.end();
+      return;
+    }
+    res.setHeader('ETag', resTag);
+    var reducePattern = req.query.sName_UA.split('').length > resourceId.length ?
+      new RegExp(escapeRegExp(req.query.sName_UA)) : false;
+    var items = response.slice(skip, stop);
+    items.forEach(function (item) {
+      if (!reducePattern || reducePattern.test(item.sName_UA)) {
+        outputStream.write(item);
+      }
+    });
+    outputStream.end();
+  });
+  cachedResource.on('fault', function () {
+    if (resourceId.pop()) {
+      cachedResource.run();
+    } else {
       var request = activiti.sendGetRequest(req, res, sURL, req.query, noop);
       req.pipe(request);
       request.on('response', function (response) {
-        prepareResponse(response).pipe(output);
+        resourceId = req.query.sName_UA ? req.query.sName_UA.split('') : [];
+        var cursor = 0;
+        prepareResponse(response).pipe(cachedResource).on('data', function (data) {
+          if (stop == cursor) {
+            outputStream.end()
+          } else if (skip <= cursor) {
+            outputStream.write(data);
+          }
+          cursor++;
+        });
       });
-    },
-    function final (response) {
-      res.writeHead(200, response.headers);
-      return response.pipe(res);
     }
-  );
+  });
+  cachedResource.on('error', function (err) {
+    res.send(err.stack);
+  });
+  cachedResource.run();
 };
--- central-js/server/components/cache/index.js
+++ central-js/server/components/cache/index.js
@@ -1,70 +1,57 @@
-var Duplex = require('stream').Duplex;
+'use strict';
+var stream = require('stream');
 var NodeCache = require("node-cache");
-var JSONStream = require('JSONStream');
-var zlib = require('zlib');
 var util = require('util');
 
-util.inherits(CachedStream, Duplex);
+util.inherits(CachedStream, stream.PassThrough);
 
 var indexCache = new NodeCache();
 var objectCache = new NodeCache();
 
-function CachedStream (getID, onHit, onFault, onFinally) {
+function fetchObjects (key) {
+  return this[key];
+}
+
+function CachedStream (trackBy, getResourceId) {
   if (!(this instanceof CachedStream)) {
-    return new CachedStream(getID, onHit, onFault, onFinally);
+    return new CachedStream('' + trackBy, (getResourceId instanceof Function) ?
+      getResourceId : function () {return getResourceId;});
   }
 
-  Duplex.call(this);
+  stream.PassThrough.call(this);
+  this._readableState.objectMode = true;
+  this._writableState.objectMode = true;
 
   var self = this;
 
-  this.objectsStream = JSONStream.parse('*', this._get.bind(this));
+  var newIndex = [];
 
-  try{
-    //console.log(k);
-    var cachedIDs = this.indexCache.get(getID());
-    if (cachedIDs) {
-      onHit(this);
-      objectCache.mget(cachedIDs, function (err, objects) {
-        console.log('%', objects);
-        this.push(objects);
-      });
-    } else {
-      onFault(this);
-      this.objectsStream.on('data', function (item) {
-        objectCache.set(item.nID, item.toString())
-      });
-    }
-  } catch(err){
-    this._read = function (len) {
-      this.push(err.stack);
-    }
-  } finally {
-    onFinally(this);
-    this.push(null);
-  }
+  self.on('data', function (item) {
+    objectCache.set(item[trackBy], item);
+    newIndex.push(item[trackBy]);
+  });
 
+  self.on('end', function () {
+    indexCache.set(getResourceId(), newIndex);
+  });
+
+
+  self.run = function () {
+    indexCache.get(getResourceId(), function (err, cachedIDs) {
+      if (err) {
+        self.emit('error', err);
+      } else if (cachedIDs) {
+        objectCache.mget(cachedIDs, function (err, objects) {
+          err ?
+            self.emit('error', err) :
+            self.emit('hit', cachedIDs.map(fetchObjects, objects), cachedIDs);
+        });
+      } else {
+        newIndex = [];
+        self.emit('fault');
+      }
+    });
+  };
 }
-
-CachedStream.prototype._get = function (item) {
-  return objectCache.get(item.nID) ? null : item;
-};
-
-  var a = 0;
-CachedStream.prototype._read = function (len) {
-
-  if (a < 10) {
-    a++;
-    this.push('1');
-  } else {
-    this.push(null);
-  }
-};
-
-CachedStream.prototype._write = function (chunk, enc, callback) {
-  //console.log('write', chunk.toString().length);
-  this.objectsStream.write(chunk);
-  callback();
-};
 
 module.exports = CachedStream;
