b8fda2cbc195066ed5c4aeceea8894df3adb0aa7
askosyr <askosyr@gmail.com>
2016-03-30 20:11:07 +0300

    https://github.com/e-government-ua/i/issues/1225 Initial commit

--- wf-base/src/main/java/org/igov/service/business/action/task/listener/doc/CreateDocument_UkrDoc.java
+++ wf-base/src/main/java/org/igov/service/business/action/task/listener/doc/CreateDocument_UkrDoc.java
@@ -45,6 +45,9 @@ public class CreateDocument_UkrDoc extends AbstractModelTask implements TaskList
     private Expression sHead;
     private Expression sBody;
     private Expression nID_Pattern;
+    private Expression sID_Order_GovPublic;
+    private Expression sSourceChannel;
+    
 
     @Autowired
     GeneralConfig generalConfig;
@@ -69,7 +72,9 @@ public class CreateDocument_UkrDoc extends AbstractModelTask implements TaskList
         String sHeadValue = getStringFromFieldExpression(this.sHead, execution);
         String sBodyValue = getStringFromFieldExpression(this.sBody, execution);
         String nID_PatternValue = getStringFromFieldExpression(this.nID_Pattern, execution);
-
+        String sID_Order_GovPublicValue = getStringFromFieldExpression(this.sID_Order_GovPublic, execution);
+        String sSourceChannelValue = getStringFromFieldExpression(this.sSourceChannel, execution);
+        
         LOG.info("Parameters of the task sLogin:{}, sHead:{}, sBody:{}, nId_PatternValue:{}", sLoginAuthorValue, sHeadValue,
                 sBodyValue, nID_PatternValue);
 
@@ -121,7 +126,7 @@ public class CreateDocument_UkrDoc extends AbstractModelTask implements TaskList
         LOG.info("Processing {} attachments", attachments.size());
 
         Map<String, Object> urkDocRequest = UkrDocUtil.makeJsonRequestObject(sHeadValue, sBodyValue, sLoginAuthorValue, nID_PatternValue,
-                attachments, execution.getId(), generalConfig);
+                attachments, execution.getId(), generalConfig, sID_Order_GovPublicValue, sSourceChannelValue);
 
         JSONObject json = new JSONObject();
         json.putAll(urkDocRequest);
