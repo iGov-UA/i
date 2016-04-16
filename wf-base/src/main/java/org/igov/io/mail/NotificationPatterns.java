f8941a7de1cdad8abaffa77587cf86b9ad369142
123 <123@192.168.1.102>
2016-04-16 21:24:54 +0300

    Change one word )

--- wf-base/src/main/java/org/igov/io/mail/NotificationPatterns.java
+++ wf-base/src/main/java/org/igov/io/mail/NotificationPatterns.java
@@ -53,7 +53,7 @@ public class NotificationPatterns {
             String sBody = String.format("Ваша заявка %s прийнята!", sID_Order) +
                     "<br>Ви завжди зможете переглянути її поточний статус у розділі <a href=\"" + generalConfig
                     .sHostCentral() + "/order/search?sID_Order=" + sID_Order
-                    + "\">\"Статуси\"</a>. Також на кожному етапі Ви будете отримувати email-повідомлення.	";
+                    + "\">\"Мій журнал\"</a>. Також на кожному етапі Ви будете отримувати email-повідомлення.	";
             
             /*
             Long nID_Task = oActionTaskService.getTaskIDbyProcess(null, sID_Order, Boolean.FALSE);
