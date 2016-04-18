1ef889731ef01d8cef6737a9321de279d884df22
askosyr <askosyr@gmail.com>
2016-04-18 09:50:14 +0300

    https://github.com/e-government-ua/i/issues/1218 Added sID_Sex to SubjectHumanSex enum

--- wf-central/src/main/java/org/igov/model/subject/SubjectHumanSex.java
+++ wf-central/src/main/java/org/igov/model/subject/SubjectHumanSex.java
@@ -2,7 +2,27 @@ package org.igov.model.subject;
 
 public enum SubjectHumanSex {
 
-	FEMALE,
-	MALE;
-	
+	FEMALE("FEMALE", "0"), MALE("MALE", "1");
+
+	private String sID_Sex;
+	private String nID_Sex;
+
+	private SubjectHumanSex(String sID_Sex, String nID_Sex) {
+		this.sID_Sex = sID_Sex;
+		this.nID_Sex = nID_Sex;
+	}
+
+	@Override
+	public String toString() {
+		return this.nID_Sex;
+	}
+
+	public String getsID_Sex() {
+		return sID_Sex;
+	}
+
+	public String getnID_Sex() {
+		return nID_Sex;
+	}
+
 }
