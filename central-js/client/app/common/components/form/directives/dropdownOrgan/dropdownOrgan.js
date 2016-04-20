4fdbd0e392e53ffffb49b5e77581a50e9e61f543
bw <bw@bw-pc>
2016-04-20 20:07:56 +0300

    fixes for service launchBusiness

--- central-js/client/app/common/components/form/directives/dropdownOrgan/dropdownOrgan.js
+++ central-js/client/app/common/components/form/directives/dropdownOrgan/dropdownOrgan.js
@@ -36,100 +36,109 @@ angular.module('app').directive('dropdownOrgan', function (OrganListFactory, $ht
       };
 
       var getSelectedRegion = function() {
-        for (var i=0;i<scope.organList.dropdown.list.length;i++) {
-          var region = scope.organList.dropdown.list[i];
-          if (region.sID_Public == scope.ngModel)
-            return region;
+        if(scope.organList.dropdown.list){
+          for (var i=0;i<scope.organList.dropdown.list.length;i++) {
+            var region = scope.organList.dropdown.list[i];
+            if (region.sID_Public == scope.ngModel)
+              return region;
+          }
+        }else{
+          return {};
         }
+
       };
 
       var attributesApplying = false;
 
       var loadAttributesData = function (currentKey) {
-        $http.post('./api/subject/organs/attributes/' + scope.serviceData.oSubject_Operator.nID + '/' + getSelectedRegion().nID,
-          getAttributesDataObject()).success(function (attributes) {
-          attributesApplying = true;
-          angular.forEach(attributes, function(attr){
-            console.log("attr.sName="+attr.sName+",currentKey="+currentKey);
+        var selectedRegion = getSelectedRegion();
+        if(selectedRegion.nID) {
 
-          //angular.forEach(attributes, function(attr){
-              if(attr.sValue && attr.sValue !== null && attr.sValue.substr(0,1)==="["){
-                console.log("attr.sValue="+attr.sValue);
-                var n=0;
-                console.log("scope.activitiForm="+scope.activitiForm);
-                if(scope.activitiForm && scope.activitiForm!==null){
-                    console.log("scope.activitiForm.formProperties="+scope.activitiForm.formProperties);
-                    if(scope.activitiForm.formProperties && scope.activitiForm.formProperties!==null){
-                        angular.forEach(scope.activitiForm.formProperties, function(oProperty){
-                            console.log("oProperty.id="+oProperty.id+",oProperty.type="+oProperty.type);
-                            if(oProperty.id === attr.sName && oProperty.type === "enum"){
-                                console.log('oProperty.id === attr.sValue && oProperty.type === "enum"');
-                                //if(scope.formData.params[attr.sName].type === "enum" && attr.sValue.substr(0,1)==="["){
-                                //if(scope.activitiForm.formProperties[attr.sName].type === "enum" && attr.sValue.substr(0,1)==="["){
-                                var sa=attr.sValue;
-                                console.log("sa(before)="+sa);
-                                sa=sa.substr(1);
-                                sa=sa.substr(0,sa.length-1);
-                                console.log("sa(after)="+sa);
-                                var as=sa.split(",");
-                                console.log("as="+as);
-                                var a=[];
-                                var nItem=0;
-                                angular.forEach(as, function(s){
-                                    if(s.substr(0,1)==="\""){
-                                        s=s.substr(1);
-                                    }
-                                    if(s.substr(s.length-1,1)==="\""){
-                                        s=s.substr(0,s.length-1);
-                                    }
-                                    var o={id: nItem+"", name: s};
-                                    a=a.concat([o]);
-                                    /*enumValues: [{id: "attr1_post", name: "через національного оператора поштового зв'язку"},…]
-                                          0: {id: "attr1_post", name: "через національного оператора поштового зв'язку"}
-                                          id: "attr1_post"
-                                          name: "через національного оператора поштового зв'язку"
-                                          1: {id: "attr2_bank", name: "на рахунок у банку"}
-                                          id: "attr2_bank"
-                                          name: "на рахунок у банку"*/
-                                    nItem++;
-                                });
-                                //console.log("scope.formData.params[attr.sName].enumValues="+scope.formData.params[attr.sName].enumValues);
-                                console.log("oProperty.enumValues="+oProperty.enumValues);
+          $http.post('./api/subject/organs/attributes/' + scope.serviceData.oSubject_Operator.nID + '/' + selectedRegion.nID,
+            getAttributesDataObject()).success(function (attributes) {
+            attributesApplying = true;
+            angular.forEach(attributes, function (attr) {
+              console.log("attr.sName=" + attr.sName + ",currentKey=" + currentKey);
+
+              //angular.forEach(attributes, function(attr){
+              if (attr.sValue && attr.sValue !== null && attr.sValue.substr(0, 1) === "[") {
+                console.log("attr.sValue=" + attr.sValue);
+                var n = 0;
+                console.log("scope.activitiForm=" + scope.activitiForm);
+                if (scope.activitiForm && scope.activitiForm !== null) {
+                  console.log("scope.activitiForm.formProperties=" + scope.activitiForm.formProperties);
+                  if (scope.activitiForm.formProperties && scope.activitiForm.formProperties !== null) {
+                    angular.forEach(scope.activitiForm.formProperties, function (oProperty) {
+                      console.log("oProperty.id=" + oProperty.id + ",oProperty.type=" + oProperty.type);
+                      if (oProperty.id === attr.sName && oProperty.type === "enum") {
+                        console.log('oProperty.id === attr.sValue && oProperty.type === "enum"');
+                        //if(scope.formData.params[attr.sName].type === "enum" && attr.sValue.substr(0,1)==="["){
+                        //if(scope.activitiForm.formProperties[attr.sName].type === "enum" && attr.sValue.substr(0,1)==="["){
+                        var sa = attr.sValue;
+                        console.log("sa(before)=" + sa);
+                        sa = sa.substr(1);
+                        sa = sa.substr(0, sa.length - 1);
+                        console.log("sa(after)=" + sa);
+                        var as = sa.split(",");
+                        console.log("as=" + as);
+                        var a = [];
+                        var nItem = 0;
+                        angular.forEach(as, function (s) {
+                          if (s.substr(0, 1) === "\"") {
+                            s = s.substr(1);
+                          }
+                          if (s.substr(s.length - 1, 1) === "\"") {
+                            s = s.substr(0, s.length - 1);
+                          }
+                          var o = {id: nItem + "", name: s};
+                          a = a.concat([o]);
+                          /*enumValues: [{id: "attr1_post", name: "через національного оператора поштового зв'язку"},…]
+                           0: {id: "attr1_post", name: "через національного оператора поштового зв'язку"}
+                           id: "attr1_post"
+                           name: "через національного оператора поштового зв'язку"
+                           1: {id: "attr2_bank", name: "на рахунок у банку"}
+                           id: "attr2_bank"
+                           name: "на рахунок у банку"*/
+                          nItem++;
+                        });
+                        //console.log("scope.formData.params[attr.sName].enumValues="+scope.formData.params[attr.sName].enumValues);
+                        console.log("oProperty.enumValues=" + oProperty.enumValues);
 //                                console.log("scope.activitiForm.formProperties[n].enumValues="+scope.activitiForm.formProperties[n].enumValues);
-                                //if(scope.formData.params[attr.sName].enumValues!==a){
-                                //if(oProperty.enumValues!==a && oProperty && oProperty !== null && oProperty.enumValues.length === 0 ){
-                                if(oProperty.enumValues){
+                        //if(scope.formData.params[attr.sName].enumValues!==a){
+                        //if(oProperty.enumValues!==a && oProperty && oProperty !== null && oProperty.enumValues.length === 0 ){
+                        if (oProperty.enumValues) {
 
-                                    console.log("<>");
-                                    //scope.formData.params[attr.sName].enumValues = a;
-                                    //if(oProperty.type === "enum" && oProperty.bVariable && oProperty.bVariable !== null && oProperty.bVariable === true){//oProperty.id === attr.sName &&
-                                    oProperty.bVariable = true;
+                          console.log("<>");
+                          //scope.formData.params[attr.sName].enumValues = a;
+                          //if(oProperty.type === "enum" && oProperty.bVariable && oProperty.bVariable !== null && oProperty.bVariable === true){//oProperty.id === attr.sName &&
+                          oProperty.bVariable = true;
 
-                                    oProperty.enumValues = a;
+                          oProperty.enumValues = a;
 //                                    scope.activitiForm.formProperties[n].bVariable = true;
 //                                    scope.activitiForm.formProperties[n].enumValues = a;
-                                    //var aEnum = $scope.data.formData.aEnum;
-                                    //scope.activitiForm.formProperties[n].sCustomType
+                          //var aEnum = $scope.data.formData.aEnum;
+                          //scope.activitiForm.formProperties[n].sCustomType
 //                                    scope.formData.params[attr.sName].sCustomType = "enum";
 
-                                }else{
-                                  oProperty.enumValues = [];
-                                }
-                            }
-                            n++;
-                        });
-                    }
+                        } else {
+                          oProperty.enumValues = [];
+                        }
+                      }
+                      n++;
+                    });
+                  }
                 }
-            }else if (angular.isDefined(scope.formData.params[attr.sName]) && currentKey != attr.sName){
+              } else if (angular.isDefined(scope.formData.params[attr.sName]) && currentKey != attr.sName) {
                 //console.log("isDefined,attr.sValue="+attr.sValue+",scope.formData.params[attr.sName].type="+scope.formData.params[attr.sName].type );
-                console.log("isDefined,attr.sValue="+attr.sValue );
+                console.log("isDefined,attr.sValue=" + attr.sValue);
                 scope.formData.params[attr.sName].value = attr.sValue || "";
-            }
+              }
+            });
+            $timeout(function () {
+              attributesApplying = false;
+            })
           });
-          $timeout(function(){
-            attributesApplying = false;
-          })
-        });
+        }
       };
 
       angular.forEach(Object.keys(scope.formData.params), function (key) {
