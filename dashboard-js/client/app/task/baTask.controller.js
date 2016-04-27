0cb18d288ad74447e34285686c791cb77aaf7cd4
hordesalik <hordesalik@gmail.com>
2016-04-27 19:45:11 +0300

    Реализовать открытие по урл-у "расширенного профиля задачи" и ссылку для админа из "обычного профиля" #1015

--- dashboard-js/client/app/task/baTask.controller.js
+++ dashboard-js/client/app/task/baTask.controller.js
@@ -5,10 +5,31 @@
     .module('dashboardJsApp')
     .controller('baTask', [
       '$scope',
+      '$state',
       'taskData',
-      function ($scope, taskData)
+      'Auth',
+      function ($scope, $state, taskData, Auth)
       {
         $scope.taskData = taskData;
+        $scope.openRegularView = function() {
+          console.log(window.history);
+          if (window.history.length > 0)
+            window.history.back();
+          else {
+            var type;
+            if (!taskData.sLoginAssigned)
+              type = 'unassigned';
+            else if(taskData.sLoginAssigned == Auth.getCurrentUser().id)
+              type = 'selfAssigned';
+            else
+              type = 'all';
+
+            $state.go('tasks.typeof.view', {
+              type: type,
+              id: taskData.nID_Task
+            });
+          }
+        };
       }
     ])
 })();
