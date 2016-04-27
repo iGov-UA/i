e74f029f1079e177fbe8997b9ee2a275ce57d4ac
Oleskii Domash <aleksej.domash@gmail.com>
2016-04-28 00:09:01 +0300

    Update servicebuiltinbankid.controller.js
    fix error of merge

--- central-js/client/app/service/built-in/controllers/servicebuiltinbankid.controller.js
+++ central-js/client/app/service/built-in/controllers/servicebuiltinbankid.controller.js
@@ -463,13 +463,8 @@ angular.module('app').controller('ServiceBuiltInBankIDController', function(
     });
   }
 
-<<<<<<< HEAD
     $scope.htmldecode = function(encodedhtml){
-     if(encodedhtml){
-=======
-  $scope.htmldecode = function(encodedhtml){
     if(encodedhtml){
->>>>>>> 9290934f89eb4b60f4eddf6d080c1a1db024e728
       var map = {
         '&amp;'     :   '&',
         '&gt;'      :   '>',
@@ -484,11 +479,7 @@ angular.module('app').controller('ServiceBuiltInBankIDController', function(
         result = result.replace(key, value);
       });
 
-<<<<<<< HEAD
-     return result;
-=======
     return result;
->>>>>>> 9290934f89eb4b60f4eddf6d080c1a1db024e728
     } else {
       return encodedhtml;
     }
