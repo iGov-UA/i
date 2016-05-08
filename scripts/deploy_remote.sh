b76b929fbd626829d4c2e25dd754071d8f7ec190
sybase <bw@pb.ua>
2016-05-08 22:40:15 +0300

    | grep -v "but failOnError was false"

--- scripts/deploy_remote.sh
+++ scripts/deploy_remote.sh
@@ -44,7 +44,7 @@ fallback ()
 	cd /sybase/tomcat_${sProject}$1/bin/ && ./_startup.sh
 	sleep 15
 	#Проверяем статус службы. Если нашлась ошибка в логе - завершаем скрипт с критической ошибкой.
-	if grep ERROR /sybase/tomcat_${sProject}$1/logs/catalina.out | grep -v log4j | grep -v stopServer; then
+	if grep ERROR /sybase/tomcat_${sProject}$1/logs/catalina.out | grep -v "but failOnError was false" | grep -v log4j | grep -v stopServer; then
 		echo "Fatal error found in tomcat_${sProject}$1/logs/catalina.out! Can't start previous configuration."
 		exit 1
 	fi
@@ -167,7 +167,7 @@ if [ $sProject == "wf-central"  ] || [ $sProject == "wf-region" ]; then
 	fi
 	done
 	#Проверяем на наличие ошибок вторичный инстанс
-	if grep ERROR /sybase/tomcat_${sProject}_double/logs/catalina.out | grep -v log4j | grep -v stopServer; then
+	if grep ERROR /sybase/tomcat_${sProject}_double/logs/catalina.out | grep -v "but failOnError was false" | grep -v log4j | grep -v stopServer; then
 		fallback _double
 	else
 		echo "Everything is OK. Continuing deployment ..."
@@ -181,8 +181,8 @@ if [ $sProject == "wf-central"  ] || [ $sProject == "wf-region" ]; then
 		#Развернем новое приложение на вторичном инстансе
 		deploy-tomcat
 			
-		#Проверяем на наличие ошибок вторичный инстанс
-		if grep ERROR /sybase/tomcat_${sProject}/logs/catalina.out | grep -v log4j | grep -v stopServer; then
+		#Проверяем на наличие ошибок вторичный инстанс //
+		if grep ERROR /sybase/tomcat_${sProject}/logs/catalina.out | grep -v "but failOnError was false" | grep -v log4j | grep -v stopServer; then
 			grep -B 3 -A 2 ERROR /sybase/tomcat_${sProject}/logs/catalina.out
 			#Откатываемся назад
 			fallback
