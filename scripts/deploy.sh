69a1264becd93b127e7b4cb55eed795934711546
sybase <bw@pb.ua>
2016-05-13 01:30:23 +0300

    export PATH=/usr/local/bin:$PATH

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -318,6 +318,7 @@ fi
 #Определяем сервер для установки
 if [[ $sVersion == "alpha" && $sProject == "central-js" ]] || [[ $sVersion == "alpha" && $sProject == "wf-central" ]]; then
 		sHost="test.igov.org.ua"
+		export PATH=/usr/local/bin:$PATH
 fi
 if [[ $sVersion == "beta" && $sProject == "central-js" ]] || [[ $sVersion == "beta" && $sProject == "wf-central" ]]; then
 		sHost="test-version.igov.org.ua"
@@ -329,6 +330,7 @@ fi
 
 if [[ $sVersion == "alpha" && $sProject == "dashboard-js" ]] || [[ $sVersion == "alpha" && $sProject == "wf-region" ]]; then
 		sHost="test.region.igov.org.ua"
+		export PATH=/usr/local/bin:$PATH
 fi
 if [[ $sVersion == "beta" && $sProject == "dashboard-js" ]] || [[ $sVersion == "beta" && $sProject == "wf-region" ]]; then
 		sHost="test-version.region.igov.org.ua"
