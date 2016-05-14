7957d2fa7bc8df6e1eb284ed357a66a6d36d8f5c
Igor Jarmak <igor.jarmak@gmail.com>
2016-05-14 13:06:43 +0300

    #1213 fix bugs with bankid mock

--- central-js/server/auth/bankid-mock/index.js
+++ central-js/server/auth/bankid-mock/index.js
@@ -4,49 +4,23 @@
 'use strict';
 
 var express = require('express')
-  , authService = require('../auth.service');
+  , authService = require('../auth.service')
+  , mock = require('../../api/user-mock/user.data.js');
 
 var router = express.Router();
 
 router.get('/', function (req, res, next) {
-  if (req.cookies['authMock'] && (req.headers.host.split(':')[0] === 'localhost' || req.headers.host.split(':')[0] === 'test.igov.org.ua')) {
-    var user = {
-      customer: {
-        type: 'physical',
-        clId: '1',
-        clIdText: '',
-        lastName: 'MockUser',
-        firstName: req.cookies['authMock'],
-        middleName: 'MockUser',
-        phone: '+380101010101',
-        birthDay: '',
-        inn: '1',
-        email: 'Mock@PRIVATBANK.UA',
-        addresses: [[Object], [Object]],
-        documents: [[Object]],
-        scans: [[Object]]
-      },
-      admin: {
-        inn: '1',
-        token: 'a'
-      },
-      subject: {
-        sID: '1',
-        sLabel: null,
-        sLabelShort: null,
-        aSubjectAccountContact: null,
-        nID: 20045
-      },
-      usercacheid: 'a'
-    };
-    req.session = authService.createSessionObject('bankIDMock', user, {});
+  //if (req.cookies['authMock'] && (req.headers.host.split(':')[0] === 'localhost' || req.headers.host.split(':')[0] === 'test.igov.org.ua')) {
+  if (mock.isMockEnabled(req.cookies)) {
+    var user = mock.user;
+    var token = mock.token;
+
+    req.session = authService.createSessionObject('bankid', user, token);
+    delete req.session.prepare;
     res.redirect(req.query.link);
   } else {
     next();
   }
-  //res.send(req.cookies);
-  //req.session = authService.createSessionObject('bankIDMock', user, info);
-  //res.redirect(req.query.link);
 });
 
 module.exports = router;
