78a64843a7a099018e8cc63c1e88bfe0cb3f8925
domash <aleksej.domash@gmail.com>
2016-05-23 21:47:32 +0300

    #1245 changing value for bankid parameter

--- central-js/server/auth/bankid/bankid.passport.js
+++ central-js/server/auth/bankid/bankid.passport.js
@@ -28,7 +28,7 @@ exports.setup = function (config) {
         if(options.eds){
           return {eds : true};
         } else if (options.mpbds){
-          return {bank : 'mpbds'};
+          return {bank : 'mcid'};
         } else {
           return {};
         }
