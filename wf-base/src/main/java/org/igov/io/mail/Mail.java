f7fb1d4a5d88718e2eee7c0d75c978d510574746
Igor Maksymov <ivmaksymov@gmail.com>
2016-05-31 23:56:33 +0300

    #1278: test

--- wf-base/src/main/java/org/igov/io/mail/Mail.java
+++ wf-base/src/main/java/org/igov/io/mail/Mail.java
@@ -74,13 +74,17 @@ public class Mail extends Abstract_Mail {
         LOG.info("(getTo()={})", getTo());
         String sTo=getTo();
         String sToNew=sTo;
+        String toName = "TESTTONAME";
+
+        sToNew = String.format("\"%s\" <%s>", toName, sTo);
+
         sToNew=sToNew.replace("\"", "");
         sToNew=sToNew.replace("\"", "");
         //sTo=sTo.replaceAll("\"", "");
-        if(!sToNew.equals(sTo)){
-            LOG.info("(getTo()(fixed)={})", sToNew);
-            _To(sToNew);
-        }
+//        if(!sToNew.equals(sTo)){
+//            LOG.info("(getTo()(fixed)={})", sToNew);
+//            _To(sToNew);
+//        }
         LOG.info("(getHead()={})", getHead());
         
         Boolean bUniSender = generalConfig.isEnable_UniSender_Mail();
@@ -171,6 +175,8 @@ public class Mail extends Abstract_Mail {
                     sbBody.append(getFrom());
                     sbBody.append("\nto:");
                     sbBody.append(getTo());
+                    sbBody.append("\ntoName:");
+                    sbBody.append(getToName());
                     sbBody.append("\nhead:");
                     sbBody.append(getHead());
 //                    sbBody.append(getBody());
