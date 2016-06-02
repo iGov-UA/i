5c647e6c092365721cb14e3533bd05a81188150b
antonlegkiy <anton.legkiy@gmail.com>
2016-06-02 13:34:37 +0300

    fix comment from citizen

--- central-js/client/app/journal/journal.search.controller.js
+++ central-js/client/app/journal/journal.search.controller.js
@@ -240,15 +240,15 @@ angular.module('journal').controller('JournalSearchController', function (
           if (sToken !== null) {
             oData = $.extend(oData, {sToken: sToken});
           }
-          if ($scope.$scope.aField) {
+          if ($scope.aOrderMessages[0].aData) {
             try {
-              angular.forEach($scope.aField, function (oField) {
+              angular.forEach($scope.aOrderMessages[0].aData, function (oField) {
                 if (oField.sType === "date") {
                   oField.sValueNew = oField.oFactory.value ? oField.oFactory.get() : oField.sValueNew;//.value
                   oField.oFactory = null;
                 }
               });
-              oData.saField = JSON.stringify($scope.aField);
+              oData.saField = JSON.stringify($scope.aOrderMessages[0].aData);
             } catch (sError) {
               ErrorsFactory.addFail({
                 sBody: 'Помилка сереалізації об`єкту з полями, у яких відповіді на зауваження!',
