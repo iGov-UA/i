1f11e07421bb08463ad5ef695e54aee994bc0b0d
Ольга <olga.kuzminova87@gmail.com>
2016-06-05 01:07:56 +0300

    fix

--- wf-base/src/main/java/org/igov/service/business/action/task/systemtask/mail/MailTaskWithAttachmentsAndSMS.java
+++ wf-base/src/main/java/org/igov/service/business/action/task/systemtask/mail/MailTaskWithAttachmentsAndSMS.java
@@ -26,8 +26,8 @@ public class MailTaskWithAttachmentsAndSMS extends Abstract_MailTaskCustom {
     private Expression saAttachmentsForSend;
 
     protected Expression sPhone_SMS;
-    protected Expression sText_SMS;    
-    
+    protected Expression sText_SMS;
+
     @Override
     public void execute(DelegateExecution oExecution) throws Exception {
         System.setProperty("mail.mime.address.strict", "false");
@@ -35,115 +35,81 @@ public class MailTaskWithAttachmentsAndSMS extends Abstract_MailTaskCustom {
         //MultiPartEmail oMultiPartEmail = MultiPartEmail_BaseFromTask(oExecution);
         Mail oMail = Mail_BaseFromTask(oExecution);
 
-
         String sPhone_SMS = getStringFromFieldExpression(this.sPhone_SMS, oExecution);
-        if(sPhone_SMS!=null){
+        if (sPhone_SMS != null) {
             String sText_SMS = getStringFromFieldExpression(this.sText_SMS, oExecution);
-            if(sText_SMS!=null){
+            if (sText_SMS != null) {
                 sText_SMS = replaceTags(sText_SMS, oExecution);
-                
-                //sPhone_SMS="38"+sPhone_SMS;
                 String sReturn;
-
                 sPhone_SMS = sPhone_SMS.replaceAll("\\ ", "");
-
-//                sReturn = oManagerOTP.sendPasswordOTP(sPhone_SMS, sText_SMS, true);                
                 sReturn = oManagerSMS.sendSMS(sPhone_SMS, sText_SMS);
-                LOG.info("(sReturn={})",sReturn);
-/*                
-                LOG.info("SKIP:(sReturn})");
-*/
-                
-/* 
-                sReturn = oManagerOTP.sendPasswordOTP(sPhone_SMS.substring(1), sText_SMS, true);
-                LOG.info("(sReturn={})",sReturn);
-
-                sReturn = oManagerOTP.sendPasswordOTP(sPhone_SMS.substring(2), sText_SMS, true);
-                LOG.info("(sReturn={})",sReturn);
-
-                sReturn = oManagerOTP.sendPasswordOTP(sPhone_SMS.substring(3), sText_SMS, true);
-                LOG.info("(sReturn={})",sReturn);
-*/
-                /*sReturn = oManagerOTP.sendPasswordOTP("+38"+sPhone_SMS, sText_SMS, true);
-                LOG.info("(sReturn={})",sReturn);
-                
-                sReturn = oManagerOTP.sendPasswordOTP("38"+sPhone_SMS, sText_SMS, true);
-                LOG.info("(sReturn={})",sReturn);
-                
-                sReturn = oManagerOTP.sendPasswordOTP("8"+sPhone_SMS, sText_SMS, true);
-                LOG.info("(sReturn={})",sReturn);*/
-
-
+                LOG.info("(sReturn={})", sReturn);
             }
         }
 
-                
-        
-        String sAttachmentsForSend = this.saAttachmentsForSend==null || "".equals(this.saAttachmentsForSend) ? "" : getStringFromFieldExpression(this.saAttachmentsForSend, oExecution);
-        //sAttachmentsForSend = sAttachmentsForSend == null ? "" : sAttachmentsForSend;
-        LOG.info("(sAttachmentsForSend={})", sAttachmentsForSend);
-        List<Attachment> aAttachment = new ArrayList<>();
-        String[] asID_Attachment = sAttachmentsForSend.split(",");
-        for (String sID_Attachment : asID_Attachment) {
-            //log.info("sID_Attachment=" + sID_Attachment);
-            if (sID_Attachment != null && !"".equals(sID_Attachment.trim()) && !"null".equals(sID_Attachment.trim())) {
-                String sID_AttachmentTrimmed = sID_Attachment.replaceAll("^\"|\"$", "");
-                LOG.info("(sID_AttachmentTrimmed={})", sID_AttachmentTrimmed);
-                Attachment oAttachment = taskService.getAttachment(sID_AttachmentTrimmed);
-                if (oAttachment != null) {
-                    aAttachment.add(oAttachment);
+        try {
+            String sAttachmentsForSend = this.saAttachmentsForSend == null || "".equals(this.saAttachmentsForSend) ? "" : getStringFromFieldExpression(this.saAttachmentsForSend, oExecution);
+            LOG.info("(sAttachmentsForSend={})", sAttachmentsForSend);
+            List<Attachment> aAttachment = new ArrayList<>();
+            String[] asID_Attachment = sAttachmentsForSend.split(",");
+            for (String sID_Attachment : asID_Attachment) {
+                //log.info("sID_Attachment=" + sID_Attachment);
+                if (sID_Attachment != null && !"".equals(sID_Attachment.trim()) && !"null".equals(sID_Attachment.trim())) {
+                    String sID_AttachmentTrimmed = sID_Attachment.replaceAll("^\"|\"$", "");
+                    LOG.info("(sID_AttachmentTrimmed={})", sID_AttachmentTrimmed);
+                    Attachment oAttachment = taskService.getAttachment(sID_AttachmentTrimmed);
+                    if (oAttachment != null) {
+                        aAttachment.add(oAttachment);
+                    }
+                } else {
+                    LOG.warn("(sID_Attachment={})", sID_Attachment);
                 }
-            } else {
-                LOG.warn("(sID_Attachment={})", sID_Attachment);
             }
-        }
 
-        if (!aAttachment.isEmpty()) {
-            InputStream oInputStream_Attachment = null;
-            String sFileName = "document";
-            String sFileExt = "txt";
-            String sDescription = "";
-            for (Attachment oAttachment : aAttachment) {
-                sFileName = oAttachment.getName();
-                String sExt="";
-                int nAt=sFileName.lastIndexOf(".");
-                if(nAt>=0){
-                    sExt = sFileName.substring(nAt);
-                }
-                sFileName = "Attach_"+oAttachment.getId()+sExt; //
-                sFileExt = oAttachment.getType().split(";")[0];
-                sDescription = oAttachment.getDescription();
-                if (sDescription == null || "".equals(sDescription.trim())) {
-                    sDescription = "(no description)";
-                }
-                LOG.info("(oAttachment.getId()={}, sFileName={}, sFileExt={}, sDescription={})",
-                        oAttachment.getId(), sFileName, sFileExt, sDescription);
-                oInputStream_Attachment = oExecution.getEngineServices().getTaskService()
-                        .getAttachmentContent(oAttachment.getId());
-                if (oInputStream_Attachment == null) {
-                    LOG.error("Attachment with (id={}) doesn't have content associated with it.", oAttachment.getId());
-                    throw new ActivitiObjectNotFoundException(
-                            "Attachment with id '" + oAttachment.getId() + "' doesn't have content associated with it.",
-                            Attachment.class);
-                }
-                DataSource oDataSource = new ByteArrayDataSource(oInputStream_Attachment, sFileExt);
-                if (oDataSource == null) {
-                    LOG.error("Attachment: oDataSource == null");
-                }
+            if (!aAttachment.isEmpty()) {
+                InputStream oInputStream_Attachment = null;
+                String sFileName = "document";
+                String sFileExt = "txt";
+                String sDescription = "";
+                for (Attachment oAttachment : aAttachment) {
+                    sFileName = oAttachment.getName();
+                    String sExt = "";
+                    int nAt = sFileName.lastIndexOf(".");
+                    if (nAt >= 0) {
+                        sExt = sFileName.substring(nAt);
+                    }
+                    sFileName = "Attach_" + oAttachment.getId() + sExt; //
+                    sFileExt = oAttachment.getType().split(";")[0];
+                    sDescription = oAttachment.getDescription();
+                    if (sDescription == null || "".equals(sDescription.trim())) {
+                        sDescription = "(no description)";
+                    }
+                    LOG.info("(oAttachment.getId()={}, sFileName={}, sFileExt={}, sDescription={})",
+                            oAttachment.getId(), sFileName, sFileExt, sDescription);
+                    oInputStream_Attachment = oExecution.getEngineServices().getTaskService()
+                            .getAttachmentContent(oAttachment.getId());
+                    if (oInputStream_Attachment == null) {
+                        LOG.error("Attachment with (id={}) doesn't have content associated with it.", oAttachment.getId());
+                        throw new ActivitiObjectNotFoundException(
+                                "Attachment with id '" + oAttachment.getId() + "' doesn't have content associated with it.",
+                                Attachment.class);
+                    }
+                    DataSource oDataSource = new ByteArrayDataSource(oInputStream_Attachment, sFileExt);
+                    if (oDataSource == null) {
+                        LOG.error("Attachment: oDataSource == null");
+                    }
 
-                //oMail._Attach(oDataSource, sFileName + "." + sFileExt, sDescription);
-                oMail._Attach(oDataSource, sFileName, sDescription);
+                    //oMail._Attach(oDataSource, sFileName + "." + sFileExt, sDescription);
+                    oMail._Attach(oDataSource, sFileName, sDescription);
 
-                LOG.info("oMultiPartEmail.attach: Ok!");
+                    LOG.info("oMultiPartEmail.attach: Ok!");
+                }
             }
-        //} else {
-        //    LOG.error("aAttachment has nothing!");
-        //    throw new ActivitiObjectNotFoundException("add the file to send");
+            oMail.send();
+        } catch (Exception ex) {
+            LOG.error("Eror!!!", ex);
+            throw ex;
         }
-
-        // send the email
-        //oMultiPartEmail.send();
-        oMail.send();
     }
 
 }
