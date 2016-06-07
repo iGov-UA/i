5d8cc6795da8dfda621b90ac2415a1aa3e386c12
Oleksii Khalikov <oleksii.khalikov@gmail.com>
2016-06-08 01:18:46 +0300

    https://github.com/e-government-ua/i/issues/1281 revert

--- public-js/markers/validation.service.js
+++ public-js/markers/validation.service.js
@@ -75,6 +75,7 @@ function ValidationService(moment, amMoment, angularMomentConfig, MarkersFactory
     }
 
     angular.forEach(markers.validate, function (marker, markerName) {
+      /*
       var isOrganJoinInclude = false;
       var isOrganNoInclude = true;
       for(var nFieldID = 0; nFieldID < marker.aField_ID.length; nFieldID++){
@@ -91,7 +92,7 @@ function ValidationService(moment, amMoment, angularMomentConfig, MarkersFactory
           marker.original = self.oFormDataParams.sID_Public_SubjectOrganJoin;
         }
       }
-
+*/
       angular.forEach(form, function (formField) {
 
         self.setValidatorByMarker(marker, markerName, formField, immediateValidation);
@@ -105,9 +106,9 @@ function ValidationService(moment, amMoment, angularMomentConfig, MarkersFactory
     if (markerName.indexOf('FileExtensions_') == 0) {
       markerName = 'FileExtensions';
     }
-    if (markerName.indexOf('FieldNotEmptyAndNonZero_') == 0) {
+    /*if (markerName.indexOf('FieldNotEmptyAndNonZero_') == 0) {
       markerName = 'FieldNotEmptyAndNonZero';
-    }
+    }*/
 
     var keyByMarkerName = self.validatorNameByMarkerName[markerName];
     var fieldNameIsListedInMarker = formField && formField.$name && _.indexOf(marker.aField_ID, formField.$name) !== -1;
@@ -757,9 +758,9 @@ function ValidationService(moment, amMoment, angularMomentConfig, MarkersFactory
      */
     'FieldNotEmptyAndNonZero': function (modelValue, viewValue, options) {
       var sValue = modelValue;
-      if(options.original){
+      /*if(options.original){
         sValue = this.oFormDataParams.sID_Public_SubjectOrganJoin.value;
-      }
+      }*/
       //debugger;
       //console.log("[FieldNotEmptyAndNonZero]sValue=" + sValue);
       if (!sValue) {
