b2a3663890bb9c127932b626933a5f6fb894726c
askosyr <askosyr@gmail.com>
2016-06-17 16:41:30 +0300

    https://github.com/e-government-ua/i/issues/1291 Added additional logging

--- wf-base/src/main/java/org/igov/service/business/action/task/systemtask/mail/Abstract_MailTaskCustom.java
+++ wf-base/src/main/java/org/igov/service/business/action/task/systemtask/mail/Abstract_MailTaskCustom.java
@@ -581,7 +581,7 @@ public abstract class Abstract_MailTaskCustom implements JavaDelegate {
 			
 			@Override
 			public void run() {
-				LOG.info("try to save service message with params: (params={})", params);
+				LOG.info("try to save service message with params with a delay: (params={})", params);
 		        String jsonServiceMessage;
 				try {
 					jsonServiceMessage = historyEventService.addServiceMessage(params);
@@ -594,7 +594,7 @@ public abstract class Abstract_MailTaskCustom implements JavaDelegate {
 		// run saving message in 10 seconds so history event will be in the database already by that time
 		executor.schedule(task, 10, TimeUnit.SECONDS);
 		executor.shutdown();
-		
+		LOG.debug("Configured thread to run in 10 seconds with params: (params={})", params);
     }
 
 }
