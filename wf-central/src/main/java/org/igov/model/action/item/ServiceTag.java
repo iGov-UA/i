181ab2e23f94a918c58bf608dd8c9bfd6abae8a7
brunneng <good.guy.finish.first@gmail.com>
2016-06-20 22:52:52 +0300

    [issue #1289]: ServiceTag model updated.

--- wf-central/src/main/java/org/igov/model/action/item/ServiceTag.java
+++ wf-central/src/main/java/org/igov/model/action/item/ServiceTag.java
@@ -1,6 +1,12 @@
 package org.igov.model.action.item;
 
-import org.igov.model.core.NamedEntity;
+import org.igov.model.access.AccessServiceRole;
+import org.igov.model.core.AbstractEntity;
+import org.igov.model.core.Entity;
+
+import javax.persistence.Column;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 /**
  * Tag of Service
@@ -9,5 +15,46 @@ import org.igov.model.core.NamedEntity;
  * Time: 20:27
  */
 @javax.persistence.Entity
-public class ServiceTag extends NamedEntity {
+public class ServiceTag extends AbstractEntity {
+
+    @Column
+    private String sID;
+
+    @Column
+    private String sName_UA;
+
+    @Column
+    private String sName_RU;
+
+    @ManyToOne(targetEntity = ServiceTagType.class)
+    @JoinColumn(name="nID_ServiceTagType", nullable = false, updatable = false)
+    private ServiceTagType serviceTagType;
+
+    public String getsID() {
+        return sID;
+    }
+    public void setsID(String sID) {
+        this.sID = sID;
+    }
+
+    public String getsName_UA() {
+        return sName_UA;
+    }
+    public void setsName_UA(String sName_UA) {
+        this.sName_UA = sName_UA;
+    }
+
+    public String getsName_RU() {
+        return sName_RU;
+    }
+    public void setsName_RU(String sName_RU) {
+        this.sName_RU = sName_RU;
+    }
+
+    public ServiceTagType getServiceTagType() {
+        return serviceTagType;
+    }
+    public void setServiceTagType(ServiceTagType serviceTagType) {
+        this.serviceTagType = serviceTagType;
+    }
 }
