08b3856e04d3c3eb0325ea309193193253929e7e
olya <olga.kuzminova87@gmail.com>
2016-06-22 11:55:39 +0300

    доработка удаления всех процессов по ключу процесса

--- wf-base/src/main/java/org/igov/service/business/action/task/systemtask/DeleteProccess.java
+++ wf-base/src/main/java/org/igov/service/business/action/task/systemtask/DeleteProccess.java
@@ -12,14 +12,20 @@ package org.igov.service.business.action.task.systemtask;
 import java.util.List;
 import org.activiti.engine.RuntimeService;
 import org.activiti.engine.delegate.DelegateExecution;
+import org.activiti.engine.delegate.Expression;
 import org.activiti.engine.delegate.JavaDelegate;
 import org.activiti.engine.runtime.ProcessInstance;
+import org.activiti.engine.runtime.ProcessInstanceQuery;
 import org.igov.io.GeneralConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component("deleteProccess")
 public class DeleteProccess implements JavaDelegate {
+
+    private final static Logger LOG = LoggerFactory.getLogger(DeleteProccess.class);
 
     @Autowired
     private RuntimeService runtimeService;
@@ -27,14 +33,33 @@ public class DeleteProccess implements JavaDelegate {
     @Autowired
     GeneralConfig generalConfig;
 
+    public Expression processDefinitionKey;
+
     @Override
     public void execute(DelegateExecution execution) throws Exception {
-        if (generalConfig.isSelfTest()) {
-            List<ProcessInstance> processInstances = runtimeService.createProcessInstanceQuery().list();
-            for (ProcessInstance processInstance : processInstances) {
-                runtimeService.deleteProcessInstance(processInstance.getProcessInstanceId(), "deprecated");
-            }
+        String processDefinitionKeyValue = getStringFromFieldExpression(this.processDefinitionKey, execution);
+        //if (generalConfig.isSelfTest()) {
+        //List<ProcessInstance> processInstances = runtimeService.createProcessInstanceQuery().list();
+        ProcessInstanceQuery processInstanceQuery = runtimeService.createProcessInstanceQuery();
+        if (processDefinitionKeyValue != null) {
+            LOG.info("Delete all active proccess with processDefinitionKeyValue: " + processDefinitionKeyValue);
+            processInstanceQuery.processDefinitionKey(processDefinitionKeyValue);
         }
+        List<ProcessInstance> processInstances = processInstanceQuery.list();
+        for (ProcessInstance processInstance : processInstances) {
+            runtimeService.deleteProcessInstance(processInstance.getProcessInstanceId(), "deprecated");
+        }
+        //}
     }
 
+    protected String getStringFromFieldExpression(Expression expression,
+            DelegateExecution execution) {
+        if (expression != null) {
+            Object value = expression.getValue(execution);
+            if (value != null) {
+                return value.toString();
+            }
+        }
+        return null;
+    }
 }
