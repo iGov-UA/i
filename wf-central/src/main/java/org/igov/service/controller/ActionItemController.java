085bb030f5c93abf783de32b34842848db78b03f
bw <bw@bw-pc>
2016-07-18 17:31:24 +0300

    List<ServiceTagTreeNodeVO> res = serviceTagService.getCatalogTreeTag(nID_Category, sFind, asID_Place_UA,                 bShowEmptyFolders, StringUtils.isNotBlank(sFind), null, null);         res.forEach(n -> n.setaService(n.getaService().stream().map(                 s -> prepareServiceToView(s, false)).collect(Collectors.toList())));

--- wf-central/src/main/java/org/igov/service/controller/ActionItemController.java
+++ wf-central/src/main/java/org/igov/service/controller/ActionItemController.java
@@ -981,6 +981,9 @@ public class ActionItemController {
     ) {
         List<ServiceTagTreeNodeVO> res = serviceTagService.getCatalogTreeTag(nID_Category, sFind, asID_Place_UA,
                 bShowEmptyFolders, StringUtils.isNotBlank(sFind), null, null);
+        res.forEach(n -> n.setaService(n.getaService().stream().map(
+                s -> prepareServiceToView(s, false)).collect(Collectors.toList())));
+        
         return JsonRestUtils.toJsonResponse(res);
     }
 
