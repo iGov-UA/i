14aa2408cb7a9d8e24f94b3cbb6ee07a3fbc4e92
Khalikov <gfalcon273@gmail.com>
2016-07-20 01:57:37 +0300

    https://github.com/e-government-ua/i/issues/1009 fix

--- public-js/markers/schema.js
+++ public-js/markers/schema.js
@@ -79,7 +79,12 @@ angular.module('iGovMarkers')
                                 aField_ID: {"$ref": "#/definitions/stringArray"},
                                 asID_Field: {type: "object", patternProperties: {"^[A-Za-z]": {type: "string"}}},
                                 sCondition: {type: "string"},
-                                asID_Field_sValue: {"$ref": "#/definitions/stringArray"},
+                                asID_Field_sValue: {
+                                    type: "array",
+                                    minItems: 1,
+                                    items: {type: "string"},
+                                    uniqueItems: false
+                                },
                                 sNote: {type: "string"}
                             },
                             required: ["aField_ID", "asID_Field", "sCondition", "asID_Field_sValue"],
@@ -246,7 +251,7 @@ angular.module('iGovMarkers')
                             required: ["aField_ID"],
                             additionalProperties: false
                         },
-                        "NumberBetween": {
+                        "^NumberBetween": {
                             type: "object",
                             properties: {
                                 aField_ID: {"$ref": "#/definitions/stringArray"},
