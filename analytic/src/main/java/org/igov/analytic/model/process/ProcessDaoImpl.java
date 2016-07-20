f19670d89611f4fabaf23b6cb600e91afa9f51d1
olga <olga@olga-pc>
2016-07-20 22:57:47 +0300

    fix context

--- analytic/src/main/java/org/igov/analytic/model/process/ProcessDaoImpl.java
+++ analytic/src/main/java/org/igov/analytic/model/process/ProcessDaoImpl.java
@@ -6,21 +6,21 @@
 package org.igov.analytic.model.process;
 
 import org.apache.log4j.Logger;
-import org.igov.model.core.GenericEntityDao;
+import org.igov.analytic.model.core.GenericEntityDaoAnalytic;
 import org.springframework.stereotype.Repository;
 
 /**
  *
  * @author olga
  */
-@Repository
-public class ProcessDaoImpl extends GenericEntityDao<Long, Process> implements ProcessDao {
+//@Transactional("transactionManagerAnalytic")
+@Repository("processDao")
+public class ProcessDaoImpl extends GenericEntityDaoAnalytic<Long, Process> implements ProcessDao {
 
     private static final Logger log = Logger.getLogger(ProcessDaoImpl.class);
 
     protected ProcessDaoImpl() {
         super(Process.class);
     }
-    
-    //transactionManagerAnalytic
+
 }
