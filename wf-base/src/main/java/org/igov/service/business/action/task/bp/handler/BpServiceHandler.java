7ee6b96ebab441cdcdeacc75603594e1157d14c3
Dmytro Tsapko <tsapko.dmitry@gmail.com>
2016-07-31 22:49:38 +0300

    [issue #1317]: "organ" parameter was trimmed for one group. (for root cause analysis purpose)

--- wf-base/src/main/java/org/igov/service/business/action/task/bp/handler/BpServiceHandler.java
+++ wf-base/src/main/java/org/igov/service/business/action/task/bp/handler/BpServiceHandler.java
@@ -206,7 +206,8 @@ public class BpServiceHandler {
         if(res.endsWith(DELIMITER)){
             res = StringUtils.removeEnd(res, DELIMITER);
         }
-        return res;
+//        return res;
+        return organs.iterator().next().toString();
     }
 
     private String getPlaceForProcess(String sID_Process) {
