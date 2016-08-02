9f89e11bb08df395634611c8f7efafbea81446fe
Dimiys <Dimiys@ua.FM>
2016-08-03 01:49:01 +0300

    Update iTest_Node.sh

--- scripts/iTest_Node.sh
+++ scripts/iTest_Node.sh
@@ -13,34 +13,105 @@ do
 			sVersion="$2"
 			shift
 			;;
-		--type)
-                        sType="$2"
-                        shift
-                        ;;
-		--tier)
-                        sTier="$2"
-                        shift
-                        ;;
+		--project)
+			sProject="$2"
+			shift
+			;;
+		--skip-deploy)
+			bSkipDeploy="$2"
+			shift
+			;;
+		--skip-build)
+			bSkipBuild="$2"
+			shift
+			;;
+		--skip-test)
+			bSkipTest="$2"
+			shift
+			;;
+		--skip-doc)
+			bSkipDoc="$2"
+			shift
+			;;
+		--deploy-timeout)
+			nSecondsWait="$2"
+			shift
+			;;
+		--compile)
+			IFS=',' read -r -a saCompile <<< "$2"
+			shift
+			;;
+		--jenkins-user)
+			sJenkinsUser="$2"
+			shift
+			;;
+		--jenkins-api)
+			sJenkinsAPI="$2"
+			shift
+			;;
+		--docker)
+			bDocker="$2"
+			shift
+			;;
+		--dockerOnly)
+			bDockerOnly="$2"
+			shift
+			;;
+		--gitCommit)
+			sGitCommit="$2"
+			shift
+			;;
+		--dockerOnly)
+			bDockerOnly="$2"
+			shift
+			;;
+		--gitCommit)
+			sGitCommit="$2"
+			shift
+			;;
+		*)
+			echo "bad option"
+			exit 1
+			;;
+	esac
+shift
+done
 
-build_in_container ()
+unset IFS
+sDate=`date "+%Y.%m.%d-%H.%M.%S"`
+
+build_central-js ()
 {
-	if [ "$sTier" == "front" ]; then
-		if [ "$bDocker" == "false" ]; then
-			sCmd="$sCmd $sBuildContainerFrontOld "
-		else
-			sCmd="$sCmd $sBuildContainerFront "
-		fi
-		echo $sCmd
-		$sCmd bash -c " \
-					cd $sProject && \
-					npm cache clean && \
-					npm install && \
-					npm list grunt && \
-					npm list grunt-google-cdn && \
-					bower install && \
-					npm install grunt-contrib-imagemin && \
-					grunt test server && \
-					cd dist && \
-					npm install --production"
-		
+	if [ "$bSkipBuild" == "true" ]; then
+		echo "Deploy to host: $sHost"
+		cd central-js
+		rsync -az --delete -e 'ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no' dist/ sybase@$sHost:/sybase/.upload/central-js/
+		return
+	fi
+	if [ "$bSkipDeploy" == "true" ]; then
+		cd central-js
+		npm cache clean
+		npm install
+		bower install
+		npm install grunt-contrib-imagemin
+		grunt test server
+		cd dist
+		npm install --production
+		cd ..
+		rm -rf /tmp/$sProject
+		return
+	else
+		cd central-js
+		npm cache clean
+		npm install
+		bower install
+		npm install grunt-contrib-imagemin
+		grunt test server
+		cd dist
+		npm install --production
+		cd ..
+		rm -rf /tmp/$sProject
+		rsync -az --delete -e 'ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no' dist/ sybase@$sHost:/sybase/.upload/central-js/
+		cd ..
+	fi
 }
