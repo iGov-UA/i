8a176eb07e8ce3e09daf0786d6d83b5932f58f2d
Dimiys <Dimiys@ua.FM>
2016-08-03 01:38:07 +0300

    Update iTest_Node.sh

--- scripts/iTest_Node.sh
+++ scripts/iTest_Node.sh
@@ -252,25 +252,3 @@ build_in_container ()
 		exit 1
 	fi
 }
-
-build_central-js ()
-{
-	if [ "$bSkipBuild" == "true" ]; then
-		echo "Deploy to host: $sHost"
-		cd central-js
-		rsync -az --delete -e 'ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no' dist/ $sHost:/sybase/.upload/central-js/
-		return
-	fi
-	if [ "$bSkipDeploy" == "true" ]; then
-		cd central-js
-		npm cache clean
-		npm install
-		bower install
-		npm install grunt-contrib-imagemin
-		grunt test server
-		cd dist
-		npm install --production
-		cd ..
-		rm -rf $sTmpPath
-		return
-Ї}
