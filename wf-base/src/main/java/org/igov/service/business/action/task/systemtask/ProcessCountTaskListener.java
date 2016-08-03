4898346cc8af579bd90b7cd84436b05f9f855532
askosyr <askosyr@gmail.com>
2016-08-03 11:52:57 +0300

    https://github.com/e-government-ua/i/issues/1314 Added logic to retrieve variable from processes variables

--- wf-base/src/main/java/org/igov/service/business/action/task/systemtask/ProcessCountTaskListener.java
+++ wf-base/src/main/java/org/igov/service/business/action/task/systemtask/ProcessCountTaskListener.java
@@ -1,7 +1,5 @@
 package org.igov.service.business.action.task.systemtask;
 
-import java.util.List;
-
 import org.activiti.engine.FormService;
 import org.activiti.engine.RuntimeService;
 import org.activiti.engine.TaskService;
@@ -9,11 +7,6 @@ import org.activiti.engine.delegate.DelegateExecution;
 import org.activiti.engine.delegate.DelegateTask;
 import org.activiti.engine.delegate.JavaDelegate;
 import org.activiti.engine.delegate.TaskListener;
-import org.activiti.engine.form.FormProperty;
-import org.activiti.engine.form.StartFormData;
-import org.activiti.engine.form.TaskFormData;
-import org.activiti.engine.impl.form.FormPropertyImpl;
-import org.activiti.engine.task.Task;
 import org.apache.commons.lang3.StringUtils;
 import org.igov.io.GeneralConfig;
 import org.igov.io.web.HttpRequester;
@@ -32,7 +25,7 @@ import org.springframework.stereotype.Component;
 @Component("ProcessCountTaskListener")
 public class ProcessCountTaskListener implements JavaDelegate, TaskListener {
 
-	private static final String S_ID_ORDER_GOV_PUBLIC = "sID_Order_GovPublic";
+	public static final String S_ID_ORDER_GOV_PUBLIC = "sID_Order_GovPublic";
 
 	private final static Logger LOG = LoggerFactory.getLogger(CreateDocument_UkrDoc.class);
 	
@@ -62,30 +55,6 @@ public class ProcessCountTaskListener implements JavaDelegate, TaskListener {
 		if (processCount != null) {
             runtimeService.setVariable(execution.getProcessInstanceId(), S_ID_ORDER_GOV_PUBLIC, processCount);
             LOG.info("Set variable to runtime process:{}", processCount);
-
-            LOG.info("Looking for a new task to set form properties");
-            List<Task> tasks = taskService.createTaskQuery().processInstanceId(execution.getId()).active().list();
-            LOG.info("Get {} active tasks for the process", tasks);
-            for (Task task : tasks) {
-                TaskFormData formData = formService.getTaskFormData(task.getId());
-                for (FormProperty formProperty : formData.getFormProperties()) {
-                    if (formProperty.getId().equals(S_ID_ORDER_GOV_PUBLIC)) {
-                        LOG.info("Found form property with the id " + S_ID_ORDER_GOV_PUBLIC + ". Setting value {}", processCount);
-                        if (formProperty instanceof FormPropertyImpl) {
-                            ((FormPropertyImpl) formProperty).setValue(processCount);
-                        }
-                    }
-                }
-                StartFormData startFormData = formService.getStartFormData(execution.getProcessDefinitionId());
-                for (FormProperty formProperty : startFormData.getFormProperties()) {
-                    if (formProperty.getId().equals(S_ID_ORDER_GOV_PUBLIC)) {
-                        LOG.info("Found start form property with the id " + S_ID_ORDER_GOV_PUBLIC + ". Setting value {}", processCount);
-                        if (formProperty instanceof FormPropertyImpl) {
-                            ((FormPropertyImpl) formProperty).setValue(processCount);
-                        }
-                    }
-                }
-            }
         }
 	}
 
