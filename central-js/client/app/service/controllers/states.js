4d4c569985f722c97cd211365215727262f7bcbe
antonlegkiy <anton.legkiy@gmail.com>
2016-08-08 15:14:01 +0300

    fix

--- central-js/client/app/service/controllers/states.js
+++ central-js/client/app/service/controllers/states.js
@@ -160,6 +160,20 @@ angular.module('app').controller('SituationController', function ($scope, AdminS
   $anchorScroll();
 });
 
+angular.module('app').directive('compileTemplate', function($compile, $parse){
+return {
+  link: function(scope, element, attr){
+    var parsed = $parse(attr.ngBindHtml);
+    function getStringValue() { return (parsed(scope) || '').toString(); }
+
+    //Recompile if the template changes
+    scope.$watch(getStringValue, function() {
+      $compile(element, null, -9999)(scope);  //The -9999 makes it skip directives so that we do not recompile ourselves
+    });
+  }
+}
+});
+
 angular.module('app').controller('ServiceGeneralController', function ($state, $scope, ServiceService, PlacesService) {
   PlacesService.resetPlaceData();
 
