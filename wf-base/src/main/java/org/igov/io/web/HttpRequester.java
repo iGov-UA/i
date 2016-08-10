02d22aeb753cca57e9415ffd3ef20315ba76ee8b
Dmytro Tsapko <tsapko.dmitry@gmail.com>
2016-08-11 02:11:33 +0300

    [issue #1335]: postInside fixed.

--- wf-base/src/main/java/org/igov/io/web/HttpRequester.java
+++ wf-base/src/main/java/org/igov/io/web/HttpRequester.java
@@ -77,9 +77,6 @@ public class HttpRequester {
             oConnection.setRequestProperty("authorization", "Basic " + sAuth);
 
             oConnection.setRequestMethod(RequestMethod.POST.name());
-            oConnection.setRequestProperty( "Content-Type", "application/x-www-form-urlencoded");
-            oConnection.setRequestProperty( "charset", "utf-8");
-            oConnection.setRequestProperty( "Content-Length", Integer.toString(saParam.getBytes().length));
             oConnection.setDoOutput(true);
             DataOutputStream oDataOutputStream = new DataOutputStream(oConnection.getOutputStream());
             // Send post request
