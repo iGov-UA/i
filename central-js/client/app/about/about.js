843f62243a094c9bfc8cd28d57d6324d2d90149a
julyprodan <klimkovichy@gmail.com>
2016-08-12 18:26:07 +0300

    добавлені участники и города по Одесской, Харьковской, Херсонской и Харьковской области. /https://alpha.test.igov.org.ua/about

--- central-js/client/app/about/about.js
+++ central-js/client/app/about/about.js
@@ -435,17 +435,7 @@ angular.module('about').controller('aboutController', function ($scope, $http) {
                 }]
             },
 
-            {
-              sID:"kherson",
-              sName: "Херсонська область",
-                a: [{
-                  sID: "kherson",
-                  sName: "м. Херсон",
-                  sCityPassportURL:"",
-                  sNewsGroupURL:"https://www.facebook.com/IGOV-%D0%9F%D0%BE%D1%80%D1%82%D0%B0%D0%BB-%D0%94%D0%B5%D1%80%D0%B6%D0%B0%D0%B2%D0%BD%D0%B8%D1%85-%D0%9F%D0%BE%D1%81%D0%BB%D1%83%D0%B3-%D0%A5%D0%B5%D1%80%D1%81%D0%BE%D0%BD-1732115370404292/?pnref=story",
-                  sVolunteersGroupURL:"https://groups.google.com/forum/?hl=ru#!forum/igov-kherson"
-                }]
-            },
+    
             {
               sID:"zaporizhya",
               sName: "Запорізька область",
