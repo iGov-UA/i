26785fca343a12e54c35b3cbe0661923b61d1fd3
Olia <Olia@olga>
2016-08-27 01:05:14 +0300

    fix

--- wf-region/src/main/java/org/igov/service/controller/ProcessController.java
+++ wf-region/src/main/java/org/igov/service/controller/ProcessController.java
@@ -254,7 +254,8 @@ public class ProcessController {
                 for (Map.Entry<String, String> removeOldProcessQuery : queryLoader.getRemoveOldProcessQueries().entrySet()) {
                     String removeOldProcessQueryValue;
                     if (removeOldProcessQuery.getKey().startsWith("update")) {
-                        removeOldProcessQueryValue = String.format(removeOldProcessQuery.getValue(), sID_Process_Def);
+                        removeOldProcessQueryValue = removeOldProcessQuery.getValue().replaceFirst("%s", sID_Process_Def);
+                        //removeOldProcessQueryValue = String.format(removeOldProcessQuery.getValue(), sID_Process_Def);
                         //removeOldProcessQueryValue = String.format(removeOldProcessQuery.getValue(), sID_Process_Def, sDateFinishAt, sDateFinishTo);
                     } else {
                         removeOldProcessQueryValue = removeOldProcessQuery.getValue();
