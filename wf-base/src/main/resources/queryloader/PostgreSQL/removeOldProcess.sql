36da77dcd7c54771b6be17435b17c7e153543b5b
Olia <Olia@olga>
2016-08-27 01:32:42 +0300

    fix

--- wf-base/src/main/resources/queryloader/PostgreSQL/removeOldProcess.sql
+++ wf-base/src/main/resources/queryloader/PostgreSQL/removeOldProcess.sql
@@ -4,8 +4,8 @@ update_act_hi_varinst;update "public"."act_hi_varinst" d set "proc_inst_id_" = '
 delete_act_hi_varinst;delete from "public"."act_hi_varinst" d where "proc_inst_id_" = '0'
 update_act_hi_attachment;update "public"."act_hi_attachment" d set "proc_inst_id_" = '0' from "public"."act_hi_procinst" p where p."proc_def_id_" like '%s%'  and p."proc_inst_id_" = d."proc_inst_id_"
 delete_act_hi_attachment;delete from "public"."act_hi_attachment" d where "proc_inst_id_" = '0'
-update_act_hi_comment;pdate "public"."act_hi_comment" d set "proc_inst_id_" = '0' from "public"."act_hi_procinst" p where p."proc_def_id_" like '%s%' and p."proc_inst_id_" = d."proc_inst_id_"
-delete_act_hi_comment;elete from "public"."act_hi_comment" d where "proc_inst_id_" = '0'
+update_act_hi_comment;update "public"."act_hi_comment" d set "proc_inst_id_" = '0' from "public"."act_hi_procinst" p where p."proc_def_id_" like '%s%' and p."proc_inst_id_" = d."proc_inst_id_"
+delete_act_hi_comment;delete from "public"."act_hi_comment" d where "proc_inst_id_" = '0'
 update_act_hi_identitylink;update "public"."act_hi_identitylink" d set "proc_inst_id_" = '0' from "public"."act_hi_procinst" p where p."proc_def_id_" like '%s%' and p."proc_inst_id_" = d."proc_inst_id_"
 delete_act_hi_identitylink;delete from "public"."act_hi_identitylink" d where "proc_inst_id_" = '0'
 update_act_hi_actinst;update "public"."act_hi_actinst" d set "proc_inst_id_" = '0' from "public"."act_hi_procinst" p where p."proc_def_id_" like '%s%' and p."proc_inst_id_" = d."proc_inst_id_"
