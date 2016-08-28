2ca7db44ac85626b43bc2e38349ebb215f419dff
Olia <Olia@olga>
2016-08-28 18:30:27 +0300

    delete process from db

--- wf-base/src/main/resources/queryloader/H2/removeOldProcess.sql
+++ wf-base/src/main/resources/queryloader/H2/removeOldProcess.sql
@@ -6,11 +6,11 @@ update_act_hi_attachment;update "public"."act_hi_attachment" d set "proc_inst_id
 delete_act_hi_attachment;delete from "public"."act_hi_attachment" d where "proc_inst_id_" = '0'
 update_act_hi_comment;update "public"."act_hi_comment" d set "proc_inst_id_" = '0' from "public"."act_hi_procinst" p where p."proc_def_id_" like '%s%' and p."start_time_" >= '%dateAt' and p."end_time_" <= '%dateTo' and p."proc_inst_id_" = d."proc_inst_id_"
 delete_act_hi_comment;delete from "public"."act_hi_comment" d where "proc_inst_id_" = '0'
-update_act_hi_identitylink;update "public"."act_hi_identitylink" d set "proc_inst_id_" = '0' from "public"."act_hi_procinst" p where p."proc_def_id_" like '%s%' and p."start_time_" >= '%dateAt' and p."end_time_" <= '%dateTo' and p."proc_inst_id_" = d."proc_inst_id_"
+update_act_hi_identitylink_byProc;update "public"."act_hi_identitylink" d set "proc_inst_id_" = '0' from "public"."act_hi_procinst" p where p."proc_def_id_" like '%s%' and p."start_time_" >= '%dateAt' and p."end_time_" <= '%dateTo' and p."proc_inst_id_" = d."proc_inst_id_"
+update_act_hi_identitylink_byTask;update "public"."act_hi_identitylink" d set "proc_inst_id_" = '0' from "public"."act_hi_procinst" p, "public"."act_hi_taskinst" t where p."proc_def_id_" like '%s%' and p."start_time_" >= '%dateAt' and p."end_time_" <= '%dateTo' and p."proc_inst_id_" = t."proc_inst_id_" and t."id_" = d."task_id_"
 delete_act_hi_identitylink;delete from "public"."act_hi_identitylink" d where "proc_inst_id_" = '0'
 update_act_hi_actinst;update "public"."act_hi_actinst" d set "proc_inst_id_" = '0' from "public"."act_hi_procinst" p where p."proc_def_id_" like '%s%' and p."start_time_" >= '%dateAt' and p."end_time_" <= '%dateTo' and p."proc_inst_id_" = d."proc_inst_id_"
 delete_act_hi_actinst;delete from "public"."act_hi_actinst" d where "proc_inst_id_" = '0'
 update_act_hi_taskinst;update "public"."act_hi_taskinst" d set "proc_inst_id_" = '0' from "public"."act_hi_procinst" p where p."proc_def_id_" like '%s%' and p."start_time_" >= '%dateAt' and p."end_time_" <= '%dateTo' and p."proc_inst_id_" = d."proc_inst_id_"
 delete_act_hi_taskinst;delete from "public"."act_hi_taskinst" d where "proc_inst_id_" = '0'
-delete_act_hi_procinst;delete from "public"."act_hi_procinst" where "proc_def_id_" like '%s%' and p."start_time_" >= '%dateAt' and p."end_time_" <= '%dateTo'
-delete_act_hi_procinst;delete from "public"."act_hi_procinst" p where "proc_def_id_" like '%s%' and p."start_time_" >= '%dateAt' and p."end_time_" <= '%dateTo'    
+delete_act_hi_procinst;delete from "public"."act_hi_procinst" p where "proc_def_id_" like '%s%' and p."start_time_" >= '%dateAt' and p."end_time_" <= '%dateTo'
