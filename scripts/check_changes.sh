b1ee6144c64f604331b9c3ca553e98d49e0a329b
Dimiys <Dimiys@ua.FM>
2016-08-29 13:35:45 +0300

    Create check_changes.sh

---
+++ scripts/check_changes.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+source="app.lst"
+
+get_change()
+{
+app=$1
+new=$(find ./$app -type f  -printf '%TY-%Tm-%Td %TT %p\n' | sort -r | head -n 1 | awk '{print $1, $2}') >> last_change
+echo $new >> last_change_$app
+
+
+while read sLine; do
+# if [[ -z $sLine ]] || [[ "$sLine" == "" ]] || [[ "$sLine" =~ ^#.* ]]; then continue; fi
+   echo $sLine 
+   get_change $sLine
+ done < $source 
+exit 0
