851e1e105323ec70a50ea2f23b4dc69cacbc41bc
Dimiys <Dimiys@ua.FM>
2016-08-29 16:49:47 +0300

    Update check_changes.sh

--- scripts/check_changes.sh
+++ scripts/check_changes.sh
@@ -3,41 +3,67 @@ source="scripts/config/app.lst"
 
 array=$(cat scripts/config/app.lst | awk '{print $2}')
 echo $array
-get_change()
-{
-app=$1
-USER=$3
-TOKEN=$4
-#array=$2
-new=$(find ./$app -type f  -printf '%TY-%Tm-%Td %TT %p\n' | sort -r | head -n 1 | awk '{print $1, $2}') >> last_change
-echo $new >> last_change_new_$app
-DIFF=$(diff -lq last_change_$app last_change_new_$app)
+get_change() {
+ app=$1
+ USER=$3
+ TOKEN=$4
+ new=$(find ./$app -type f  -printf '%TY-%Tm-%Td %TT %p\n' | sort -r | head -n 1 | awk '{print $1, $2}') >> last_change
+ echo $new >> last_change_new_$app
+ DIFF=$(diff -lq last_change_$app last_change_new_$app)
 for x in ${array[@]}
-do
+  do
 if [[ "$DIFF" != "" ]]; then
    if [[ "$app" == "central-js" ]]; then
-    echo 'change in Central-front'
+    echo 'change in' ${array[@]}'_Central-front'
     curl -k -XPOST --user $USER":"$TOKEN "https://ci-jenkins.tech.igov.org.ua/job/"$x"_Front_Central/buildWithParameters?delay=0sec" 
+     if [[ "$x" == "test_alpha-old" ]]; then
+      TOKEN=$5
+      curl -k -XPOST --user $USER":"$TOKEN "https://ci-jenkins-backup.tech.igov.org.ua/job/"$x"_Front_Central/buildWithParameters?delay=0sec"
+      echo 'change in' $x'_Central-front'
+     fi
+
    elif [[ "$app" == "dashboard-js" ]]; then
-      echo 'change in Region-front'
+      echo 'change in' $x'_Region-front'
       curl -k -XPOST --user $USER":"$TOKEN "https://ci-jenkins.tech.igov.org.ua/job/"$x"_Front_Region/buildWithParameters?delay=0sec" 
+             if [[ "$x" == "test_alpha-old" ]]; then
+               TOKEN=$5
+               curl -k -XPOST --user $USER":"$TOKEN "https://ci-jenkins-backup.tech.igov.org.ua/job/"$x"_Front_Region/buildWithParameters?delay=0sec"
+               echo 'change in' $x'_Region-front'
+             fi
+
         elif [[ "$app" == "wf-base" ||  "$app" == "storage-static" ||  "$app" == "storage-temp" ]]; then
-           echo 'change in Back'
+           echo 'change in' $x'_Back'
            touch no
            curl -k -XPOST --user $USER":"$TOKEN "https://ci-jenkins.tech.igov.org.ua/job/"$x"_Back/buildWithParameters?delay=0sec"
+                  if [[ "$x" == "test_alpha-old" ]]; then
+                     TOKEN=$5
+                     curl -k -XPOST --user $USER":"$TOKEN "https://ci-jenkins-backup.tech.igov.org.ua/job/"$x"_Back/buildWithParameters?delay=0sec"
+                     echo 'change in' $x'_Back'
+                  fi
+
            elif [[ "$app" == "wf-central" || ! -f no ]]; then
-              echo "change in Back-Central"
-              curl -k -XPOST --user $USER":"$TOKEN "https://ci-jenkins.tech.igov.org.ua/job/"$x"_Back_Central/buildWithParameters?delay=0sec" 
+              echo 'change in' $x'_Central-Back'
+             curl -k -XPOST --user $USER":"$TOKEN "https://ci-jenkins.tech.igov.org.ua/job/"$x"_Back_Central/buildWithParameters?delay=0sec" 
+                            if [[ "$x" == "test_alpha-old" ]]; then
+                              TOKEN=$5
+                              curl -k -XPOST --user $USER":"$TOKEN "https://ci-jenkins-backup.tech.igov.org.ua/job/"$x"_Back_Central/buildWithParameters?delay=0sec"
+                              echo 'change in' $x'_Central-front'
+                            fi
+
                     elif [[ "$app" == "wf-region" || ! -f no ]]; then
-#                        TOKEN=$5
-                        echo "change in Back-Region"
-                       curl -k -XPOST --user $USER":"$TOKEN "https://ci-jenkins.tech.igov.org.ua/job/"$x"_Back_Region/buildWithParameters?delay=0sec"
-     fi
-else
-echo "no change"
+                        echo 'change in' $x'_Region-Back'
+                  #     curl -k -XPOST --user $USER":"$TOKEN "https://ci-jenkins.tech.igov.org.ua/job/"$x"_Back_Region/buildWithParameters?delay=0sec"
+                              if [[ "$x" == "test_alpha-old" ]]; then
+                                TOKEN=$5
+                                curl -k -XPOST --user $USER":"$TOKEN "https://ci-jenkins-backup.tech.igov.org.ua/job/"$x"_Front_Central/buildWithParameters?delay=0sec"
+                                echo 'change in' $x'_Region-Back'
+                              fi
+      fi
+   else
+   echo "no change"
 fi
 done
-#echo "ok"
+
 mv last_change_new_$app last_change_$app
 }
 
