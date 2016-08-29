b59c1e0cc79bda950a1b5d2198cdc9b8f1777519
Dimiys <Dimiys@ua.FM>
2016-08-29 13:59:29 +0300

    Update check_changes.sh

--- scripts/check_changes.sh
+++ scripts/check_changes.sh
@@ -7,7 +7,7 @@ get_change()
 app=$1
 new=$(find ./$app -type f  -printf '%TY-%Tm-%Td %TT %p\n' | sort -r | head -n 1 | awk '{print $1, $2}') >> last_change
 echo $new >> last_change_$app
-
+}
 
 while read sLine; do
 # if [[ -z $sLine ]] || [[ "$sLine" == "" ]] || [[ "$sLine" =~ ^#.* ]]; then continue; fi
