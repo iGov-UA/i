d2a3176c5ce26181c7f7bdc634d3afcb1f9b92d6
Dimiys <Dimiys@ua.FM>
2016-08-31 17:52:43 +0300

    Update delta_check_changes.sh

--- scripts/delta_check_changes.sh
+++ scripts/delta_check_changes.sh
@@ -8,11 +8,11 @@ get_change() {
  TOKEN=$3
  sJob=$4
  sHost=test_delta
- new=$(find ./$sApp -type f  -printf '%TY-%Tm-%Td %TT %p\n' | sort -r | head -n 1 | awk '{print $1, $2}') >> last_change
+ new=$(find ./$sApp -type f  -printf '%TY-%Tm-%Td %TT %p\n' | sort -r | head -n 1 | awk '{print $1, $2}')
  echo $new >> last_change_new_$sApp
  DIFF=$(diff -lq last_change_$sApp last_change_new_$sApp)
   if [[ "$DIFF" != "" ]]; then
-     if [[ '$sApp' == "central-js" ]] || [[ '$sApp' == "dashboard-js" ]]; then
+     if [[ "$sApp" == "central-js" ]] || [[ "$sApp" == "dashboard-js" ]]; then
      curl -k -XPOST --user $USER":"$TOKEN "https://ci-jenkins.tech.igov.org.ua/job/"$sHost"_"$sJob"/buildWithParameters?delay=0sec" 
      echo 'Start Job' $sHost'_'$sJob
          elif [[ "$sApp" == "wf-base" ]] || [[ "$sApp" == "storage-static" ]] || [[ "$sApp" == "storage-temp" ]]; then
@@ -31,7 +31,7 @@ mv last_change_new_$sApp last_change_$sApp
 rm no
 
  while read sLine; do
- if [[ -z $sLine ]] || [[ "$sLine" == "" ]] || [[ "$sLine" =~ ^#.* ]]; then continue; fi
+# if [[ -z $sLine ]] || [[ "$sLine" == "" ]] || [[ "$sLine" =~ ^#.* ]]; then continue; fi
     get_change $sLine
  done < $sSource 
 exit 0
