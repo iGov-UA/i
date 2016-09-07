9fe2243c333299dfb9a6d9c87de1661753a4ce2b
Dmytro Tsapko <tsapko.dmitry@gmail.com>
2016-09-08 00:21:07 +0300

    [issue #1351]: /getServicesStatistic query offloaded to filed and QueryLoader is used.

---
+++ wf-central/src/main/resources/queryloader/PostgreSQL/get_ServicesStatistics.sql
@@ -0,0 +1,9 @@
+select min(hes."nID_Service") AS nID, hes."nID_Service" AS nID_Service, min(s."sName") AS ServiceName,
+       hes."sID_UA" AS SID_UA, min(p."sName") AS placeName, count(*) AS nCountTotal,
+       avg(hes."nRate") AS averageRate, avg(hes."nTimeMinutes") AS averageTime
+from "HistoryEvent_Service" AS hes, "Service" AS s, "Place" AS p
+where s."nID" = hes."nID_Service"
+      and p."sID_UA" = hes."sID_UA"
+      and hes."sDate" >= to_timestamp( :dateFrom , 'YYYY-MM-DD hh24:mi:ss')
+      and hes."sDate" < to_timestamp( :dateTo  , 'YYYY-MM-DD hh24:mi:ss')
+group by hes."nID_Service", hes."sID_UA"
