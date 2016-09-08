85479fa4f64f351f86d272775db2c991053ea2d4
Dashenko-Inna <dascenkoinna@gmail.com>
2016-09-08 15:58:35 +0300

    issues/1340
    Signed-off-by: Dashenko-Inna <dascenkoinna@gmail.com>

---
+++ analytic/src/main/java/org/igov/analytic/model/attribute/AttributeName.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.igov.analytic.model.attribute;
+
+import org.igov.model.core.NamedEntity;
+
+/**
+ *
+ * @author olga
+ */
+@javax.persistence.Entity
+public class AttributeName extends NamedEntity{ 
+    
+}
