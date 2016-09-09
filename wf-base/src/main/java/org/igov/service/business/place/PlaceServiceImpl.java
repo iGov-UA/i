463aa1fb6554444b9d3408666e1502926daae824
HS <HS@192.168.0.214>
2016-09-09 18:24:15 +0300

    Place #1350

--- wf-base/src/main/java/org/igov/service/business/place/PlaceServiceImpl.java
+++ wf-base/src/main/java/org/igov/service/business/place/PlaceServiceImpl.java
@@ -9,7 +9,6 @@ import java.util.HashMap;
 import java.util.Map;
 import org.igov.io.GeneralConfig;
 import org.igov.io.web.HttpRequester;
-import org.igov.service.business.access.AccessDataService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,12 +21,10 @@ import org.springframework.stereotype.Service;
 @Service
 public class PlaceServiceImpl implements PlaceService {
 
-     private static final Logger LOG = LoggerFactory.getLogger(PlaceServiceImpl.class);
+    private static final Logger LOG = LoggerFactory.getLogger(PlaceServiceImpl.class);
     private final String URI_GET_PLACE_BY_PROCESS = "/wf/service/action/event/getPlaceByProcess";
     @Autowired
     private HttpRequester httpRequester;
-    @Autowired
-    private AccessDataService accessDataDao;
     @Autowired
     private GeneralConfig generalConfig;
 
@@ -39,23 +36,8 @@ public class PlaceServiceImpl implements PlaceService {
         return doRemoteRequest(URI_GET_PLACE_BY_PROCESS, params);
     }
 
-    public String doRemoteRequest(String sURL, Map<String, String> mParam, String nID_Process, String sUserTaskName)
-            throws Exception {
-        mParam.put("nID_Process", nID_Process);
-        if (sUserTaskName != null) {
-            mParam.put("sUserTaskName", sUserTaskName);
-        }
-        return doRemoteRequest(sURL, mParam);
+    private String doRemoteRequest(String sURL, Map<String, String> mParam) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    private String doRemoteRequest(String sServiceContext, Map<String, String> mParam) throws Exception {
-        String soResponse = "";
-        if (!generalConfig.getSelfHostCentral().contains("ksds.nads.gov.ua") && !generalConfig.getSelfHostCentral().contains("staff.igov.org.ua")) {
-            String sURL = generalConfig.getSelfHostCentral() + sServiceContext;
-            LOG.info("(sURL={},mParam={})", sURL, mParam);
-            soResponse = httpRequester.getInside(sURL, mParam);
-            LOG.info("(soResponse={})", soResponse);
-        }
-        return soResponse;
-    }
 }
