e23760aef535788f009aa44774a684d44eac2a3f
antonlegkiy <anton.legkiy@gmail.com>
2016-09-19 15:13:36 +0300

    added select & autocomplete fields, fix validation, #1354

--- central-js/client/app/common/components/form/directives/dropdownOrgan/dropdownOrgan.js
+++ central-js/client/app/common/components/form/directives/dropdownOrgan/dropdownOrgan.js
@@ -144,7 +144,7 @@ angular.module('app').directive('dropdownOrgan', function (OrganListFactory, $ht
 
       angular.forEach(Object.keys(scope.formData.params), function (key) {
         scope.$watch('formData.params.' + key + '.value', function () {
-          if (scope.ngModel !== null && scope.ngModel !== '0' && scope.ngModel.length > 0 && !attributesApplying){
+          if (scope.ngModel !== null && scope.ngModel !== undefined && scope.ngModel !== '0' && scope.ngModel.length > 0 && !attributesApplying){
               loadAttributesData(key);
           }
         });
