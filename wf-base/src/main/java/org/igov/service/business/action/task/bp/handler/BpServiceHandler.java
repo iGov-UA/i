cfdb32656f935c686c13924170e26701e43713b4
HS <HS@192.168.0.214>
2016-09-19 21:02:07 +0300

    fix place for escalation

--- wf-base/src/main/java/org/igov/service/business/action/task/bp/handler/BpServiceHandler.java
+++ wf-base/src/main/java/org/igov/service/business/action/task/bp/handler/BpServiceHandler.java
@@ -177,9 +177,9 @@ public class BpServiceHandler {
         mGuideTaskParamKey.put("email", "email"); 
         mParam.put("phone", "" + mTaskParam.get("phone"));
         mGuideTaskParamKey.put("phone", "Контактний телефон громадянина");
-        mParam.put("Place", getPlaceForProcess("sID_Process"));   
+        mParam.put("Place", getPlaceByProcess("sID_Process"));   
         mGuideTaskParamKey.put("Place", "Обраний населений пункт");
-        LOG.info("1111111111111111111111111111getPlaceByProcess(sID_Process): " + getPlaceForProcess("sID_Process") + " sID_Process: " + sID_Process);
+        LOG.info("1111111111111111111111111111getPlaceByProcess(sID_Process): " + getPlaceByProcess("sID_Process") + " sID_Process: " + sID_Process);
         // mParam.put("email", mTaskParam.get("email"));
         Map mTaskParamConverted = convertTaskParam(mTaskParam);
         String sField = convertTaskParamToString(mTaskParamConverted);
