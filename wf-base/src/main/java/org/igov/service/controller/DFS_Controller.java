726ce240aa4d5ad3ab78faec98b6c256d6d9228f
olga <olga@olga-pc>
2016-09-20 11:17:01 +0300

    add service

--- wf-base/src/main/java/org/igov/service/controller/DFS_Controller.java
+++ wf-base/src/main/java/org/igov/service/controller/DFS_Controller.java
@@ -1,8 +1,11 @@
 package org.igov.service.controller;
 
+import com.google.common.io.Files;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import java.io.File;
 import java.io.StringWriter;
+import java.nio.charset.Charset;
 import java.util.Map;
 import javax.servlet.http.HttpServletResponse;
 import javax.xml.bind.JAXBContext;
@@ -11,6 +14,7 @@ import javax.xml.bind.Marshaller;
 import org.igov.io.GeneralConfig;
 import org.igov.io.db.kv.temp.IBytesDataInmemoryStorage;
 import org.igov.io.db.kv.temp.model.ByteArrayMultipartFile;
+import org.igov.io.fs.FileSystemData;
 import org.igov.service.business.action.task.core.AbstractModelTask;
 import org.igov.util.swind.jaxb.DBody;
 import org.igov.util.swind.jaxb.DHead;
@@ -23,6 +27,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -35,6 +40,39 @@ public class DFS_Controller {
 
     @Autowired
     private IBytesDataInmemoryStorage oBytesDataInmemoryStorage;
+
+    @ApiOperation(value = "/putAttachmentToRedis_new", notes = "##### Контроллер платежей. Регистрация проведенного платежа - по колбэку от платежной системы\n")
+    @RequestMapping(value = {"/putAttachmentToRedis_new"}, method = RequestMethod.POST, headers = {"Accept=application/json"})
+    public @ResponseBody
+    byte[] putAttachmentToRedis_new(@RequestBody(required = false) Map<String, String> data,
+            @RequestParam(required = false) String sPathFile,
+            HttpServletResponse httpResponse) throws Exception {
+        String declarContent = null;
+        //генерируем хмл в виде файла и проставляем значения в него. отдаем на клиента
+        LOG.info("data: " + data);
+        //считываем контент файла
+        //вставляем значения в него
+        File file = FileSystemData.getFile(FileSystemData.SUB_PATH_XML, sPathFile);
+        declarContent = Files.toString(file, Charset.defaultCharset());
+        LOG.info("Created document with customer info to embedd to message: {}", declarContent);
+        String regex, replacement;
+        for (Map.Entry<String, String> entry : data.entrySet()) {
+            regex = "<" + entry.getKey().trim() + ">";
+            replacement = regex + entry.getValue();
+            declarContent = declarContent.replaceAll(regex, replacement);
+        }
+        //запись контента в xml файл
+        MultipartFile multipartFile = new ByteArrayMultipartFile(declarContent.getBytes(),
+                "SwinEd", "SwinEd", "application/xml");
+        httpResponse.setHeader("Content-disposition", "attachment; filename=" + multipartFile.getName());
+        httpResponse.setHeader("Content-Type", multipartFile.getContentType());
+        httpResponse.setContentLength(declarContent.getBytes().length);
+        String key = oBytesDataInmemoryStorage.putBytes(AbstractModelTask
+                .multipartFileToByteArray(multipartFile, multipartFile.getOriginalFilename())
+                .toByteArray());
+        return declarContent.getBytes();
+        //return declarContent.getBytes();
+    }
 
     @ApiOperation(value = "/putAttachmentToRedis", notes = "##### Контроллер платежей. Регистрация проведенного платежа - по колбэку от платежной системы\n")
     @RequestMapping(value = {"/putAttachmentToRedis"}, method = RequestMethod.POST, headers = {"Accept=application/json"})
@@ -93,14 +131,14 @@ public class DFS_Controller {
         String declarContent = sw.toString();
         LOG.info("Created document with customer info to embedd to message: {}", declarContent);
         //запись контента в xml файл
-        MultipartFile file = new ByteArrayMultipartFile(declarContent.getBytes(), 
+        MultipartFile file = new ByteArrayMultipartFile(declarContent.getBytes(),
                 "SwinEd", "SwinEd", "application/xml");
         httpResponse.setHeader("Content-disposition", "attachment; filename=" + file.getName());
         httpResponse.setHeader("Content-Type", file.getContentType());
         httpResponse.setContentLength(declarContent.getBytes().length);
         String key = oBytesDataInmemoryStorage.putBytes(AbstractModelTask
-                    .multipartFileToByteArray(file, file.getOriginalFilename())
-                    .toByteArray());
+                .multipartFileToByteArray(file, file.getOriginalFilename())
+                .toByteArray());
         return key + " " + declarContent;
         //return declarContent.getBytes();
     }
