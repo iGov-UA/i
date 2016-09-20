a801772c4e7da93bfb22305126d5a518de0ea83c
Oleksii Khalikov <oleksii.khalikov@gmail.com>
2016-09-20 03:19:59 +0300

    test getSubjectMessageDataTest

--- wf-central/src/main/java/org/igov/service/controller/DebugCentralController.java
+++ wf-central/src/main/java/org/igov/service/controller/DebugCentralController.java
@@ -2,6 +2,7 @@ package org.igov.service.controller;
 
 import com.google.common.base.*;
 import com.google.common.base.Objects;
+import com.google.common.base.Optional;
 import io.swagger.annotations.*;
 import org.activiti.engine.HistoryService;
 import org.activiti.engine.history.HistoricIdentityLink;
@@ -456,125 +457,69 @@ public class DebugCentralController {
             + "Пример:\n"
             + "https://test.igov.org.ua/wf/service/subject/message/getSubjectMessageData?nID_SubjectMessage=111111")
     @RequestMapping(value = "/test/getSubjectMessageDataTesting", method = { RequestMethod.GET })
-    public @ResponseBody String getSubjectMessageData(
+    public
+    @ResponseBody
+    void getSubjectMessageData(
             @ApiParam(value = "номер-ИД записи с сообщением", required = false) @RequestParam(value = "nID_SubjectMessage", required = true) String sID_SubjectMessage,
             @ApiParam(value = "Номер-ИД субьекта (хозяина заявки сообщения)", required = false) @RequestParam(value = "nID_Subject", required = false) Long nID_Subject,
             @ApiParam(value = "булевский флаг, Включить авторизацию", required = false) @RequestParam(value = "bAuth", required = false, defaultValue = "false") Boolean bAuth,
             HttpServletResponse httpResponse) throws CommonServiceException{
 
-
-
-        Map<String, String> result = new HashMap<>();
-
         //content of the message file
         String res = null;
-        Long nID_SubjectMessage = 0L;
-        try {
-            nID_SubjectMessage = Long.valueOf(sID_SubjectMessage);
-            result.put("472 nID_SubjectMessage)", nID_SubjectMessage.toString());
-        } catch (Exception e){
-            result.put("474 Exception nID_SubjectMessage)", e.getMessage());
-        }
-        Long nID_HistoryEvent_Service = 0L;
-        SubjectMessage message = null;
+        //try{
+        Long nID_SubjectMessage = Long.valueOf(sID_SubjectMessage);
+        SubjectMessage message = subjectMessagesDao.getMessage(nID_SubjectMessage);
+        Long nID_HistoryEvent_Service = message.getnID_HistoryEvent_Service();
 
-        try{
-            message = subjectMessagesDao.getMessage(nID_SubjectMessage);
-            result.put("481 subjectMessagesDao.getMessage(nID_SubjectMessage)", message.toString());
-            try {
-                nID_HistoryEvent_Service = message.getnID_HistoryEvent_Service();
-                result.put("484 nID_HistoryEvent_Service)", nID_HistoryEvent_Service.toString());
-            } catch (Exception e){
-                result.put("486 Exception nID_HistoryEvent_Service)", e.getMessage());
-            }
-
-        } catch (Exception e){
-            result.put("490 Exception subjectMessagesDao.getMessage(nID_SubjectMessage)", e.getMessage());
-        }
-
-
-
-        com.google.common.base.Optional<HistoryEvent_Service> oHistoryEvent_Service = null;
-
-        try {
-            oHistoryEvent_Service = historyEventServiceDao.findById(nID_HistoryEvent_Service);
-            result.put("499 historyEventServiceDao.findById(nID_HistoryEvent_Service)", oHistoryEvent_Service.toString());
-        }catch (Exception e){
-            result.put("501 Exception historyEventServiceDao.findById(nID_HistoryEvent_Service)", e.getMessage());
-        }
-
-        try {
-            oHistoryEvent_Service.isPresent();
-            result.put("506 oHistoryEvent_Service.isPresent()", (new Boolean(oHistoryEvent_Service.isPresent())).toString());
-        } catch (Exception e){
-            result.put("508 Exception oHistoryEvent_Service.isPresent()", e.getMessage());
-        }
+        Optional<HistoryEvent_Service> oHistoryEvent_Service = historyEventServiceDao.findById(nID_HistoryEvent_Service);
 
         if(bAuth && oHistoryEvent_Service.isPresent()){
-            result.put("512 bAuth && oHistoryEvent_Service.isPresent() is", (new Boolean(bAuth && oHistoryEvent_Service.isPresent())).toString());
             try {
                 actionEventService.checkAuth(oHistoryEvent_Service.get(), nID_Subject, oHistoryEvent_Service.get().getsToken());
-                result.put("515 actionEventService.checkAuth", "ok");
             } catch (Exception e) {
-                result.put("517 Exception actionEventService.checkAuth", e.getMessage());
+                LOG.error("Exception actionEventService.checkAuth: " + e.getMessage());
             }
         }
-
         if(message == null || isBlank(message.getsID_DataLink())){
-            result.put("522 message == null || isBlank(message.getsID_DataLink()) is", (new Boolean(message == null || isBlank(message.getsID_DataLink()))).toString());
             LOG.info("Message is not found by nID_Message {}", nID_SubjectMessage);
             CommonServiceException newErr = new CommonServiceException(ExceptionCommonController.BUSINESS_ERROR_CODE, "Record not found");
             throw newErr;
         }
         LOG.info("Message is recieved by nID_Message {}", nID_SubjectMessage);
-        result.put("528 Message is recieved by nID_Message", nID_SubjectMessage.toString());
-
-        try {
-            isNotBlank(message.getsID_DataLink());
-            result.put("532 isNotBlank(message.getsID_DataLink())", "ok");
-        } catch (Exception e){
-            result.put("534 Exception isNotBlank(message.getsID_DataLink())",  e.getMessage());
-        }
 
         if (isNotBlank(message.getsID_DataLink())){
             LOG.info("Field sID_DataLink in message is not null");
-            result.put("539 isNotBlank(message.getsID_DataLink())", (new Boolean(isNotBlank(message.getsID_DataLink()))).toString());
 
-            byte[] resBytes = null;
-            try {
-                resBytes = durableBytesDataStorage.getData(message.getsID_DataLink());
-                result.put("544 resBytes", "ok");
-            } catch (Exception e){
-                result.put("546 Exception resBytes",  e.getMessage());
+            byte[] resBytes = durableBytesDataStorage.getData(message.getsID_DataLink());
+            if(resBytes == null){
+                LOG.error("durableBytesDataStorage.getData(message.getsID_DataLink()) is NULL");
             }
 
-            try {
-                httpResponse.setHeader("Content-Type", "text/html;charset=UTF-8");
-                result.put("551 httpResponse.setHeader", "ok");
-                res = new String(resBytes, Charset.forName("UTF-8"));
-                result.put("553 res = new String", "ok");
-            } catch (Exception e){
-                result.put("555 Exception httpResponse.setHeader",  e.getMessage());
-            }
-
+            httpResponse.setHeader("Content-Type", "text/html;charset=UTF-8");
+            res = new String(resBytes, Charset.forName("UTF-8"));
 
             try {
                 httpResponse.getWriter().print(res);
-                result.put("561 httpResponse.getWriter().print(res)",  "ok");
             } catch (IOException e) {
-                result.put("563 Exception httpResponse.getWriter().print(res)",  e.getMessage());
+                LOG.error("Exception httpResponse.getWriter().print(res): " + e.getMessage());;
             }
             try {
                 httpResponse.getWriter().close();
-                result.put("567 httpResponse.getWriter().close()",  "ok");
             } catch (IOException e) {
-                result.put("569 Exception httpResponse.getWriter().close()",  e.getMessage());
+                LOG.error("Exception httpResponse.getWriter().close(): " + e.getMessage());
             }
         }
-        result.put("572 Controller",  "ok");
-
-        return JSONValue.toJSONString(result);
-        
+    		/*}catch(Exception e){
+    			if(e instanceof CommonServiceException)
+    				throw (CommonServiceException)e;
+    			else
+    			{
+    				LOG.error("FAIL: {}", e.getMessage());
+    	            LOG.trace("FAIL:", e);
+    	            throw new CommonServiceException(500, "Unknown exception: " + e.getMessage());
+    			}
+    		}*/
     }
 
 }
