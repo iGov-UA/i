4b8116e230ab9d71a40110ab12e0a530bfb7a4cb
HS <HS@192.168.0.214>
2016-09-21 17:31:01 +0300

    fix

--- wf-base/src/main/java/org/igov/service/business/escalation/EscalationService.java
+++ wf-base/src/main/java/org/igov/service/business/escalation/EscalationService.java
@@ -45,7 +45,7 @@ public class EscalationService {
     GeneralConfig oGeneralConfig;
 
     private static final String SEARCH_DELAYED_TASKS_URL = "/wf/service/action/task/getStartFormData?nID_Task=";// /task-activiti/
-    private static final String ORDER_HISTORY_URL = "/wf/search?sID_Order="; // #1350 п.11
+    private static final String ORDER_HISTORY_URL = "/wf/search?sID_Order="; // #1350 п.11 <a href="URL">текст ссылки</a>
     //private static final String REGIONAL_SERVER_PATH = "https://region.org.gov.ua";
 
     @Autowired
@@ -127,8 +127,8 @@ public class EscalationService {
                     onID_Task = mTaskParam.get("nID_task_activiti");
                     mTaskParam.put("processLink", regionalServerPath + SEARCH_DELAYED_TASKS_URL + onID_Task);
                     BpServiceHandler.mGuideTaskParamKey.put("processLink", "Удалить");
-                    mTaskParam.put("sURL_OrderHistory", ORDER_HISTORY_URL + oTask.getParentTaskId());
-                    LOG.info("!!!!!!!!!!!!!!!regionalServerPath + ORDER_HISTORY_URL + oTask.getParentTaskId(): "+regionalServerPath + ORDER_HISTORY_URL + oTask.getParentTaskId());
+                    mTaskParam.put("sURL_OrderHistory", "<a href="+ ORDER_HISTORY_URL + oTask.getParentTaskId()+">[nID_Protected]</a>");
+                    LOG.info("!!!!!!!!!!!!!!!regionalServerPath + ORDER_HISTORY_URL + oTask.getParentTaskId(): "+regionalServerPath + ORDER_HISTORY_URL + oTask.getParentTaskId())+"regionalServerPath: "+regionalServerPath;
                     mTaskParam.put("nID_EscalationRule", oEscalationRule.getId());
                     BpServiceHandler.mGuideTaskParamKey.put("nID_EscalationRule", "ИД эскалации правила");
                     //                LOG.info("checkTaskOnEscalation (mTaskParam={})", mTaskParam);
