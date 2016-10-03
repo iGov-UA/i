eab962f44efc98ee7d14d1ed2cee4c7b2615043b
HS <HS@192.168.0.214>
2016-10-04 01:32:37 +0300

    change name2

--- wf-base/src/main/java/org/igov/run/schedule/JobsInitializer.java
+++ wf-base/src/main/java/org/igov/run/schedule/JobsInitializer.java
@@ -42,8 +42,8 @@ public class JobsInitializer implements InitializingBean, ApplicationContextAwar
     @Override
     public void afterPropertiesSet() throws Exception {
         addEscalationJob(scheduler);
-//        addFeedBackJob(scheduler);
-//        addBuilderFlowSlotsJob(scheduler);
+        addFeedBackJob(scheduler);
+        addBuilderFlowSlotsJob(scheduler);
     }
 
     private void addEscalationJob(Scheduler scheduler) throws SchedulerException {
