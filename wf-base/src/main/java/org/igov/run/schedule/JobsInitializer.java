f26ce0e78536d2fc52e64a957442dc47512afeda
HS <HS@192.168.0.214>
2016-10-04 10:27:35 +0300

    bacak time on 02 00 am

--- wf-base/src/main/java/org/igov/run/schedule/JobsInitializer.java
+++ wf-base/src/main/java/org/igov/run/schedule/JobsInitializer.java
@@ -41,7 +41,7 @@ public class JobsInitializer implements InitializingBean, ApplicationContextAwar
 
     @Override
     public void afterPropertiesSet() throws Exception {
-//        addEscalationJob(scheduler);
+        addEscalationJob(scheduler);
         addFeedBackJob(scheduler);
         addBuilderFlowSlotsJob(scheduler);
     }
@@ -101,7 +101,7 @@ public class JobsInitializer implements InitializingBean, ApplicationContextAwar
                 "oCronTrigger_EveryNight_BuilderFlowSlotsJobGroup");
         try {
             LOG.info("oCronExpression__EveryNight_Deep...");
-            CronExpression oCronExpression__EveryNight_Deep = new CronExpression("0 25 10 1/1 * ?");
+            CronExpression oCronExpression__EveryNight_Deep = new CronExpression("0 0 2 1/1 * ?");
             LOG.info("oCronExpression__EveryNight_Deep.setCronExpression...");
             oCronTrigger_EveryNight_Deep.setCronExpression(oCronExpression__EveryNight_Deep);
         } catch (Exception oException) {
