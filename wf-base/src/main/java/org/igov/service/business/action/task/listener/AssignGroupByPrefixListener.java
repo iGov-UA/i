7d353b8fbc653a046091b697ef23c0339bc16850
Kuhtin Sergey <kuhtin.sv@gmail.com>
2016-10-04 17:18:39 +0300

    refactoring after test

--- wf-base/src/main/java/org/igov/service/business/action/task/listener/AssignGroupByPrefixListener.java
+++ wf-base/src/main/java/org/igov/service/business/action/task/listener/AssignGroupByPrefixListener.java
@@ -1,9 +1,7 @@
 package org.igov.service.business.action.task.listener;
 
 import java.util.List;
-import java.util.Optional;
 import org.activiti.engine.ActivitiIllegalArgumentException;
-import org.activiti.engine.RuntimeService;
 import org.activiti.engine.delegate.DelegateExecution;
 import org.activiti.engine.delegate.DelegateTask;
 import org.activiti.engine.delegate.Expression;
@@ -13,7 +11,6 @@ import org.springframework.stereotype.Component;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import static org.igov.service.business.action.task.core.AbstractModelTask.getStringFromFieldExpression;
-import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  *
@@ -23,28 +20,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 public class AssignGroupByPrefixListener implements TaskListener {
 
     private static final transient Logger LOG = LoggerFactory.getLogger(AssignGroupByPrefixListener.class);
-    //private Expression prefix;
     private Expression resultField_Group;
     private Expression suffix_chief;
     private Expression suffix_empolyee;
 
-    @Autowired
-    private RuntimeService runtimeService;
-
     @Override
     public void notify(DelegateTask delegateTask) {
         DelegateExecution execution = delegateTask.getExecution();
-        //String prefix = getStringFromFieldExpression(this.prefix, execution);
-        //LOG.info("prefix: " + prefix);
         String chief = getStringFromFieldExpression(this.suffix_chief, execution);
         LOG.info("suffix_chief: " + chief);
         String employee = getStringFromFieldExpression(this.suffix_empolyee, execution);
         LOG.info("suffix_empolyee: " + employee);
         String resultField_Group = getStringFromFieldExpression(this.resultField_Group, execution);
         LOG.info("resultField_Group: " + resultField_Group);
+        
         // getting TaskOwner's user
         String sTaskOwner = delegateTask.getAssignee();
         LOG.info("TaskOwner: " + sTaskOwner);
+        
         // List of TaskOwner's group
         List<Group> ownerGroup = execution.getEngineServices().getIdentityService().createGroupQuery()
                 .groupMember(sTaskOwner).list();
@@ -53,14 +46,16 @@ public class AssignGroupByPrefixListener implements TaskListener {
         LOG.info(String.format("Count of groups like %s is %s", chief, nCountOfGroups));
 
         if (nCountOfGroups != 1) {
-            LOG.warn(String.format("Groups $s for users like boss are absent", chief));
-            throw new ActivitiIllegalArgumentException(String.format("Групи $s для співробітника не існує, або їх декілька", chief));
+            LOG.warn(String.format("Groups %s for users like boss are absent", chief));
+            throw new ActivitiIllegalArgumentException(String.format("Групи %s для співробітника не існує, або їх декілька", chief));
         }
         
         Group oEmployeesGroup = ownerGroup.stream()
                 .filter(group -> group.getId().endsWith(chief))
                 .findFirst()
                 .get();
+        
+        //Employees group
         String sEmploeesGroup = oEmployeesGroup.getId().replace(chief, employee);
         LOG.info("sEmploeesGroup: " + sEmploeesGroup );
         
