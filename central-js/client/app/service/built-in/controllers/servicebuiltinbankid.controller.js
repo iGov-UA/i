d6ccf5ea148752ef8e1997864cd9bd63f2cbf2ab
Oleksii Khalikov <oleksii.khalikov@gmail.com>
2016-10-04 13:49:17 +0300

    https://github.com/e-government-ua/i/issues/1373 return

--- central-js/client/app/service/built-in/controllers/servicebuiltinbankid.controller.js
+++ central-js/client/app/service/built-in/controllers/servicebuiltinbankid.controller.js
@@ -791,6 +791,9 @@ angular.module('app').controller('ServiceBuiltInBankIDController',
         }
       };
 
+      $scope.insertSeparator = function(sPropertyId){
+        return FieldAttributesService.insertSeparators(sPropertyId);
+      };
 
       // блокировка кнопок выбора файлов на время выполнения процесса загрузки ранее выбранного файла
       $rootScope.isFileProcessUploading = {
