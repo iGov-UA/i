2c3986279a87f9cf0fe5ab0b2c1ce7fe47cc8dc2
Olia <Olia@olga>
2016-10-06 00:07:23 +0300

    fix

--- wf-base/src/main/java/org/igov/service/controller/ObjectFileCommonController.java
+++ wf-base/src/main/java/org/igov/service/controller/ObjectFileCommonController.java
@@ -734,92 +734,10 @@ public class ObjectFileCommonController {
             //replacement = regex + URLEncoder.encode(entry.getValue(), "UTF-8"); //<![CDATA[текст]]>
             replacement = regex + entry.getValue().replaceAll(">", "&gt;").replaceAll("<", "&lt;")
                     .replaceAll("\"", "&quot;").replaceAll("'", "&apos;").replaceAll("&", "&amp;");
-            declarContent = declarContent.replaceAll(regex, replacement);
+            //declarContent = declarContent.replaceAll(regex, replacement);
         }
-        //запись контента в xml файл
-        //MultipartFile multipartFile = new ByteArrayMultipartFile(declarContent.getBytes(),
-        //        sID_Pattern, sID_Pattern, "application/xml");
-        //httpResponse.setHeader("Content-disposition", "attachment; filename=" + multipartFile.getName());
-        //httpResponse.setHeader("Content-Type", multipartFile.getContentType());
-        //httpResponse.setContentLength(multipartFile.getBytes().length);
-        //String key = oBytesDataInmemoryStorage.putBytes(AbstractModelTask
-        //        .multipartFileToByteArray(multipartFile, multipartFile.getOriginalFilename())
-        //        .toByteArray());
-        //result.put("sID_Redis", key);
         result.put("soPatternFilled", declarContent.replaceAll(System.getProperty("line.separator"), ""));
         return result;
     }
-
-    /*
-    @ApiOperation(value = "/putAttachmentToRedis_old", notes = "##### Контроллер платежей. Регистрация проведенного платежа - по колбэку от платежной системы\n")
-    @RequestMapping(value = {"/putAttachmentToRedis_old"}, method = RequestMethod.POST, headers = {"Accept=application/json"})
-    public @ResponseBody
-    String putAttachmentToRedis_old(@RequestBody(required = false) Map<String, String> data,
-            HttpServletResponse httpResponse) throws Exception {
-        //генерируем хмл в виде файла и проставляем значения в него. отдаем на клиента
-        LOG.info("data: " + data);
-        DBody body = new DBody();
-        ObjectFactory factory = new ObjectFactory();
-        body.setHLNAME(data.get("hlname"));
-        body.setHPNAME(data.get("hpname"));
-        body.setHFNAME(factory.createDBodyHFNAME(data.get("hfname")));
-        body.setHTIN(data.get("htin"));
-        body.setHEMAIL(data.get("hemail"));
-        body.setHPASS(factory.createDBodyHPASS(data.get("hpass")));
-        body.setHPASSDATE(factory.createDBodyHPASSDATE(data.get("hpassDate")));
-        body.setHPASSISS(factory.createDBodyHPASS(data.get("hpassiss")));
-        body.setHSTREET(data.get("hstreet"));
-        body.setHBUILD(data.get("hbuild"));
-        body.setHAPT(factory.createDBodyHAPT(data.get("hapart")));
-        body.setHCOUNTRY(data.get("country"));
-        body.setR01G01(Integer.valueOf(data.get("kvStart")));
-        body.setR01G02(Integer.valueOf(data.get("yStart")));
-        body.setR02G01(Integer.valueOf(data.get("kvEnd")));
-        body.setR02G02(Integer.valueOf(data.get("yEnd")));
-
-        DHead head = new DHead();
-        head.setTIN(null);
-        head.setLINKEDDOCS(factory.createDHeadLINKEDDOCS(null));
-        head.setCDOC(null);
-        head.setCDOCSUB(null);
-        head.setCDOCVER(null);
-        head.setCDOCTYPE(null);
-        head.setCDOCCNT(null);
-        head.setCREG(null);
-        head.setCRAJ(null);
-        head.setPERIODMONTH(null);
-        head.setPERIODTYPE(null);
-        head.setPERIODYEAR(null);
-        head.setCSTIORIG(null);
-        head.setCDOCSTAN(null);
-        head.setDFILL(null);
-        head.setSOFTWARE(null);
-        head.setTIN(null);
-
-        DeclarContent declar = new DeclarContent();
-        declar.setDECLARHEAD(head);
-        declar.setDECLARBODY(body);
-
-        StringWriter sw = new StringWriter();
-        JAXBContext jc = JAXBContext.newInstance(DeclarContent.class);
-        Marshaller m = jc.createMarshaller();
-        m.marshal(declar, sw);
-
-        String declarContent = sw.toString();
-        LOG.info("Created document with customer info to embedd to message: {}", declarContent);
-        //запись контента в xml файл
-        MultipartFile file = new ByteArrayMultipartFile(declarContent.getBytes(),
-                "SwinEd", "SwinEd", "application/xml");
-        httpResponse.setHeader("Content-disposition", "attachment; filename=" + file.getName());
-        httpResponse.setHeader("Content-Type", file.getContentType());
-        httpResponse.setContentLength(declarContent.getBytes().length);
-        String key = oBytesDataInmemoryStorage.putBytes(AbstractModelTask
-                .multipartFileToByteArray(file, file.getOriginalFilename())
-                .toByteArray());
-        return key + " " + declarContent;
-        //return declarContent.getBytes();
-    }
-
-    */
     
 }
