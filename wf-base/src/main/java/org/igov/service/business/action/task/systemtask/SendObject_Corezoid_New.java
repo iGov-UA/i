ecd3a21e786522c62cb600603682df0344690891
Olia <Olia@olga>
2016-10-11 23:10:13 +0300

    fix

--- wf-base/src/main/java/org/igov/service/business/action/task/systemtask/SendObject_Corezoid_New.java
+++ wf-base/src/main/java/org/igov/service/business/action/task/systemtask/SendObject_Corezoid_New.java
@@ -1,6 +1,5 @@
 package org.igov.service.business.action.task.systemtask;
 
-import org.igov.service.business.action.task.listener.*;
 import java.util.HashMap;
 import java.util.Map;
 import org.activiti.engine.delegate.DelegateExecution;
@@ -24,8 +23,6 @@ public class SendObject_Corezoid_New extends Abstract_MailTaskCustom implements 
 
     @Autowired
     Corezoid corezoid;
-
-    private Expression soJSON;
 
     private Expression soData;
 
@@ -59,25 +56,4 @@ public class SendObject_Corezoid_New extends Abstract_MailTaskCustom implements 
                 generalConfig.getsSecretKey_Corezoid_Gorsovet_Exchange(), data);
         LOG.info("sendToCorezoid result:" + result);
     }
-    /*@Override
-    public void execute(DelegateExecution oExecution) throws Exception {
-        String soJSON_Value = this.soJSON.getExpressionText();
-        String sID_Conveyour_Value = this.sID_Conveyour.getExpressionText();
-        LOG.info("soJSON_Value: " + soJSON_Value + " sID_Conveyour_Value: " + sID_Conveyour_Value);
-        String sJSON_Result = replaceTags(soJSON_Value, oExecution);
-        LOG.info("sJSON_Result: " + sJSON_Result);
-        JSONObject soJSON_Result = new JSONObject(sJSON_Result);
-        Map<String, Object> data = new HashMap();
-        Iterator it = soJSON_Result.keys();
-        while (it.hasNext()) {
-            String key = (String) it.next();
-            data.put(key, soJSON_Result.get(key));
-        }
-        //data.put("nID_Task", Integer.parseInt(delegateTask.getId()));
-        LOG.info("sendToCorezoid data:" + data);
-        String result = corezoid.sendToCorezoid(sID_Conveyour_Value,
-                generalConfig.getsUser_Corezoid_Gorsovet_Exchange(), 
-                generalConfig.getsSecretKey_Corezoid_Gorsovet_Exchange(), data);
-        LOG.info("sendToCorezoid result:" + result);
-    }*/
 }
