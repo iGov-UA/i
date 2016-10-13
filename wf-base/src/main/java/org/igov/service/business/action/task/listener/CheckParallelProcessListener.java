ad3face84ea2308d071f356f7ed1d026660acf97
Kuhtin Sergey <kuhtin.sv@gmail.com>
2016-10-13 16:02:37 +0300

    with standart form of message from Exception

--- wf-base/src/main/java/org/igov/service/business/action/task/listener/CheckParallelProcessListener.java
+++ wf-base/src/main/java/org/igov/service/business/action/task/listener/CheckParallelProcessListener.java
@@ -42,7 +42,7 @@ public class CheckParallelProcessListener implements ExecutionListener {
         LOG.info("sValFromProcess: " + sValFromProcess);
 
         if (!checkValue.trim().equals(sValFromProcess.trim())) {
-            throw new TaskAlreadyUnboundException(sErrorMessage);
+            throw new TaskAlreadyUnboundException("errMsg=" + sErrorMessage);
         }
     }
 
