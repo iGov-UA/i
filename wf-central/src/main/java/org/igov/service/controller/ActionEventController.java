cdc44883055f545fecc01cb2192ab11df5e282ad
HS <HS@192.168.0.214>
2016-10-26 20:55:04 +0300

    add logs for test issue 1397

--- wf-central/src/main/java/org/igov/service/controller/ActionEventController.java
+++ wf-central/src/main/java/org/igov/service/controller/ActionEventController.java
@@ -643,7 +643,6 @@ public class ActionEventController {
                 LOG.info("Looking history event services by IDs " + anID_HistoryEvent_Service);
 //                List<SubjectMessage> aSubjectMessage = subjectMessagesDao.findAllByInValues("nID_HistoryEvent_Service", anID_HistoryEvent_Service);
                 List<SubjectMessageFeedback> aSubjectMessage = subjectMessageFeedbackDao.findByOrder(sDateTo);
-                LOG.info("aSubjectMessage: "+ aSubjectMessage);
                 LOG.info("Found {} subject messages by nID_HistoryEvent_Service values", aSubjectMessage.size());
                 Map<Long, SubjectMessage> mSubjectMessage = new HashMap<>();
 //                for (SubjectMessage oSubjectMessage : aSubjectMessage) {
@@ -669,7 +668,7 @@ public class ActionEventController {
                     asCell.add(oHistoryEvent_Service.getnRate() != null ? oHistoryEvent_Service.getnRate().toString() : "");
                     SubjectMessageFeedback oSubjectMessageFeedback = new SubjectMessageFeedback();
                     String sTextFeedback = "";
-                    if (mSubjectMessage.get(oHistoryEvent_Service.getId()) != null) {
+                    if (oSubjectMessageFeedback.getnID_Service() != null) {
                         sTextFeedback = oSubjectMessageFeedback.getoSubjectMessage().getBody();
                         LOG.info("sTextFeedback: "+sTextFeedback);
                     } else {
