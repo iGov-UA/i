1395b242369f119695a2a1e68269835961e9f0c4
antonlegkiy <anton.legkiy@gmail.com>
2016-10-27 13:09:13 +0300

    fix

--- dashboard-js/client/components/print/PrintTemplateProcessor.js
+++ dashboard-js/client/components/print/PrintTemplateProcessor.js
@@ -44,7 +44,7 @@ angular.module('dashboardJsApp').factory('PrintTemplateProcessor', ['$sce', 'Aut
   return {
     processPrintTemplate: function (task, form, printTemplate, reg, fieldGetter) {
       var _printTemplate = printTemplate;
-      var templates = [], ids = [], found, idArray = [];
+      var templates = [], ids = [], found;
       while (found = reg.exec(_printTemplate)) {
         templates.push(found[1]);
         ids.push(found[2]);
