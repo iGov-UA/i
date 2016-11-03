b18d4ed86907864c3f5fb8007a61cf8c5d26755e
HS <HS@192.168.0.214>
2016-11-03 22:29:09 +0200

    fix issue 1397

--- wf-central/src/main/java/org/igov/service/controller/ActionEventController.java
+++ wf-central/src/main/java/org/igov/service/controller/ActionEventController.java
@@ -673,7 +673,8 @@ public class ActionEventController {
 //                    SubjectMessageFeedback oSubjectMessageFeedback = oSubjectMessageService.getSubjectMessageFeedbackById(oHistoryEvent_Service.getId());
                     LOG.info("oSubjectMessageFeedback.isPresent(): "+oSubjectMessageFeedback.isPresent());
 //                    LOG.info("oSubjectMessageFeedback.get().getoSubjectMessage().getnID_HistoryEvent_Service(): "+oSubjectMessageFeedback.get().getoSubjectMessage().getnID_HistoryEvent_Service());
-                    if (oSubjectMessageFeedback.isPresent() && oSubjectMessageFeedback.get().getoSubjectMessage().getnID_HistoryEvent_Service() != null) {
+//                    if (oSubjectMessageFeedback.isPresent() && oSubjectMessageFeedback.get().getoSubjectMessage().getnID_HistoryEvent_Service() != null) {
+                    if (oSubjectMessageFeedback.isPresent()) {
                         sTextFeedback = oSubjectMessageFeedback.get().getoSubjectMessage().getBody();
                     } else {
 //                        LOG.error("Unable to find feedabck for history event with ID {}", oHistoryEvent_Service.getId());
