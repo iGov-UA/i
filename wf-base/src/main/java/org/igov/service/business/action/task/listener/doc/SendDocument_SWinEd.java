9567cc9e810a299f9e6f06513e645f9b8cb0bfaf
askosyr <askosyr@gmail.com>
2016-11-09 00:56:25 +0200

    https://github.com/e-government-ua/i/issues/1424 Added additional logging

--- wf-base/src/main/java/org/igov/service/business/action/task/listener/doc/SendDocument_SWinEd.java
+++ wf-base/src/main/java/org/igov/service/business/action/task/listener/doc/SendDocument_SWinEd.java
@@ -58,9 +58,10 @@ public class SendDocument_SWinEd extends AbstractModelTask implements TaskListen
             if (oFile_XML_SWinEd != null) {
                 GateSoapProxy gate = new GateSoapProxy(URL);
                 LOG.info("!!! Before sending request to gate web service. sID_File_XML_SWinEdValue:" + oByteArrayMultipartFile.getOriginalFilename() + 
-                		" sEmailValue:" + sEmailValue);
+                		" sEmailValue:" + sEmailValue + " endpoint:" + gate.getEndpoint() + " content:" + oByteArrayMultipartFile.getBytes());
                 ProcessResult result = gate.send(oByteArrayMultipartFile.getOriginalFilename(), sEmailValue, oByteArrayMultipartFile.getBytes());
                 LOG.info("!!!response:" + result.getValue());
+                resp = result.getValue();
             } else {
                 LOG.info("sID_File_XML_SWinEdValue: " + sID_File_XML_SWinEdValue + " oFile_XML_SWinEd is null!!!");
             }
