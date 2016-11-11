2239ff570bffd00fd4721457d11a2b751fc3a5d0
skuhtin <sergiy.kuhtin@gmail.com>
2016-11-11 14:28:56 +0200

    fix saveForm

--- wf-base/src/main/java/org/igov/service/controller/ActionTaskCommonController.java
+++ wf-base/src/main/java/org/igov/service/controller/ActionTaskCommonController.java
@@ -2564,7 +2564,7 @@ public class ActionTaskCommonController {//extends ExecutionBaseResource
         //String value = null;
         try {
             LOG.info("Input params - " + sParams);
-            sParams= new String(sParams.getBytes("windows-1251"),"utf-8");
+            sParams= new String(sParams.getBytes("utf-8"),"windows-1251");
             LOG.info("After refactoring params - " + sParams);
             org.json.simple.JSONObject jsonObj = (org.json.simple.JSONObject) new JSONParser().parse(sParams);
             LOG.info("Succ. parsing of input data passed");
