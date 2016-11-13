906317fbddf6eaf9befe1546f2e1f033a3b140f7
Igor Jarmak <igor.jarmak@gmail.com>
2016-11-13 15:01:51 +0200

    https://github.com/e-government-ua/i/issues/1422  implemented

--- central-js/server/api/sign-content/index.js
+++ central-js/server/api/sign-content/index.js
@@ -2,9 +2,12 @@
 
 var express = require('express')
   , router = express.Router()
-  , sign = require('./sign.controller');
+  , sign = require('./sign.controller')
+  , decrypt = require('./decrypt.controller');
 
 router.get('/sign', sign.signContent);
 router.use('/sign/callback', sign.callback);
+router.get('/decrypt', decrypt.decryptContent);
+router.use('/decrypt/callback', decrypt.callback);
 
 module.exports = router;
