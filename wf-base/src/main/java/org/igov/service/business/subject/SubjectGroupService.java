11ffba7d7c69f0412efa87ac01172965d368340b
Dashenko-Inna <dascenkoinna@gmail.com>
2016-11-16 02:50:29 +0200

    edit Signed-off-by: Dashenko-Inna <dascenkoinna@gmail.com>

--- wf-base/src/main/java/org/igov/service/business/subject/SubjectGroupService.java
+++ wf-base/src/main/java/org/igov/service/business/subject/SubjectGroupService.java
@@ -70,13 +70,13 @@ public class SubjectGroupService {
 					}
 				}
 
-				if (idParentList.contains(child.getId())) {
 					for (VSubjectGroupParentNode vSubjectGroupParentNode : parentSubjectGroups) {
+						if(vSubjectGroupParentNode.getChildren().contains(parent.getId())) {
 						countChild++;
 						if (countChild.compareTo(deepLevel) < 0) {
 							vSubjectGroupParentNode.getChildren().add(child);
 						}
-					}
+						}
 				}
 
 			}
@@ -89,8 +89,6 @@ public class SubjectGroupService {
 						.compareTo(((VSubjectGroupParentNode) vSubjectGroupParentNodeTwo).getGroup().getId());
 			}
 		});
-
-		Map<SubjectGroup, List<SubjectGroup>> map = new HashMap<>();
 
 		final List<VSubjectGroupParentNode> parentSubjectGroupsFilltr = Lists
 				.newArrayList(Collections2.filter(parentSubjectGroups, new Predicate<VSubjectGroupParentNode>() {
@@ -105,139 +103,5 @@ public class SubjectGroupService {
 		return parentSubjectGroups;
 
 	}
-
-	/*
-	 * public List<SubjectGroup> getFullResult(String sID_Group_Activiti, Long
-	 * deepLevel, List<VSubjectGroupParentNode> parentSubjectGroups,
-	 * VSubjectGroupParentNode parentSubjectGroup) { if ((deepLevel == null ||
-	 * deepLevel == 0) && (sID_Group_Activiti == null ||
-	 * sID_Group_Activiti.isEmpty())) {
-	 * 
-	 * final List<List<SubjectGroup>> childrensParList =
-	 * Lists.newArrayList(Collections2.transform( parentSubjectGroups, new
-	 * Function<VSubjectGroupParentNode, List<SubjectGroup>>() {
-	 * 
-	 * @Override public List<SubjectGroup> apply(VSubjectGroupParentNode
-	 * vSubjectGroupParentNode) { return vSubjectGroupParentNode.getChildren();
-	 * } }));
-	 * 
-	 * final List<SubjectGroup> childrensByGroup =
-	 * Lists.newArrayList(Collections2.transform( childrensParList, new
-	 * Function<List<SubjectGroup>, SubjectGroup>() {
-	 * 
-	 * @Override public SubjectGroup apply( List<SubjectGroup>
-	 * vSubjectGroupChildrenNodeList) { return
-	 * vSubjectGroupChildrenNodeList.get(0); } }));
-	 * 
-	 * 
-	 * VSubjectGroupTreeResult subjectGroupTreeResult = new
-	 * VSubjectGroupTreeResult();
-	 * parentSubjectGroup.accept(subjectGroupTreeResult);
-	 * 
-	 * return childrensByGroup; }
-	 *//**
-		 * получить только отфильтрованные по sID_Group_Activiti
-		 */
-	/*
-	 * final List<VSubjectGroupParentNode> parentSubjectGroupsFilltr = Lists
-	 * .newArrayList(Collections2.filter(parentSubjectGroups, new
-	 * Predicate<VSubjectGroupParentNode>() {
-	 * 
-	 * @Override public boolean apply(VSubjectGroupParentNode
-	 * vSubjectGroupParentNode) { return
-	 * vSubjectGroupParentNode.getGroup().getsID_Group_Activiti().equals(
-	 * sID_Group_Activiti); } }));
-	 * 
-	 *//**
-		 * получаем лист детей отфильтрованного списка
-		 */
-	/*
-	 * final List<List<VSubjectGroupChildrenNode>> childrensParList =
-	 * Lists.newArrayList(Collections2.transform( parentSubjectGroupsFilltr, new
-	 * Function<VSubjectGroupParentNode, List<VSubjectGroupChildrenNode>>() {
-	 * 
-	 * @Override public List<VSubjectGroupChildrenNode>
-	 * apply(VSubjectGroupParentNode vSubjectGroupParentNode) { return
-	 * vSubjectGroupParentNode.getChildren(); } }));
-	 * 
-	 *//**
-		 * только лист
-		 */
-	/*
-	 * final List<VSubjectGroupChildrenNode> childrensByGroup =
-	 * Lists.newArrayList(Collections2.transform( childrensParList, new
-	 * Function<List<VSubjectGroupChildrenNode>, VSubjectGroupChildrenNode>() {
-	 * 
-	 * @Override public VSubjectGroupChildrenNode apply(
-	 * List<VSubjectGroupChildrenNode> vSubjectGroupChildrenNodeList) { return
-	 * vSubjectGroupChildrenNodeList.get(0); } }));
-	 * 
-	 * final List<Long> nIdList = Lists
-	 * .newArrayList(Collections2.transform(childrensByGroup, new
-	 * Function<VSubjectGroupChildrenNode, Long>() {
-	 * 
-	 * @Override public Long apply(VSubjectGroupChildrenNode
-	 * vSubjectGroupChildrenNode) { return
-	 * vSubjectGroupChildrenNode.getGroup().getId(); } }));
-	 * 
-	 * final List<VSubjectGroupParentNode> parentSubjectGroupsFilltrRes = Lists
-	 * .newArrayList(Collections2.filter(parentSubjectGroups, new
-	 * Predicate<VSubjectGroupParentNode>() {
-	 * 
-	 * @Override public boolean apply(VSubjectGroupParentNode
-	 * vSubjectGroupParentNode) {
-	 * 
-	 * return nIdList.contains(vSubjectGroupParentNode.getGroup().getId()); }
-	 * }));
-	 * 
-	 * List<VSubjectGroupParentNode> newList = new
-	 * ArrayList<>(parentSubjectGroupsFilltrRes);
-	 * newList.addAll(parentSubjectGroupsFilltr);
-	 * 
-	 * Collections.sort(newList, new Comparator() {
-	 * 
-	 * @Override public int compare(Object vSubjectGroupParentNode, Object
-	 * vSubjectGroupParentNodeTwo) { return ((VSubjectGroupParentNode)
-	 * vSubjectGroupParentNode).getGroup().getId()
-	 * .compareTo(((VSubjectGroupParentNode)
-	 * vSubjectGroupParentNodeTwo).getGroup().getId()); } });
-	 * 
-	 * final List<List<VSubjectGroupChildrenNode>> childrensParListRes =
-	 * Lists.newArrayList(Collections2 .transform(newList, new
-	 * Function<VSubjectGroupParentNode, List<VSubjectGroupChildrenNode>>() {
-	 * 
-	 * @Override public List<VSubjectGroupChildrenNode>
-	 * apply(VSubjectGroupParentNode vSubjectGroupParentNode) { return
-	 * vSubjectGroupParentNode.getChildren(); } }));
-	 * 
-	 *//**
-		 * только лист
-		 *//*
-		 * final List<VSubjectGroupChildrenNode> childrensByGroupRes =
-		 * Lists.newArrayList(Collections2.transform( childrensParListRes, new
-		 * Function<List<VSubjectGroupChildrenNode>,
-		 * VSubjectGroupChildrenNode>() {
-		 * 
-		 * @Override public VSubjectGroupChildrenNode apply(
-		 * List<VSubjectGroupChildrenNode> vSubjectGroupChildrenNodeList) {
-		 * return vSubjectGroupChildrenNodeList.get(0); } }));
-		 * 
-		 * 
-		 * final List<SubjectGroup> childrens = Lists.newArrayList(
-		 * Collections2.transform(childrensByGroupRes, new
-		 * Function<VSubjectGroupChildrenNode, SubjectGroup>() {
-		 * 
-		 * @Override public SubjectGroup apply(VSubjectGroupChildrenNode
-		 * vSubjectGroupChildrenNodeList) { return
-		 * vSubjectGroupChildrenNodeList.getGroup(); } })); if ((deepLevel ==
-		 * null || deepLevel == 0) && (sID_Group_Activiti != null ||
-		 * !sID_Group_Activiti.isEmpty())) { return childrens; }
-		 * 
-		 * VSubjectGroupTreeResult subjectGroupTreeResult = new
-		 * VSubjectGroupTreeResult();
-		 * parentSubjectGroup.accept(subjectGroupTreeResult);
-		 * 
-		 * return childrens; }
-		 */
 
 }
