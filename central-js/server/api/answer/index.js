08b44d57718f1e0b163452b12d60626f47815466
Igor Jarmak <igor.jarmak@gmail.com>
2016-11-16 23:09:58 +0200

    https://github.com/e-government-ua/i/issues/1426 implemented

--- central-js/server/api/answer/index.js
+++ central-js/server/api/answer/index.js
@@ -4,5 +4,6 @@ var controller = require('./answer.controller.js');
 
 
 router.get('/DFS/decrypted', controller.getDecrypted);
+router.get('/DFS/decrypted/json', controller.getJSON);
 
 module.exports = router;
