1f42c0de02310acd6a77db7e3eba8fee8cf34103
Dashenko-Inna <dascenkoinna@gmail.com>
2016-11-16 04:58:03 +0200

    edit Signed-off-by: Dashenko-Inna <dascenkoinna@gmail.com>

--- wf-base/src/main/java/org/igov/service/controller/SubjectGroupController.java
+++ wf-base/src/main/java/org/igov/service/controller/SubjectGroupController.java
@@ -3,8 +3,6 @@ package org.igov.service.controller;
 
 import java.util.List;
 
-import org.igov.model.subject.SubjectGroup;
-import org.igov.model.subject.VSubjectGroupChildrenNode;
 import org.igov.model.subject.VSubjectGroupParentNode;
 import org.igov.service.business.subject.SubjectGroupService;
 import org.slf4j.Logger;
