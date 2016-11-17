6ba185e8e4d3bc0d03fe89618550f8d82902c0b7
Dashenko-Inna <dascenkoinna@gmail.com>
2016-11-17 12:37:15 +0200

    edit
    Signed-off-by: Dashenko-Inna <dascenkoinna@gmail.com>

--- wf-base/src/main/java/org/igov/service/business/subject/SubjectGroupService.java
+++ wf-base/src/main/java/org/igov/service/business/subject/SubjectGroupService.java
@@ -6,6 +6,7 @@
 package org.igov.service.business.subject;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
@@ -13,6 +14,9 @@ import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -40,47 +44,85 @@ public class SubjectGroupService {
 	@Autowired
 	private BaseEntityDao<Long> baseEntityDao;
 
-	public List<VSubjectGroupParentNode> getCatalogTreeSubjectGroups(String sID_Group_Activiti, Long deepLevel) {
+	public List<List<SubjectGroup>> getCatalogTreeSubjectGroups(String sID_Group_Activiti, Long deepLevel) {
 		countChild = 0L;
 		List<SubjectGroupTree> subjectGroupRelations = new ArrayList<>(baseEntityDao.findAll(SubjectGroupTree.class));
 
 		List<VSubjectGroupParentNode> parentSubjectGroups = new ArrayList<>();
-		Map<SubjectGroup, List<SubjectGroup>> subjToNodeMap = new HashMap<>();
+		Map<Long, List<SubjectGroup>> subjToNodeMap = new HashMap<>();
 		VSubjectGroupParentNode parentSubjectGroup = null;
 		Set<Long> idParentList = new LinkedHashSet<>();
+		Set<Long> idChildrenList = new LinkedHashSet<>();
 		for (SubjectGroupTree subjectGroupRelation : subjectGroupRelations) {
 			final SubjectGroup parent = subjectGroupRelation.getoSubjectGroup_Parent();
 
 			if (parent.getId() != FAKE_ROOT_SUBJECT_ID) {
 				parentSubjectGroup = new VSubjectGroupParentNode();
 				final SubjectGroup child = subjectGroupRelation.getoSubjectGroup_Child();
+				if(!idChildrenList.contains(child)) {
+					idChildrenList.add(child.getId());
+				}
 				if (!idParentList.contains(parent.getId())) {
 					idParentList.add(parent.getId());
 					parentSubjectGroup.setGroup(parent);
 					parentSubjectGroup.addChild(child);
 					parentSubjectGroups.add(parentSubjectGroup);
-					subjToNodeMap.put(parent, parentSubjectGroup.getChildren());
+					subjToNodeMap.put(parent.getId(), parentSubjectGroup.getChildren());
 				} else {
 					for (VSubjectGroupParentNode vSubjectGroupParentNode : parentSubjectGroups) {
 						if (vSubjectGroupParentNode.getGroup().getId().equals(parent.getId())) {
 							vSubjectGroupParentNode.getChildren().add(child);
-							subjToNodeMap.put(parent, vSubjectGroupParentNode.getChildren());
+							subjToNodeMap.put(parent.getId(), vSubjectGroupParentNode.getChildren());
 						}
 					}
 				}
-				List<SubjectGroup> childSubjectGroup = subjToNodeMap.get(child);
-	            if (childSubjectGroup != null) {
-	            	subjToNodeMap.put(child, childSubjectGroup);
-	            	parentSubjectGroup.getChildren().add(child);	            
-	            	parentSubjectGroups.add(parentSubjectGroup);	
-	            
-	            }
-
 			}
 			
 		}
 		
+		SortedSet<VSubjectGroupParentNode> nodeSet = new TreeSet<VSubjectGroupParentNode>(new Comparator<VSubjectGroupParentNode>() {
+		    public int compare(VSubjectGroupParentNode node1, VSubjectGroupParentNode node2) {
 
+		        if (node1.getGroup() == null) {
+		            if (node2.getGroup() == null) {
+		                return  node1.getGroup().getId().compareTo(node2.getGroup().getId());
+		            }
+		            return -1;
+		        }
+
+		        if (node2.getGroup() == null) return 1;
+
+		        int parentCompare = node1.getGroup().getId()
+		                .compareTo(node2.getGroup().getId());
+
+		        if (parentCompare == 0)
+		            return node1.getGroup().getId().compareTo(node2.getGroup().getId());
+
+		        return parentCompare;
+		    }
+		});
+		
+		nodeSet.addAll(parentSubjectGroups);
+		
+		Map<SubjectGroup, List<SubjectGroup>> map = new HashMap<SubjectGroup, List<SubjectGroup>>();
+
+		for(VSubjectGroupParentNode node : nodeSet)
+		{
+		    if(map.get(node.getGroup())==null)
+		    {
+		        map.put(node.getGroup(), new ArrayList<SubjectGroup>());
+		    }
+		    map.get(node.getGroup()).add(node.getGroup());
+		    SubjectGroup parentNode = node.getGroup();
+		    while(parentNode!=null)
+		    {
+		        map.get(parentNode).add(node.getGroup());
+		        parentNode = node.getGroup();
+		    }
+		}
+		
+		List<List<SubjectGroup>> values = map.values().stream().collect(Collectors.toList());
+		
 		Collections.sort(parentSubjectGroups, new Comparator() {
 			@Override
 			public int compare(Object vSubjectGroupParentNode, Object vSubjectGroupParentNodeTwo) {
@@ -113,7 +155,7 @@ public class SubjectGroupService {
 
 		VSubjectGroupTreeResult subjectGroupTreeResult = new VSubjectGroupTreeResult();
 		parentSubjectGroup.accept(subjectGroupTreeResult);
-		return parentSubjectGroups;
+		return values;
 
 	}
 
