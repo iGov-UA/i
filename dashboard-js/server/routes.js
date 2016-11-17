bec52bfc5f9b686cb502d913b2b2d3f9bdf8ee53
antonlegkiy <anton.legkiy@gmail.com>
2016-11-17 16:52:12 +0200

    added select fields to dashboards table

--- dashboard-js/server/routes.js
+++ dashboard-js/server/routes.js
@@ -20,6 +20,13 @@ module.exports = function(app) {
   app.use('/auth', require('./auth'));
   app.use('/api/profile', require('./api/profile'));
   app.use('/api/users', require('./api/user'));
+  app.use('/api/countries', require('./api/countries'));
+  app.use('/api/currencies', require('./api/currencies'));
+  app.use('/api/object-customs', require('./api/object-customs'));
+  app.use('/api/subject', require('./api/subject'));
+  app.use('/api/object-earth-target', require('./api/object-earth-target'));
+  app.use('/api/subject-action-kved', require('./api/subject-action-kved'));
+  app.use('/api/object-place', require('./api/object-place'));
 
   // All undefined asset or api routes should return a 404
   app.route('/:url(api|auth|components|app|bower_components|assets|public-js)/*')
