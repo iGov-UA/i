778590ca4d6478ca9432dc8c4a81b109172251ad
BW <bw@bw-laptop>
2016-11-19 22:49:52 +0200

    https://github.com/e-government-ua/i/issues/1405 2.2)

--- wf-base/src/main/java/org/igov/service/business/document/DocumentStepService.java
+++ wf-base/src/main/java/org/igov/service/business/document/DocumentStepService.java
@@ -166,79 +166,99 @@ public class DocumentStepService {
 
     public JSONObject getDocumentStepRights(String sLogin, String snID_Process_Activiti){
         //assume that we can have only one active task per process at the same time
-        LOG.debug("sLogin={}, snID_Process_Activiti={}", sLogin, snID_Process_Activiti);
-        List<Task> activeTasks = oTaskService.createTaskQuery().processInstanceId(snID_Process_Activiti).active().list();
-        if(activeTasks.size() < 1 || activeTasks.get(0) == null){
+        LOG.info("sLogin={}, snID_Process_Activiti={}", sLogin, snID_Process_Activiti);
+        List<Task> aTaskActive = oTaskService.createTaskQuery().processInstanceId(snID_Process_Activiti).active().list();
+        if(aTaskActive.size() < 1 || aTaskActive.get(0) == null){
             throw new IllegalArgumentException("Process with ID: " + snID_Process_Activiti + " has no active task.");
         }
-        Task activeTask = activeTasks.get(0);
-        String taskKey = activeTask.getTaskDefinitionKey();
-
-        ProcessInstance process = runtimeService
+        Task oTaskActive = aTaskActive.get(0);
+        String sKey_UserTask = oTaskActive.getTaskDefinitionKey();
+        String sID_BP = oTaskActive.getProcessDefinitionId();
+        LOG.info("sID_BP={}", sID_BP);
+        if(sID_BP!=null && sID_BP.contains(":")){
+            String[] as = sID_BP.split("\\:");
+            sID_BP = as[0];
+            LOG.info("FIX(:) sID_BP={}", sID_BP);
+        }
+        if(sID_BP!=null && sID_BP.contains(".")){
+            String[] as = sID_BP.split("\\.");
+            sID_BP = as[0];
+            LOG.info("FIX(.) sID_BP={}", sID_BP);
+        }
+        final String sGroupPrefix = new StringBuilder(sID_BP).append("_").toString();
+ 
+        ProcessInstance oProcessInstance = runtimeService
                 .createProcessInstanceQuery()
                 .processInstanceId(snID_Process_Activiti)
                 .active()
                 .singleResult();
-
-        Map<String, Object> processVariables = process.getProcessVariables();
-        String activeStep = (String) processVariables.get("sKey_Step_Document");
-        if(StringUtils.isEmpty(activeStep)){
-            throw new IllegalStateException("There is no active Document Sep." +
-                    " Process variable sKey_Step_Document is empty.");
+        Map<String, Object> mProcessVariable = oProcessInstance.getProcessVariables();
+        
+        List<DocumentStep> aDocumentStep = documentStepDao.findAllBy("snID_Process_Activiti", snID_Process_Activiti);
+        LOG.debug("aDocumentStep={}", aDocumentStep);
+        
+        DocumentStep oDocumentStep_Common = aDocumentStep
+                .stream()
+                .filter(o -> o.getsKey_Step().equals("_"))
+                .findAny()
+                .orElse(null);
+        LOG.debug("oDocumentStep_Common={}", oDocumentStep_Common);
+        
+        String sKey_Step_Document = (String) mProcessVariable.get("sKey_Step_Document");
+        if(StringUtils.isEmpty(sKey_Step_Document)){
+            //throw new IllegalStateException("There is no active Document Sep." +
+            //        " Process variable sKey_Step_Document is empty.");
+            //sKey_Step_Document="1";
         }
-        List<DocumentStep> steps = documentStepDao.findAllBy("snID_Process_Activiti", snID_Process_Activiti);
-        LOG.debug("steps: {}", steps);
-        DocumentStep commonStep = steps
+        DocumentStep oDocumentStep_Active = aDocumentStep
                 .stream()
-                .filter(step -> step.getsKey_Step().equals("_"))
+                .filter(o -> sKey_Step_Document == null ? o.getnOrder().equals(1) : o.getsKey_Step().equals(sKey_Step_Document))
                 .findAny()
                 .orElse(null);
-        LOG.debug("commonStep: {}", commonStep);
-        DocumentStep currentActiveStep = steps
-                .stream()
-                .filter(step -> step.getsKey_Step().equals(activeStep))
-                .findAny()
-                .orElse(null);
-        LOG.debug("currentActiveStep: {}", currentActiveStep);
-        if(currentActiveStep == null){
+        LOG.debug("oDocumentStep_Active={}", oDocumentStep_Active);
+        if(oDocumentStep_Active == null){
             throw new IllegalStateException("There is no active Document Sep, process variable sKey_Step_Document="
-                    + activeStep);
+                    + sKey_Step_Document);
         }
 
-        List<Group> usersGroups = identityService.createGroupQuery().groupMember(sLogin).list();
-        Set<String> groups = new HashSet<>();
-        if(usersGroups != null){
-            usersGroups.stream().forEach(group -> groups.add(group.getId()));
+        List<Group> aGroup = identityService.createGroupQuery().groupMember(sLogin).list();
+        Set<String> asID_Group = new HashSet<>();
+        if(aGroup != null){
+            aGroup.stream().forEach(group -> asID_Group.add(group.getId()));
         }
-        LOG.debug("sLogin={}, groups: {}",sLogin, groups);
+        LOG.debug("sLogin={}, asID_Group={}",sLogin, asID_Group);
         //Lets collect DocumentStepSubjectRight by according users groups
 
-        List<DocumentStepSubjectRight> rightsForUserFromCommonStep = commonStep
+        List<DocumentStepSubjectRight> aDocumentStepSubjectRight_Common = oDocumentStep_Common
                 .getRights()
                 .stream()
-                .filter(right -> groups.contains(right.getsKey_GroupPostfix()))
+                .filter(o -> asID_Group.contains(new StringBuilder(sGroupPrefix).append(o.getsKey_GroupPostfix())))
                 .collect(Collectors.toList());
-        LOG.debug("rightsForUserFromCommonStep: {}", rightsForUserFromCommonStep);
+        LOG.debug("aDocumentStepSubjectRight_Common={}", aDocumentStepSubjectRight_Common);
 
-        List<DocumentStepSubjectRight> rightsforUserFromActiveStep = currentActiveStep
+        List<DocumentStepSubjectRight> aDocumentStepSubjectRight_Active = oDocumentStep_Active
                 .getRights()
                 .stream()
-                .filter(right -> groups.contains(right.getsKey_GroupPostfix()))
+                .filter(o -> asID_Group.contains(new StringBuilder(sGroupPrefix).append(o.getsKey_GroupPostfix())))
                 .collect(Collectors.toList());
-        LOG.debug("rightsforUserFromActiveStep: {}", rightsforUserFromActiveStep);
+        LOG.debug("aDocumentStepSubjectRight_Active={}", aDocumentStepSubjectRight_Active);
 
+        List<DocumentStepSubjectRight> aDocumentStepSubjectRight = new LinkedList(aDocumentStepSubjectRight_Common);
+        aDocumentStepSubjectRight.addAll(aDocumentStepSubjectRight_Active);
+        LOG.debug("aDocumentStepSubjectRight={}", aDocumentStepSubjectRight);
+        
         //Let's find current active task properties
         Set<String> taskFormPropertiesIDs = new TreeSet<>();
-        TaskFormData taskData = formService.getTaskFormData(taskKey);
+        TaskFormData oTaskFormData = formService.getTaskFormData(sKey_UserTask);
 
         taskFormPropertiesIDs
-                .addAll(taskData.getFormProperties().stream().map(FormProperty::getId).collect(Collectors.toList()));
+                .addAll(oTaskFormData.getFormProperties().stream().map(FormProperty::getId).collect(Collectors.toList()));
 
         //grunts for specific field when we accumulating from single DocumentStepSubjectRight are summed in
         // prohibitive way
         //First of all we process rights from common step.
 
-        Map<String, Object> resultGruntsFromCommonStep = buildGrunts(rightsForUserFromCommonStep, taskFormPropertiesIDs);
+        Map<String, Object> resultGruntsFromCommonStep = buildGrunts(aDocumentStepSubjectRight_Common, taskFormPropertiesIDs);
 
 
 
