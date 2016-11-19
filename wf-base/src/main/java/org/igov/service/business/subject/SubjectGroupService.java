8f9b51d5764d88b87e594a15859f11056f8decaa
Dashenko-Inna <dascenkoinna@gmail.com>
2016-11-19 11:32:44 +0200

    #1404 -п.2.3
    Signed-off-by: Dashenko-Inna <dascenkoinna@gmail.com>

--- wf-base/src/main/java/org/igov/service/business/subject/SubjectGroupService.java
+++ wf-base/src/main/java/org/igov/service/business/subject/SubjectGroupService.java
@@ -6,6 +6,7 @@
 package org.igov.service.business.subject;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.LinkedHashSet;
 import java.util.List;
@@ -27,7 +28,7 @@ import com.google.common.base.Function;
 import com.google.common.collect.Collections2;
 import com.google.common.collect.Lists;
 
-import edu.emory.mathcs.backport.java.util.Arrays;
+
 
 /**
  * Сервис получения организационной иерархии
@@ -37,140 +38,182 @@ import edu.emory.mathcs.backport.java.util.Arrays;
 @Service
 public class SubjectGroupService {
 
-    private static final Log LOG = LogFactory.getLog(SubjectGroupService.class);
-    private static final long FAKE_ROOT_SUBJECT_ID = 0;
-    
-    @Autowired
-    private BaseEntityDao<Long> baseEntityDao;
-    
-    @Autowired
-    private UsersService usersService;
+	private static final Log LOG = LogFactory.getLog(SubjectGroupService.class);
+	private static final long FAKE_ROOT_SUBJECT_ID = 0;
 
-    public SubjectGroupAndUser getCatalogSubjectGroups(String sID_Group_Activiti, Long deepLevel) {
-        List<SubjectGroup> aChildResult = new ArrayList();
-        List<SubjectGroupTree> subjectGroupRelations = new ArrayList<>(baseEntityDao.findAll(SubjectGroupTree.class));
-        List<VSubjectGroupParentNode> parentSubjectGroups = new ArrayList<>();
-        Map<Long, List<SubjectGroup>> subjToNodeMap = new HashMap<>();
-        Map<String, Long> mapGroupActiviti = new HashMap<>();
-        VSubjectGroupParentNode parentSubjectGroup = null;
-        Set<Long> idParentList = new LinkedHashSet<>();
-        for (SubjectGroupTree subjectGroupRelation : subjectGroupRelations) {
-            final SubjectGroup parent = subjectGroupRelation.getoSubjectGroup_Parent();
+	@Autowired
+	private BaseEntityDao<Long> baseEntityDao;
 
-            if (parent.getId() != FAKE_ROOT_SUBJECT_ID) {
-                parentSubjectGroup = new VSubjectGroupParentNode();
-                final SubjectGroup child = subjectGroupRelation.getoSubjectGroup_Child();
-                if (!idParentList.contains(parent.getId())) {
-                    idParentList.add(parent.getId());
-                    parentSubjectGroup.setGroup(parent); //устанавливаем парентов
-                    parentSubjectGroup.addChild(child); //доавляем детей
-                    parentSubjectGroups.add(parentSubjectGroup);
-                    subjToNodeMap.put(parent.getId(), parentSubjectGroup.getChildren()); //мапа парент - ребенок
-                    mapGroupActiviti.put(parent.getsID_Group_Activiti(), parent.getId()); // мапа группа - ид парента
-                } else {
-                    for (VSubjectGroupParentNode vSubjectGroupParentNode : parentSubjectGroups) {
-                        //убираем дубликаты
-                        if (vSubjectGroupParentNode.getGroup().getId().equals(parent.getId())) {
-                            vSubjectGroupParentNode.getChildren().add(child); // если дубликат парента- добавляем его детей к общему списку
-                            subjToNodeMap.put(parent.getId(), vSubjectGroupParentNode.getChildren());//мапа парент - ребенок
-                            mapGroupActiviti.put(parent.getsID_Group_Activiti(), parent.getId());// мапа группа - ид парента
-                        }
-                    }
-                }
-            }
+	@Autowired
+	private UsersService usersService;
 
-        }
+	public SubjectGroupAndUser getCatalogSubjectGroups(String sID_Group_Activiti, Long deepLevel) {
+		List<SubjectGroup> aChildResult = new ArrayList();
+		List<SubjectGroupTree> subjectGroupRelations = new ArrayList<>(baseEntityDao.findAll(SubjectGroupTree.class));
+		List<VSubjectGroupParentNode> parentSubjectGroups = new ArrayList<>();
+		Map<Long, List<SubjectGroup>> subjToNodeMap = new HashMap<>();
+		Map<String, Long> mapGroupActiviti = new HashMap<>();
+		VSubjectGroupParentNode parentSubjectGroup = null;
+		Set<Long> idParentList = new LinkedHashSet<>();
+		for (SubjectGroupTree subjectGroupRelation : subjectGroupRelations) {
+			final SubjectGroup parent = subjectGroupRelation.getoSubjectGroup_Parent();
 
-        //Map<Long, List<SubjectGroup>> subjToNodeMapFiltr = new HashMap<>();
-        Long groupFiltr = mapGroupActiviti.get(sID_Group_Activiti); //достаем ид sID_Group_Activiti которое на вход
+			if (parent.getId() != FAKE_ROOT_SUBJECT_ID) {
+				parentSubjectGroup = new VSubjectGroupParentNode();
+				final SubjectGroup child = subjectGroupRelation.getoSubjectGroup_Child();
+				if (!idParentList.contains(parent.getId())) {
+					idParentList.add(parent.getId());
+					parentSubjectGroup.setGroup(parent); // устанавливаем
+															// парентов
+					parentSubjectGroup.addChild(child); // доавляем детей
+					parentSubjectGroups.add(parentSubjectGroup);
+					subjToNodeMap.put(parent.getId(), parentSubjectGroup.getChildren()); // мапа
+																							// парент
+																							// -
+																							// ребенок
+					mapGroupActiviti.put(parent.getsID_Group_Activiti(), parent.getId()); // мапа
+																							// группа
+																							// -
+																							// ид
+																							// парента
+				} else {
+					for (VSubjectGroupParentNode vSubjectGroupParentNode : parentSubjectGroups) {
+						// убираем дубликаты
+						if (vSubjectGroupParentNode.getGroup().getId().equals(parent.getId())) {
+							vSubjectGroupParentNode.getChildren().add(child); // если
+																				// дубликат
+																				// парента-
+																				// добавляем
+																				// его
+																				// детей
+																				// к
+																				// общему
+																				// списку
+							subjToNodeMap.put(parent.getId(), vSubjectGroupParentNode.getChildren());// мапа
+																										// парент
+																										// -
+																										// ребенок
+							mapGroupActiviti.put(parent.getsID_Group_Activiti(), parent.getId());// мапа
+																									// группа
+																									// -
+																									// ид
+																									// парента
+						}
+					}
+				}
+			}
 
-        List<SubjectGroup> children = subjToNodeMap.get(groupFiltr); //достаем его детей
-        // children полный список первого уровня
-        if (children != null && !children.isEmpty()) {
+		}
 
-            //получаем только ид чилдренов полного списка детей первого уровня
-            final List<Long> idChildren = Lists.newArrayList(
-                    Collections2.transform(children, new Function<SubjectGroup, Long>() {
-                        @Override
-                        public Long apply(SubjectGroup subjectGroup) {
-                            return subjectGroup.getId();
-                        }
-                    }));
-            aChildResult.addAll(children);
-            getChildren(children, idChildren, subjToNodeMap, idParentList, deepLevel.intValue(), 1, aChildResult);
+		// Map<Long, List<SubjectGroup>> subjToNodeMapFiltr = new HashMap<>();
+		Long groupFiltr = mapGroupActiviti.get(sID_Group_Activiti); // достаем
+																	// ид
+																	// sID_Group_Activiti
+																	// которое
+																	// на вход
 
-            //subjToNodeMapFiltr.put(groupFiltr, aChildResult);
-        }
-        
-        //Получаем орг иерархию и людей
-        Map<SubjectGroup, Set<Map<String, String>>> subjUsers = new HashMap<>();
-        if(aChildResult!=null && !aChildResult.isEmpty()) {
-        	for(SubjectGroup subjectGroup: aChildResult) {
-        		List<Map<String, String>> aSubjectUser = usersService.getUsersByGroup(subjectGroup.getsID_Group_Activiti());
-        		Set<Map<String, String>>setUser = new LinkedHashSet<>(aSubjectUser); 
-        		subjUsers.put(subjectGroup, setUser);
-        	}
-        }
-        
-        SubjectGroupAndUser subjectGroupAndUser = new SubjectGroupAndUser();
-        subjectGroupAndUser.setaSubjectGroup(aChildResult);
-        subjectGroupAndUser.setaSubjectUser(Arrays.asList(subjUsers.values().toArray()));
-        
+		List<SubjectGroup> children = subjToNodeMap.get(groupFiltr); // достаем
+																		// его
+																		// детей
+		// children полный список первого уровня
+		if (children != null && !children.isEmpty()) {
 
-        return subjectGroupAndUser;
+			// получаем только ид чилдренов полного списка детей первого уровня
+			final List<Long> idChildren = Lists
+					.newArrayList(Collections2.transform(children, new Function<SubjectGroup, Long>() {
+						@Override
+						public Long apply(SubjectGroup subjectGroup) {
+							return subjectGroup.getId();
+						}
+					}));
+			aChildResult.addAll(children);
+			getChildren(children, idChildren, subjToNodeMap, idParentList, deepLevel.intValue(), 1, aChildResult);
 
-    }
+			// subjToNodeMapFiltr.put(groupFiltr, aChildResult);
+		}
 
-    /**
-     * Метод структуру иерархии согласно заданной глубины и группы
-     *
-     * @param aChildLevel результирующий список со всеми нужными нам детьми
-     * @param anID_ChildLevel ид детей уровня на котором мы находимся
-     * @param subjToNodeMap мапа соответствия всех ид перентов и список его
-     * детей
-     * @param anID_PerentAll ид всех перентов
-     * @param deepLevelRequested желаемая глубина
-     * @param deepLevelFact фактическая глубина
-     * @param result
-     * @return
-     */
-    public List<SubjectGroup> getChildren(List<SubjectGroup> aChildLevel, List<Long> anID_ChildLevel, Map<Long, List<SubjectGroup>> subjToNodeMap,
-            Set<Long> anID_PerentAll, int deepLevelRequested, int deepLevelFact, List<SubjectGroup> result) {
+		// Получаем орг иерархию и людей
+		Map<SubjectGroup, List<Map<String, String>>> subjUsers = new HashMap<>();
+		if (aChildResult != null && !aChildResult.isEmpty()) {
+			for (SubjectGroup subjectGroup : aChildResult) {
+				List<Map<String, String>> aSubjectUser = usersService
+						.getUsersByGroup(subjectGroup.getsID_Group_Activiti());
+				Set<Map<String, String>> setUser = new LinkedHashSet<>(aSubjectUser);
+				subjUsers.put(subjectGroup, Lists.newArrayList(setUser));
+			}
+		}
 
-        List<SubjectGroup> aChildLevel_Result = new ArrayList<>();
-        List<Long> anID_ChildLevel_Result = new ArrayList<>();
-        if (deepLevelRequested == 0) {
-            deepLevelRequested = 1000;
-        }
-        LOG.info("aChildLevel: " + aChildLevel.size() + " anID_ChildLevel: " + anID_ChildLevel);
-        if (deepLevelFact < deepLevelRequested) {
-            for (Long nID_ChildLevel : anID_ChildLevel) {
-                if (anID_PerentAll.contains(nID_ChildLevel)) {
-                    //LOG.info("nID_ChildLevel: " + nID_ChildLevel);
-                    aChildLevel_Result = subjToNodeMap.get(nID_ChildLevel);//достаем детей детей
-                    if (aChildLevel_Result != null && !aChildLevel_Result.isEmpty()) {
-                        LOG.info("nID_ChildLevel: " + nID_ChildLevel + " aChildLevel_Result: " + aChildLevel_Result.size());
-                        //получаем только ид чилдренов
-                        anID_ChildLevel_Result = Lists.newArrayList(
-                                Collections2.transform(aChildLevel_Result, new Function<SubjectGroup, Long>() {
-                                    @Override
-                                    public Long apply(SubjectGroup subjectGroup) {
-                                        return subjectGroup.getId();
-                                    }
-                                }));
-                        LOG.info("nID_ChildLevel: " + nID_ChildLevel + " anID_ChildLevel_Result: " + anID_ChildLevel_Result.size());
-                        result.addAll(aChildLevel_Result); //добавляем детей к общему списку детей
-                        LOG.info("result: " + result.size());
-                    }
-                }
-            }
-            deepLevelFact++;
-            LOG.info("deepLevelFact: " + deepLevelFact + " deepLevelRequested: " + deepLevelRequested);
-            if (deepLevelFact < deepLevelRequested) {
-                getChildren(aChildLevel_Result, anID_ChildLevel_Result, subjToNodeMap, anID_PerentAll, deepLevelRequested, deepLevelFact, result);
-            }
-        }
-        return result;
-    }
+		SubjectGroupAndUser subjectGroupAndUser = new SubjectGroupAndUser();
+		subjectGroupAndUser.setaSubjectGroup(aChildResult);
+		subjectGroupAndUser.setaSubjectUser((List)subjUsers.values());
+
+		return subjectGroupAndUser;
+
+	}
+
+	/**
+	 * Метод структуру иерархии согласно заданной глубины и группы
+	 *
+	 * @param aChildLevel
+	 *            результирующий список со всеми нужными нам детьми
+	 * @param anID_ChildLevel
+	 *            ид детей уровня на котором мы находимся
+	 * @param subjToNodeMap
+	 *            мапа соответствия всех ид перентов и список его детей
+	 * @param anID_PerentAll
+	 *            ид всех перентов
+	 * @param deepLevelRequested
+	 *            желаемая глубина
+	 * @param deepLevelFact
+	 *            фактическая глубина
+	 * @param result
+	 * @return
+	 */
+	public List<SubjectGroup> getChildren(List<SubjectGroup> aChildLevel, List<Long> anID_ChildLevel,
+			Map<Long, List<SubjectGroup>> subjToNodeMap, Set<Long> anID_PerentAll, int deepLevelRequested,
+			int deepLevelFact, List<SubjectGroup> result) {
+
+		List<SubjectGroup> aChildLevel_Result = new ArrayList<>();
+		List<Long> anID_ChildLevel_Result = new ArrayList<>();
+		if (deepLevelRequested == 0) {
+			deepLevelRequested = 1000;
+		}
+		LOG.info("aChildLevel: " + aChildLevel.size() + " anID_ChildLevel: " + anID_ChildLevel);
+		if (deepLevelFact < deepLevelRequested) {
+			for (Long nID_ChildLevel : anID_ChildLevel) {
+				if (anID_PerentAll.contains(nID_ChildLevel)) {
+					// LOG.info("nID_ChildLevel: " + nID_ChildLevel);
+					aChildLevel_Result = subjToNodeMap.get(nID_ChildLevel);// достаем
+																			// детей
+																			// детей
+					if (aChildLevel_Result != null && !aChildLevel_Result.isEmpty()) {
+						LOG.info("nID_ChildLevel: " + nID_ChildLevel + " aChildLevel_Result: "
+								+ aChildLevel_Result.size());
+						// получаем только ид чилдренов
+						anID_ChildLevel_Result = Lists.newArrayList(
+								Collections2.transform(aChildLevel_Result, new Function<SubjectGroup, Long>() {
+									@Override
+									public Long apply(SubjectGroup subjectGroup) {
+										return subjectGroup.getId();
+									}
+								}));
+						LOG.info("nID_ChildLevel: " + nID_ChildLevel + " anID_ChildLevel_Result: "
+								+ anID_ChildLevel_Result.size());
+						result.addAll(aChildLevel_Result); // добавляем детей к
+															// общему списку
+															// детей
+						LOG.info("result: " + result.size());
+					}
+				}
+			}
+			deepLevelFact++;
+			LOG.info("deepLevelFact: " + deepLevelFact + " deepLevelRequested: " + deepLevelRequested);
+			if (deepLevelFact < deepLevelRequested) {
+				getChildren(aChildLevel_Result, anID_ChildLevel_Result, subjToNodeMap, anID_PerentAll,
+						deepLevelRequested, deepLevelFact, result);
+			}
+		}
+		return result;
+	}
 
 }
