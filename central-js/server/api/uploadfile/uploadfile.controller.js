8c9f309541cbcbd1877e6125dd6f2fd055aae603
Igor Jarmak <igor.jarmak@gmail.com>
2016-11-20 08:24:19 +0200

    https://github.com/e-government-ua/i/issues/1422 refactored, fixed region err

--- central-js/server/api/uploadfile/uploadfile.controller.js
+++ central-js/server/api/uploadfile/uploadfile.controller.js
@@ -6,5 +6,8 @@ var request = require('request')
 module.exports.uploadProxy = function (req, res) {
   var sHost = req.region.sHost;
   var sURL = sHost + '/service/object/file/upload_file_to_redis';
-  proxy.upload(req, res, sURL);
+
+  proxy.upload(req, res, sURL, function(error){
+    res.status(500).send({ error: error });
+  });
 };
