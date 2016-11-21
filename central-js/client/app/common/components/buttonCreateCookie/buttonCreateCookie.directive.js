c337a7f311aded37882be66f092c8ce06e1d0137
Igor Jarmak <igor.jarmak@gmail.com>
2016-11-21 21:26:54 +0200

    auth mock fixed

--- central-js/client/app/common/components/buttonCreateCookie/buttonCreateCookie.directive.js
+++ central-js/client/app/common/components/buttonCreateCookie/buttonCreateCookie.directive.js
@@ -1,83 +1,87 @@
-/**
- * Created by Igor on 5/4/2016.
- */
-'use strict';
+(function () {
+  'use strict';
 
-angular.module('app')
-  .directive('buttonCreateCookie', ['$location',
-    function (location) {
-      return {
-        restrict: 'E',
-        scope: {
-          ngModel: '='
-        },
-        templateUrl: 'app/common/components/buttonCreateCookie/buttonCreateCookie.directive.html',
-        link: function buttonEnableCoockie(scope) {
-          var cookieName = 'authMock';
+  angular.module('app')
+    .directive('buttonCreateCookie', buttonCreateCookie);
 
-          scope.model = {
-            enable: typeof getCookie(cookieName) !== 'undefined',
-          };
+  function buttonCreateCookie() {
+    return {
+      restrict: 'EA',
+      scope: {},
+      templateUrl: 'app/common/components/buttonCreateCookie/buttonCreateCookie.directive.html',
+      controller: ButtonCreateCookieController,
+      controllerAs: 'vm',
+      bindToController: true
+    };
+  }
 
-          scope.onChange = function (arg) {
-            if (arg) {
-              setCookie(cookieName, 'MockUser', {
-                path: "/",
-                expires: 180
-              });
-            } else if (!arg) {
-              deleteCookie(cookieName, 'MockUser');
-            }
-          };
+  function ButtonCreateCookieController($scope) {
+    var vm = this;
+    var cookieName = 'authMock';
 
-          scope.isVisible = function () {
-            if (this.$root.profile.isKyivCity) return false;
-            //return location.host() === 'localhost' || location.host() === 'test.igov.org.ua';
-            return getCookie('bServerTest') === 'true';
-          };
+    vm.onChange = onChange;
 
-          function getCookie(name) {
-            var matches = document.cookie.match(new RegExp(
-              "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
-            ));
-            return matches ? decodeURIComponent(matches[1]) : undefined;
-          }
+    vm.enable = typeof getCookie(cookieName) !== 'undefined';
 
-          function setCookie(name, value, options) {
-            options = options || {};
+    function onChange() {
+      vm.enable = !vm.enable;
+      if (vm.enable) {
+        setCookie(cookieName, 'MockUser', {
+          path: "/",
+          expires: 180
+        });
+      } else if (!vm.enable) {
+        deleteCookie(cookieName, 'MockUser');
+      }
+    }
 
-            var expires = options.expires;
+    vm.isVisible = function () {
+      if ($scope.$root.profile.isKyivCity) return false;
+      return getCookie('bServerTest') === 'true';
+    };
 
-            if (typeof expires == "number" && expires) {
-              var d = new Date();
-              d.setTime(d.getTime() + expires * 1000);
-              expires = options.expires = d;
-            }
-            if (expires && expires.toUTCString) {
-              options.expires = expires.toUTCString();
-            }
 
-            value = encodeURIComponent(value);
+    function getCookie(name) {
+      var matches = document.cookie.match(new RegExp(
+        "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+      ));
+      return matches ? decodeURIComponent(matches[1]) : undefined;
+    }
 
-            var updatedCookie = name + "=" + value;
 
-            for (var propName in options) {
-              updatedCookie += "; " + propName;
-              var propValue = options[propName];
-              if (propValue !== true) {
-                updatedCookie += "=" + propValue;
-              }
-            }
-            document.cookie = updatedCookie;
-          }
+    function setCookie(name, value, options) {
+      options = options || {};
 
-          function deleteCookie(name, value) {
-            setCookie(name, value, {
-              expires: -1,
-              path: "/"
-            });
-          }
+      var expires = options.expires;
+
+      if (typeof expires == "number" && expires) {
+        var d = new Date();
+        d.setTime(d.getTime() + expires * 1000);
+        expires = options.expires = d;
+      }
+      if (expires && expires.toUTCString) {
+        options.expires = expires.toUTCString();
+      }
+
+      value = encodeURIComponent(value);
+
+      var updatedCookie = name + "=" + value;
+
+      for (var propName in options) {
+        updatedCookie += "; " + propName;
+        var propValue = options[propName];
+        if (propValue !== true) {
+          updatedCookie += "=" + propValue;
         }
       }
-    }]
-);
+      document.cookie = updatedCookie;
+    }
+
+    function deleteCookie(name, value) {
+      setCookie(name, value, {
+        expires: -1,
+        path: "/"
+      });
+    }
+  }
+})();
