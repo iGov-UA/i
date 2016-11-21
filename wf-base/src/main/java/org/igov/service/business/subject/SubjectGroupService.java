1d305f5fadf815d668ade14af96856f710ed06eb
Dashenko-Inna <dascenkoinna@gmail.com>
2016-11-21 16:31:28 +0200

    #1404 - fix Signed-off-by: Dashenko-Inna <dascenkoinna@gmail.com>

--- wf-base/src/main/java/org/igov/service/business/subject/SubjectGroupService.java
+++ wf-base/src/main/java/org/igov/service/business/subject/SubjectGroupService.java
@@ -124,7 +124,7 @@ public class SubjectGroupService {
 								public boolean apply(SubjectUser subjectUser) {
 									// получить только отфильтрованный список по
 									// sFind в фио
-									return subjectUser.getsFirstName().contains(sFind.toLowerCase());
+									return subjectUser.getsFirstName().toLowerCase().contains(sFind.toLowerCase());
 								}
 							}));
 					subjUsers.put(subjectGroup, processUserFiltr);
