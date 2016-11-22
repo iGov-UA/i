0713d01a32625ed70e29ad70d50f4f511e971660
dpekach <dImA1995>
2016-11-22 13:11:30 +0200

    slight refactoring is performed(for issue #1428)

---
+++ wf-base/src/main/java/org/igov/service/business/action/execute/old/ActionExecuteOldService.java
@@ -0,0 +1,22 @@
+package org.igov.service.business.action.execute.old;
+
+import org.hibernate.Session;
+import org.igov.model.action.execute.item.ActionExecuteOld;
+import org.joda.time.DateTime;
+
+import java.util.List;
+
+/**
+ * Created by dpekach on 19.11.16.
+ */
+public interface ActionExecuteOldService {
+    Long setActionExecuteOld(Long nID_ActionExecuteStatus,
+                             DateTime oDateMake, DateTime oDateEdit, Integer nTry,
+                             String sObject, String sMethod, byte[] soRequest, String smParam, String sReturn, Session session);
+
+    List<ActionExecuteOld> getActionExecuteOldListByCriteria(Integer nRowsMax, String sMethodMask, String asID_Status,
+                                                             Integer nTryMax, Long nID, Session session);
+
+    void moveActionExecuteOld(Integer nRowsMax, String sMethodMask, String asID_Status, Integer nTryMax,
+                              Long nID, Session session);
+}
