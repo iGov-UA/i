8c282d77896347f0313eb5be5236b62f954a1f8c
bw <bw@bw-laptop>
2016-11-28 01:45:13 +0200

    https://github.com/e-government-ua/i/issues/1405 5) Создать листенер DocumentInit_iDoc со следующими функциями:

---
+++ wf-base/src/main/java/org/igov/service/business/action/task/listener/doc/DocumentInit_iDoc.java
@@ -0,0 +1,55 @@
+package org.igov.service.business.action.task.listener.doc;
+
+
+import org.activiti.engine.FormService;
+import org.activiti.engine.RuntimeService;
+import org.activiti.engine.TaskService;
+import org.activiti.engine.delegate.DelegateExecution;
+import org.activiti.engine.delegate.DelegateTask;
+import org.activiti.engine.delegate.TaskListener;
+import org.igov.io.GeneralConfig;
+import org.igov.io.web.HttpRequester;
+import org.igov.service.business.action.task.core.AbstractModelTask;
+import org.igov.service.business.document.DocumentStepService;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component("DocumentInit_iDoc")
+public class DocumentInit_iDoc extends AbstractModelTask implements TaskListener {
+
+    private final static Logger LOG = LoggerFactory.getLogger(CreateDocument_UkrDoc.class);
+
+    @Autowired
+    private DocumentStepService oDocumentStepService;
+    
+    @Autowired
+    GeneralConfig generalConfig;
+
+    @Autowired
+    RuntimeService runtimeService;
+
+    @Autowired
+    FormService formService;
+
+    @Autowired
+    TaskService taskService;
+    
+    @Autowired
+    HttpRequester httpRequester;
+    
+    @Override
+    public void notify(DelegateTask delegateTask) {
+        
+        DelegateExecution execution = delegateTask.getExecution();
+        try {
+            oDocumentStepService.checkDocumentInit(execution.getProcessInstanceId());
+        } catch (Exception oException) {
+            LOG.error("", oException);
+            throw oException;
+        }
+    }
+
+}
