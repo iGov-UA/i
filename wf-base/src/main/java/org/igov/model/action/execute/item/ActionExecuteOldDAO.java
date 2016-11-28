b0302c5d33d5bde57084d28f411e4bc67a6f2f07
dpekach <dImA1995>
2016-11-28 15:03:32 +0200

    #1428 Changelist(to be fixed)

--- wf-base/src/main/java/org/igov/model/action/execute/item/ActionExecuteOldDAO.java
+++ wf-base/src/main/java/org/igov/model/action/execute/item/ActionExecuteOldDAO.java
@@ -1,15 +1,14 @@
 package org.igov.model.action.execute.item;
 
-import java.util.List;
-
+import org.hibernate.Session;
 import org.igov.model.core.EntityDao;
-import org.joda.time.DateTime;
+
+import java.util.List;
 
 public interface ActionExecuteOldDAO extends EntityDao<Long, ActionExecuteOld> {
 	
 	ActionExecuteOld getActionExecute(Long id);
 	List<ActionExecuteOld> getAllActionExecutes();
-	Long setActionExecuteOld(Long nID_ActionExecuteStatus, DateTime oDateMake, DateTime oDateEdit, Integer nTry, String sObject, String sMethod, byte[] soRequest, String smParam, String sReturn);
-	List<ActionExecuteOld> getActionExecute(Integer nRowsMax, String sMethodMask, String asID_Status, Integer nTryMax, Long nID);
-	void moveActionExecuteOld(Integer nRowsMax, String sMethodMask, String asID_Status, Integer nTryMax, Long nID);
+
+	Session getSessionForService();
 }
