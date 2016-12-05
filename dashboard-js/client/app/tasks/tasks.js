e22f46869d67b9a649aed77b4899386fdfccce27
antonlegkiy <anton.legkiy@gmail.com>
2016-12-05 13:22:27 +0200

    #1440

--- dashboard-js/client/app/tasks/tasks.js
+++ dashboard-js/client/app/tasks/tasks.js
@@ -118,9 +118,29 @@
             'tasks',
             'documentRights',
             function (oTask, tasks, documentRights) {
-              if(documentRights){
+              if(documentRights) {
                 return tasks.getDocumentStepLogins(oTask.processInstanceId);
               }
+            }
+          ],
+          processSubject: [
+            'oTask',
+            'tasks',
+            'documentRights',
+            'taskForm',
+            function (oTask, tasks, documentRights, taskForm) {
+              // if(documentRights) {
+                var nDeepLevel;
+                for(var i =0; i<taskForm.length; i++) {
+                  if(taskForm[i].id === 'nDeepLevel') {
+                    nDeepLevel = taskForm[i].value;
+                    break;
+                  }
+                }
+                if(nDeepLevel){
+                  return tasks.getProcessSubject(oTask.processInstanceId, nDeepLevel);
+                }
+              // }
             }
           ]
         }
--- dashboard-js/client/components/filters/splitDate.filter.js
+++ dashboard-js/client/components/filters/splitDate.filter.js
@@ -4,6 +4,8 @@ angular.module('dashboardJsApp')
       return  value.split('.')[0];
     }
   })
+
+  // отображает дату формата d.m.yyyy как dd/mm/yyyy
   .filter('checkDate', function () {
     return function (value) {
       var split = value.split('.');
@@ -12,5 +14,21 @@ angular.module('dashboardJsApp')
       else if(split[0].length === 1 && split[1].length === 2) return 0 + split[0] + '/' + split[1] + '/' + split[2];
       else if(split[0].length === 2 && split[1].length === 2) return value;
       else if(split.length !== 3) return value;
+    }
+  })
+
+  // отображает дату формата yyyy-mm-dd hh:mm:ss как yyyy-mm-dd
+  .filter('removeHoursFromDate', function () {
+    return function (value) {
+      var date = value.split(' ');
+      if(date.length !== 2) return value;
+      else if(date.length === 2){
+        var check = date[0].split('-');
+        if(check.length === 3) {
+          if(check[0].length === 4 && check[1].length === 2 && check[2].length === 2) return date[0];
+        } else {
+          return value
+        }
+      }
     }
   });
