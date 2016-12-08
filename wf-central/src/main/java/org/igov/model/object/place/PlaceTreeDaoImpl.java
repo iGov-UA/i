91c114ec2d489dcbc2e9cf8c1e1dfe5f883b82f7
Kovylin Yegor <egor.kovylin@privatbank.ua>
2016-12-08 17:57:15 +0200

    #1432: SetTasksListener fix

--- wf-central/src/main/java/org/igov/model/object/place/PlaceTreeDaoImpl.java
+++ wf-central/src/main/java/org/igov/model/object/place/PlaceTreeDaoImpl.java
@@ -1,11 +1,13 @@
 package org.igov.model.object.place;
 
 import org.igov.model.core.GenericEntityDao;
+import org.springframework.stereotype.Repository;
 
 /**
  *
  * @author Kovilin
  */
+@Repository
 public class PlaceTreeDaoImpl extends GenericEntityDao<Long, PlaceTree> implements PlaceTreeDao{
 
     public PlaceTreeDaoImpl() {
