f0d5db87f8b57934a48a9fd7efb8ab6aa7e31459
dpekach <dImA1995>
2016-12-12 15:02:32 +0200

    #1441, final commit: bug is fixed, unnecessary logs are deleted, incorrect translation is corrected

--- wf-base/src/main/java/org/igov/service/business/action/task/listener/FileTaskInheritance.java
+++ wf-base/src/main/java/org/igov/service/business/action/task/listener/FileTaskInheritance.java
@@ -53,6 +53,8 @@ public class FileTaskInheritance extends AbstractModelTask implements TaskListen
             asID_Attachment_ToAdd = getInheritedAttachmentIdsFromTask(attachments,
                     sInheritedAttachmentsIds);
             addAttachmentsToCurrentTask(asID_Attachment_ToAdd, oTask);*/
+
+            //Issue #1441: next 2 lines should be commented out or logic of addAttachmentsToCurrentTask() should be rewritten;
 //            List<Attachment> attachments = findAttachments(sInheritedAttachmentsIds, oExecution.getId());
 //            addAttachmentsToCurrentTask(attachments, oTask);
         } catch (Exception oException) {
@@ -79,37 +81,39 @@ public class FileTaskInheritance extends AbstractModelTask implements TaskListen
 
     }
 
-//    private void addAttachmentsToCurrentTask(List<Attachment> attachmentsToAdd,
-//            DelegateTask task) {
-//        final String METHOD_NAME = "addAttachmentsToCurrentTask(List<Attachment> attachmentsToAdd, DelegateExecution execution)";
-//        LOG.trace("Entering method '{}'", METHOD_NAME);
-//
-//        //TaskService taskService = task.getExecution().getEngineServices()
-//        //       .getTaskService();
-//        int n = 0;
-//        LOG.info("Task id: {}", task.getId());
-//        List<Attachment> attachmentList = taskService.getTaskAttachments(task.getId());
-//        LOG.info("After getting attachments list; size = {}", attachmentList.size());
-//        for(Attachment attachment: attachmentList) {
-//            LOG.info("Attachment description: {} attachment ID {}", attachment.getDescription(), attachment.getId());
-//        }
-//        for (Attachment attachment : attachmentsToAdd) {
-//            LOG.info("(n={},task.getId()={},task.getExecution().getProcessInstanceId()={},attachment.getName()={},attachment.getDescription()={},attachment.getId()={})"
-//                    ,n++, task.getId(), task.getExecution().getProcessInstanceId(),attachment.getName(),attachment.getDescription(), attachment.getId());
-//            Attachment newAttachment = taskService.createAttachment(
-//                    attachment.getType(), task.getId(),
-//                    task.getExecution().getProcessInstanceId(), attachment.getName(),
-//                    attachment.getDescription(),
-//                    taskService.getAttachmentContent(attachment.getId()));
-//            LOG.info(MessageFormat
-//                    .format("Created new attachment for the task {0} with ID {1} from the attachment with ID {2}",
-//                            task.getId(), newAttachment.getId(),
-//                            attachment.getId()));
-//        }
-//
-//        LOG.trace("Exiting method '{}'", METHOD_NAME);
-//    }
 
+    //#1441 fix:
+    /*private void addAttachmentsToCurrentTask(List<Attachment> attachmentsToAdd,
+            DelegateTask task) {
+        final String METHOD_NAME = "addAttachmentsToCurrentTask(List<Attachment> attachmentsToAdd, DelegateExecution execution)";
+        LOG.trace("Entering method '{}'", METHOD_NAME);
+
+        //TaskService taskService = task.getExecution().getEngineServices()
+        //       .getTaskService();
+        int n = 0;
+        LOG.info("Task id: {}", task.getId());
+        List<Attachment> attachmentList = taskService.getTaskAttachments(task.getId());
+        LOG.info("After getting attachments list; size = {}", attachmentList.size());
+        for(Attachment attachment: attachmentList) {
+            LOG.info("Attachment description: {} attachment ID {}", attachment.getDescription(), attachment.getId());
+        }
+        for (Attachment attachment : attachmentsToAdd) {
+            LOG.info("(n={},task.getId()={},task.getExecution().getProcessInstanceId()={},attachment.getName()={},attachment.getDescription()={},attachment.getId()={})"
+                    ,n++, task.getId(), task.getExecution().getProcessInstanceId(),attachment.getName(),attachment.getDescription(), attachment.getId());
+            Attachment newAttachment = taskService.createAttachment(
+                    attachment.getType(), task.getId(),
+                    task.getExecution().getProcessInstanceId(), attachment.getName(),
+                    attachment.getDescription(),
+                    taskService.getAttachmentContent(attachment.getId()));
+            LOG.info(MessageFormat
+                    .format("Created new attachment for the task {0} with ID {1} from the attachment with ID {2}",
+                            task.getId(), newAttachment.getId(),
+                            attachment.getId()));
+        }
+
+        LOG.trace("Exiting method '{}'", METHOD_NAME);
+    }
+*/
 
     
     @Deprecated
