d5d8b7ce5965c132824c20bdfa4dfc5dd2956d48
dpekach <dImA1995>
2016-12-13 23:20:25 +0200

    #1441: reopened issue

--- wf-base/src/main/java/org/igov/service/business/action/task/listener/FileTaskInheritance.java
+++ wf-base/src/main/java/org/igov/service/business/action/task/listener/FileTaskInheritance.java
@@ -65,19 +65,26 @@ public class FileTaskInheritance extends AbstractModelTask implements TaskListen
             LOG.info("Current attachments size: {}", currentAttachments.size());
 
             for(Attachment attachment: currentAttachments) {
-                LOG.info("Attachment info: {}\n; attachment ID: {}", attachment.getDescription(), attachment.getId());
+                LOG.info("CurrentAttachment: Attachment info: {}\n; attachment ID: {}", attachment.getDescription(), attachment.getId());
             }
 
             List<Attachment> attachments = findAttachments(sInheritedAttachmentsIds, oExecution.getId());
             for(Attachment attachment: attachments) {
-                LOG.info("Attachment info: {}\n; attachment ID: {}", attachment.getDescription(), attachment.getId());
+                LOG.info("Attachments: Attachment info: {}\n; attachment ID: {}", attachment.getDescription(), attachment.getId());
             }
 
 
             for(Attachment attachment: currentAttachments) {
-                if(attachments.contains(attachment))
-                    attachments.remove(attachment);
+                if(attachments.contains(attachment)){
+                    boolean deleted = attachments.remove(attachment);
+                    if(deleted) {
+                        LOG.info("Duplicate is successfully deleted");
+                    }
+                }
+
             }
+
+            LOG.info("Attachments: attachments size={}", attachments.size());
 
             addAttachmentsToCurrentTask(attachments, oTask);
         } catch (Exception oException) {
