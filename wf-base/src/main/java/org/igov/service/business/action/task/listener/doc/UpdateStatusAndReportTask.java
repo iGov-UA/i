464259f291967218d3973b9e63816d81f54be4dd
Kovylin Yegor <egor.kovylin@privatbank.ua>
2016-12-16 20:44:22 +0200

    feedback fix

--- wf-base/src/main/java/org/igov/service/business/action/task/listener/doc/UpdateStatusAndReportTask.java
+++ wf-base/src/main/java/org/igov/service/business/action/task/listener/doc/UpdateStatusAndReportTask.java
@@ -8,8 +8,12 @@ package org.igov.service.business.action.task.listener.doc;
 import org.activiti.engine.delegate.DelegateTask;
 import org.activiti.engine.delegate.Expression;
 import org.activiti.engine.delegate.TaskListener;
+import org.igov.model.process.ProcessSubject;
 import static org.igov.service.business.action.task.core.AbstractModelTask.getStringFromFieldExpression;
+import org.igov.service.business.action.task.systemtask.mail.Abstract_MailTaskCustom;
 import org.igov.service.business.process.ProcessSubjectService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -23,18 +27,26 @@ public class UpdateStatusAndReportTask implements TaskListener {
     private Expression sID_ProcessSubjectStatus;
     
     private Expression sReport; 
+        
+    static final transient Logger LOG = LoggerFactory.getLogger(UpdateStatusAndReportTask.class);
 
     @Autowired
     private ProcessSubjectService processSubjectService;
 
     @Override
     public void notify(DelegateTask delegateTask) {
+        
+        LOG.info("UpdateStatusTask start....");
+        
         String sID_ProcessSubjectStatus_Value
                 = getStringFromFieldExpression(this.sID_ProcessSubjectStatus, delegateTask.getExecution());
         
         String sReport_Value = getStringFromFieldExpression(this.sReport, delegateTask.getExecution());
+        LOG.info("sReport bewore method work: " + sReport_Value);
         
-        processSubjectService.setProcessSubjectStatusAndReport(delegateTask.getExecutionId(), sID_ProcessSubjectStatus_Value, sReport_Value);
+        ProcessSubject oProcessSubject = processSubjectService.setProcessSubjectStatusAndReport(delegateTask.getExecutionId(), sID_ProcessSubjectStatus_Value, sReport_Value);
+        
+        LOG.info("sReport after method work: " + oProcessSubject.getsReport());
     }
 
 }
