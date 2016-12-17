6184ced89b1b913373c2ec56bc9f084bebd0abfd
Kovylin Yegor <egor.kovylin@privatbank.ua>
2016-12-17 18:42:54 +0200

    feedback fix

--- wf-central/src/main/java/org/igov/service/business/subject/SubjectMessageService.java
+++ wf-central/src/main/java/org/igov/service/business/subject/SubjectMessageService.java
@@ -352,16 +352,14 @@ public class SubjectMessageService {
                 }
                 subjectMessage.setDate(new DateTime());
                 subjectMessage.setsSubjectInfo(sAuthorFIO);
-                if (sMail != null) {
-                    subjectMessage.setMail(sMail);
-//                    createSubjectContact(sMail, subjectMessage.getoSubject());
-//                    LOG.info("1TEST_sMail: " + sMail);
-//                    subjectContactDao.saveOrUpdate(createSubjectContact(sMail, subjectMessage.getoSubject()));
-//                    LOG.info("1sMail: " + subjectMessage.getMail());
-//                    LOG.info("2oMail: " + subjectMessage.getoMail().getsValue());
-                }
+                
                 if (nID_Subject != null) {
                     subjectMessage.setId_subject(nID_Subject);
+                }
+                if (sMail != null) {
+                    subjectMessage.setMail(sMail);
+                    createSubjectContact(sMail, subjectMessage.getoSubject());
+                    subjectContactDao.saveOrUpdate(createSubjectContact(sMail, subjectMessage.getoSubject()));
                 }
                 subjectMessage = subjectMessageDao.saveOrUpdate(subjectMessage);
                 messageFeedback.setoSubjectMessage(subjectMessage);
