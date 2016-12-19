b6b463f5f23aa48d94a8b26276ac826adeb80a21
Kovylin Yegor <egor.kovylin@privatbank.ua>
2016-12-19 14:48:02 +0200

    feedback fix

--- wf-base/src/main/java/org/igov/service/business/action/task/listener/doc/SetTasks.java
+++ wf-base/src/main/java/org/igov/service/business/action/task/listener/doc/SetTasks.java
@@ -30,6 +30,7 @@ public class SetTasks implements TaskListener {
     private Expression sDateDoc;
     private Expression sApplicant;
     private Expression nCountAttach;
+    private Expression asUrgently;
     private Expression sContent;
     private Expression sNote;
     private Expression sDoc1;
@@ -37,7 +38,6 @@ public class SetTasks implements TaskListener {
     private Expression asTypeResolution;
     private Expression sTextResolution;
     private Expression sDateExecution;
-    /*private Expression sTextReport;*/
     
     @Autowired
     private ProcessSubjectService processSubjectService;
@@ -53,27 +53,17 @@ public class SetTasks implements TaskListener {
                 = getStringFromFieldExpression(this.sID_Attachment, delegateTask.getExecution());
         String sContent_Value
                 = getStringFromFieldExpression(this.sContent, delegateTask.getExecution());
-        /*
-        
-        String sTypeDoc_Value
-                = getStringFromFieldExpression(this.sTypeDoc, delegateTask.getExecution());
-        String sID_Order_GovPublic_Value
-                = getStringFromFieldExpression(this.sID_Order_GovPublic, delegateTask.getExecution());*/
-        
-        String sDateRegistration_Value
-                = getStringFromFieldExpression(this.sDateRegistration, delegateTask.getExecution());
         String sAutorResolution_Value
                 = getStringFromFieldExpression(this.sAutorResolution, delegateTask.getExecution());
         String sDateExecution_Value
                 = getStringFromFieldExpression(this.sDateExecution, delegateTask.getExecution());
-        String sAsTypeResolution_Value
-                = getStringFromFieldExpression(this.asTypeResolution, delegateTask.getExecution());
-        String sTextResolution_Value
-                = getStringFromFieldExpression(this.sTextResolution, delegateTask.getExecution());
-        String sDoc1_Value
-                = getStringFromFieldExpression(this.sDoc1, delegateTask.getExecution());
-        
-        /*String sDateDoc_Value
+        String sTypeDoc_Value
+                = getStringFromFieldExpression(this.sTypeDoc, delegateTask.getExecution());
+        String sID_Order_GovPublic_Value
+                = getStringFromFieldExpression(this.sID_Order_GovPublic, delegateTask.getExecution());
+        String sDateRegistration_Value
+                = getStringFromFieldExpression(this.sDateRegistration, delegateTask.getExecution());
+        String sDateDoc_Value
                 = getStringFromFieldExpression(this.sDateDoc, delegateTask.getExecution());
         String sApplicant_Value
                 = getStringFromFieldExpression(this.sApplicant, delegateTask.getExecution());
@@ -83,33 +73,32 @@ public class SetTasks implements TaskListener {
                 = getStringFromFieldExpression(this.sNote, delegateTask.getExecution());
         String sAsUrgently_Value
                 = getStringFromFieldExpression(this.asUrgently, delegateTask.getExecution());
-        String sTextReport_Value
-                = getStringFromFieldExpression(this.sTextReport, delegateTask.getExecution());*/
-                
+        String sAsTypeResolution_Value
+                = getStringFromFieldExpression(this.asTypeResolution, delegateTask.getExecution());
+        String sTextResolution_Value
+                = getStringFromFieldExpression(this.sTextResolution, delegateTask.getExecution());
+        String sDoc1_Value
+                = getStringFromFieldExpression(this.sDoc1, delegateTask.getExecution());
+                        
         Map<String, String> mParam = new HashMap<>();
         
         mParam.put("sTaskProcessDefinition", sTaskProcessDefinition_Value);
         mParam.put("sID_Attachment", sID_Attachment_Value);
         mParam.put("sContent", sContent_Value);
-        //mParam.put("sTypeDoc", sTypeDoc_Value);
-        //mParam.put("sID_Order_GovPublic", sID_Order_GovPublic_Value);
-        mParam.put("sDateRegistration", sDateRegistration_Value);
         mParam.put("sAutorResolution", sAutorResolution_Value);
         mParam.put("sDateExecution", sDateExecution_Value);
+        mParam.put("sTypeDoc", sTypeDoc_Value);
+        mParam.put("sID_Order_GovPublic", sID_Order_GovPublic_Value);
+        mParam.put("sDateRegistration", sDateRegistration_Value);
+        mParam.put("sDateDoc", sDateDoc_Value);
+        mParam.put("sApplicant", sApplicant_Value);
+        mParam.put("nCountAttach", snCountAttach_Value);
+        mParam.put("sNote", sNote_Value);
+        mParam.put("asUrgently", sAsUrgently_Value);
         mParam.put("asTypeResolution", sAsTypeResolution_Value);
         mParam.put("sTextResolution", sTextResolution_Value);
         mParam.put("sDoc1", sDoc1_Value);
-        
-        //mParam.put("sDateDoc", sDateDoc_Value);
-        //mParam.put("sApplicant", sApplicant_Value);
-        //mParam.put("nCountAttach", snCountAttach_Value);
-        //mParam.put("sNote", sNote_Value);
-        //mParam.put("asUrgently", sAsUrgently_Value);
 
-        //mParam.put("sTextReport", sTextReport_Value);
-        
-        /*processSubjectService.setProcessSubjects(sTaskProcessDefinition_Value, sID_Attachment_Value, sContent_Value, 
-                sAutorResolution_Value, sTextResolution_Value, sDateExecution_Value, delegateTask.getExecution().getId());*/
         LOG.info("SetTasks start with param..." + mParam);
         processSubjectService.setProcessSubjects(mParam, delegateTask.getExecution().getId());
                 
