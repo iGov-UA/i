3528d56cf72f63fd90523c8e4502bc35dfcfed24
Kovylin Yegor <egor.kovylin@privatbank.ua>
2017-01-01 23:41:41 +0200

    test new attach

--- analytic/src/test/java/org/igov/io/db/kv/analytic/BytesDataStorageTest.java
+++ analytic/src/test/java/org/igov/io/db/kv/analytic/BytesDataStorageTest.java
@@ -10,7 +10,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import org.igov.io.db.kv.statical.exceptions.RecordNotFoundException;
 
-//@Ignore
+@Ignore
 @RunWith(SpringJUnit4ClassRunner.class)
 //@ContextConfiguration(locations = {
 //		"/META-INF/spring/org-igov-io-db-kv-analytic-context.xml"})
