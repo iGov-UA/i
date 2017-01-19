703346620edd4555c088370e2dd8ae8f6f341608
olga <olga@olga-pc>
2017-01-19 15:41:27 +0200

    Merge origin/test into test
    Conflicts:
    	central-js/server/api/process-form/form.controller.js
    	wf-base/src/main/java/org/igov/service/business/action/task/core/AbstractModelTask.java
    	wf-base/src/main/java/org/igov/service/business/action/task/listener/doc/SetTasks.java
    	wf-base/src/main/java/org/igov/service/business/flow/FlowService.java
    	wf-base/src/main/java/org/igov/service/business/process/ProcessSubjectService.java
    	wf-central/src/main/java/org/igov/service/business/action/item/ServiceTagService.java

--- wf-central/src/main/java/org/igov/service/business/action/item/ServiceTagService.java
+++ wf-central/src/main/java/org/igov/service/business/action/item/ServiceTagService.java
@@ -13,8 +13,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
