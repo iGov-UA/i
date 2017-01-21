494b87e785b535532e1f5379f97739f28160c81d
Kovylin Yegor <egor.kovylin@privatbank.ua>
2017-01-21 20:16:34 +0200

    testing new attach schema

--- wf-base/src/main/java/org/igov/service/business/action/task/core/AbstractModelTask.java
+++ wf-base/src/main/java/org/igov/service/business/action/task/core/AbstractModelTask.java
@@ -608,7 +608,7 @@ public abstract class AbstractModelTask {
 
                                                                                 byte[] aByteFile = oMultipartFile.getBytes();
 
-                                                                                String sNewTableElemValue = oAttachmetService.createAttachment(oExecution.getProcessInstanceId(), oFileValue.get("id"),
+                                                                                String sNewTableElemValue = oAttachmetService.createAttachment(oExecution.getProcessInstanceId(), oValue.get("id"),
                                                                                         (String) oJsonTableFile.get("sFileNameAndExt"),
                                                                                         (boolean) oJsonTableFile.get("bSigned"), "Mongo", "text/html",
                                                                                         aAttribute, aByteFile, false);
