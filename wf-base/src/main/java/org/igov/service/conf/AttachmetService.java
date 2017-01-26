c08f6353a3c787cd2fca225adf7a6e2488418b79
Kovylin Yegor <egor.kovylin@privatbank.ua>
2017-01-26 19:09:55 +0200

    Add encoding and transliteration for new attachement service

--- wf-base/src/main/java/org/igov/service/conf/AttachmetService.java
+++ wf-base/src/main/java/org/igov/service/conf/AttachmetService.java
@@ -71,17 +71,8 @@ public class AttachmetService {
         SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
         SimpleDateFormat dtf = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
         
-        String translit = "ололошечка";
-        LOG.info("translit text: " + sTextTranslit(translit));
-        
-       // try {
-            sFileNameAndExt = (sFileNameAndExt != null)?
-                    transliterate(getFileName(sFileNameAndExt))
-                    //new String(getFileName(sFileNameAndExt).getBytes("ASCII"), "UTF-8")
+        sFileNameAndExt = (sFileNameAndExt != null)?transliterate(getFileName(sFileNameAndExt.replace(" ", "_")))
                     + "." + getFileExtention(sTextTranslit(sFileNameAndExt)):sFileNameAndExt;
-        /*} catch (UnsupportedEncodingException ex) {
-            LOG.info("File name convert error: " + ex);
-        }*/
         
         LOG.info(" ----------- " + dtf.format(new Date()) + " ----------- ");
         LOG.info("createAttachment nID_Process: " + nID_Process);
@@ -229,8 +220,8 @@ public class AttachmetService {
     
     public static String transliterate(String message)
     {
-        char[] abcCyr =   {' ','а','б','в','г','д','е','ё', 'ж','з','и','й','к','л','м','н','о','п','р','с','т','у','ф','х', 'ц','ч', 'ш','щ','ъ','ы','ь','э', 'ю','я','А','Б','В','Г','Д','Е','Ё', 'Ж','З','И','Й','К','Л','М','Н','О','П','Р','С','Т','У','Ф','Х', 'Ц', 'Ч','Ш', 'Щ','Ъ','Ы','Б','Э','Ю','Я','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
-        String[] abcLat = {" ","a","b","v","g","d","e","e","zh","z","i","y","k","l","m","n","o","p","r","s","t","u","f","h","ts","ch","sh","sch", "","i", "","e","ju","ja","A","B","V","G","D","E","E","Zh","Z","I","Y","K","L","M","N","O","P","R","S","T","U","F","H","Ts","Ch","Sh","Sch", "","I", "","E","Ju","Ja","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
+        char[] abcCyr =   {' ', '_','а','б','в','г','д','е','ё', 'ж','з','и','й','к','л','м','н','о','п','р','с','т','у','ф','х', 'ц','ч', 'ш','щ','ъ','ы','ь','э', 'ю','я','А','Б','В','Г','Д','Е','Ё', 'Ж','З','И','Й','К','Л','М','Н','О','П','Р','С','Т','У','Ф','Х', 'Ц', 'Ч','Ш', 'Щ','Ъ','Ы','Б','Э','Ю','Я','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+        String[] abcLat = {" ", "_", "a","b","v","g","d","e","e","zh","z","i","y","k","l","m","n","o","p","r","s","t","u","f","h","ts","ch","sh","sch", "","i", "","e","ju","ja","A","B","V","G","D","E","E","Zh","Z","I","Y","K","L","M","N","O","P","R","S","T","U","F","H","Ts","Ch","Sh","Sch", "","I", "","E","Ju","Ja","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
         StringBuilder builder = new StringBuilder();
         
         for (int i = 0; i < message.length(); i++) {
