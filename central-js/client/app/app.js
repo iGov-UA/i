5159af56c8e88c60e968ce7e3a1f8cbd5c3fbe54
domash <aleksej.domash@gmail.com>
2017-02-05 22:28:46 +0200

    showing nice error on stateChangeError, reject user data promise on resolve, adding more logging to user service

--- central-js/client/app/app.js
+++ central-js/client/app/app.js
@@ -47,19 +47,42 @@ angular.module('app', [
   datepickerConfig.formatMonth = 'MMM';
   datepickerConfig.startingDay = 1;
   datepickerPopupConfig.clearText = 'Очистити';
-}).run(function ($rootScope, $state, statesRepository) {
+}).run(function ($rootScope, $state, $injector, statesRepository) {
   $rootScope.state = $state;
   $rootScope.profile = {
-    isKyivCity : !!statesRepository.isKyivCity()
+    isKyivCity: !!statesRepository.isKyivCity()
   };
-  $rootScope.$on('$stateChangeError', function (event, toState, toParams, fromState, fromParams, error) {
-    if (error && error.data) {
-      console.error('stateChangeError', error.data);
-      //TODO: Заменить на нормальный див-диалог из ErrorFactory
-      alert("Невідома помилка: " + error.data);
+
+
+  function getErrorText(error) {
+    if (error && error.code && error.message) {
+      return 'Помилка \'' + error.message +'\'';
     } else {
-      console.error('stateChangeError', error);
-      alert("Невідома помилка: " + error);
+      if (error && error.data) {
+        return 'Невідома помилка \'' + JSON.stringify(error.data) + '\'';
+      } else {
+        return 'Невідома помилка \'' + JSON.stringify(error) + '\'';
+      }
+    }
+  }
+
+  var errorsFactory;
+
+  $rootScope.$on('$stateChangeError', function (event, toState, toParams, fromState, fromParams, error) {
+    event.preventDefault();
+
+    if (!errorsFactory) {
+      errorsFactory = $injector.get('ErrorsFactory');
+    }
+
+    var errorText = getErrorText(error);
+    if (errorsFactory) {
+      errorsFactory.push({
+        type: 'danger',
+        text: errorText
+      });
+    } else {
+      alert(errorText);
     }
   });
 });
