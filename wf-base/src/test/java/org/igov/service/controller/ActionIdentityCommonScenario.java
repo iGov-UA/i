7a15a59aa3d3da3f5b39ff0f47aebeaf1765667e
kirei7 <vlad.sereda.y@gmail.com>
2017-02-06 14:39:07 +0200

    add shouldSuccessfullyGetUsers() test

--- wf-base/src/test/java/org/igov/service/controller/ActionIdentityCommonScenario.java
+++ wf-base/src/test/java/org/igov/service/controller/ActionIdentityCommonScenario.java
@@ -11,6 +11,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
@@ -40,25 +41,32 @@ public class ActionIdentityCommonScenario {
 
     @Test
     public void shouldSuccessfullyGetUsers() throws Exception {
-        //check if all users returned in response to a query with no parameters
         String sURL_Template = "/action/identity/getUsers";
-        String sJSON_Data = mockMvc.perform(get(sURL_Template)).
-                andExpect(status().isOk()).
-                andExpect(content().contentType(APPLICATION_JSON_CHARSET_UTF_8)).
-                andReturn().getResponse().getContentAsString();
-        List<Map<String, String>> aUsers = JsonRestUtils.readObject(sJSON_Data, List.class);
+        RequestBuilder requestBuilder;
+        //check that all users are returned in response to a query with no parameters
+        requestBuilder = get(sURL_Template);
+        List<Map<String, String>> aUsers =
+                JsonRestUtils.readObject(fetchJSONData(requestBuilder), List.class);
         Assert.assertEquals(3, aUsers.size());
 
-        //check if there is only one user returned in response to a query with
+        //check that there is only one user returned in response to a query with
         //ID_Group parameter
-        sJSON_Data = mockMvc.perform(get(sURL_Template).
-                param("sID_Group", "admin")).
-                andExpect(status().isOk()).
-                andExpect(content().contentType(APPLICATION_JSON_CHARSET_UTF_8)).
-                andReturn().getResponse().getContentAsString();
-        aUsers = JsonRestUtils.readObject(sJSON_Data, List.class);
+        requestBuilder = get(sURL_Template).param("sID_Group", "admin");
+        aUsers = JsonRestUtils.readObject(fetchJSONData(requestBuilder), List.class);
         Assert.assertEquals(1, aUsers.size());
         Assert.assertEquals("kermit", aUsers.get(0).get("sLogin"));
 
+        //check that nothing is returned in response to a query with
+        //non-existing group name in ID_Group parameter
+        requestBuilder = get(sURL_Template).param("sID_Group", "someMeaninglessPhrase");
+        aUsers = JsonRestUtils.readObject(fetchJSONData(requestBuilder), List.class);
+        Assert.assertEquals(0,aUsers.size());
+    }
+
+    private String fetchJSONData(RequestBuilder requestBuilder) throws Exception{
+        return mockMvc.perform(requestBuilder).
+                andExpect(status().isOk()).
+                andExpect(content().contentType(APPLICATION_JSON_CHARSET_UTF_8)).
+                andReturn().getResponse().getContentAsString();
     }
 }
