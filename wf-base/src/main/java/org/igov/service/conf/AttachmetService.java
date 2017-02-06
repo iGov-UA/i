a2fbe37d686f1c53cc875ad64d53797b37386501
Kovylin Yegor <egor.kovylin@privatbank.ua>
2017-02-06 15:57:46 +0200

    modify logic in MailTaskWithAttachementsAndSMS for new attach schema

--- wf-base/src/main/java/org/igov/service/conf/AttachmetService.java
+++ wf-base/src/main/java/org/igov/service/conf/AttachmetService.java
@@ -71,7 +71,7 @@ public class AttachmetService {
         SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
         SimpleDateFormat dtf = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
         
-        sFileNameAndExt = (sFileNameAndExt != null)?transliterate(getFileName(sFileNameAndExt.replace(" ", "_")))
+        sFileNameAndExt = (sFileNameAndExt != null)?transliterateAll(getFileName(sFileNameAndExt.replace(" ", "_")))
                     + "." + getFileExtention(sTextTranslit(sFileNameAndExt)):sFileNameAndExt;
 
         sFileNameCustom = (sFileNameCustom != null)?transliterateAll(getFileName(sFileNameCustom.replace(" ", "_")))
@@ -222,29 +222,29 @@ public class AttachmetService {
         return "";
     }
 
-    private static char[] abcCyr =   {' ', '_','а','б','в','г','д','е','ё', 'ж','з','и','й','к','л','м','н','о','п','р','с','т','у','ф','х', 'ц','ч', 'ш','щ','ъ','ы','ь','э', 'ю','я','А','Б','В','Г','Д','Е','Ё', 'Ж','З','И','Й','К','Л','М','Н','О','П','Р','С','Т','У','Ф','Х', 'Ц', 'Ч','Ш', 'Щ','Ъ','Ы','Б','Э','Ю','Я','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
-    private static String[] abcLat = {" ", "_", "a","b","v","g","d","e","e","zh","z","i","y","k","l","m","n","o","p","r","s","t","u","f","h","ts","ch","sh","sch", "","i", "","e","ju","ja","A","B","V","G","D","E","E","Zh","Z","I","Y","K","L","M","N","O","P","R","S","T","U","F","H","Ts","Ch","Sh","Sch", "","I", "","E","Ju","Ja","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
+    private static final char[] CYR_ABC =   {' ', '_','а','б','в','г','д','е','ё', 'ж','з','и','й','к','л','м','н','о','п','р','с','т','у','ф','х', 'ц','ч', 'ш','щ','ъ','ы','ь','э', 'ю','я','А','Б','В','Г','Д','Е','Ё', 'Ж','З','И','Й','К','Л','М','Н','О','П','Р','С','Т','У','Ф','Х', 'Ц', 'Ч','Ш', 'Щ','Ъ','Ы','Б','Э','Ю','Я','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+    private static final String[] LAT_ABC = {" ", "_", "a","b","v","g","d","e","e","zh","z","i","y","k","l","m","n","o","p","r","s","t","u","f","h","ts","ch","sh","sch", "","i", "","e","ju","ja","A","B","V","G","D","E","E","Zh","Z","I","Y","K","L","M","N","O","P","R","S","T","U","F","H","Ts","Ch","Sh","Sch", "","I", "","E","Ju","Ja","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
     
-    public static String transliterate(String message)
+    /*public static String transliterate(String message)
     {
         StringBuilder builder = new StringBuilder();
         
         for (int i = 0; i < message.length(); i++) {
-            for(int x = 0; x < abcCyr.length; x++ )
-                if (message.charAt(i) == abcCyr[x]) {
-                    builder.append(abcLat[x]);
+            for(int x = 0; x < CYR_ABC.length; x++ )
+                if (message.charAt(i) == CYR_ABC[x]) {
+                    builder.append(LAT_ABC[x]);
                 }
         }
         return builder.toString();
-    }
+    }*/
 
     public static String transliterateAll(String message){
         StringBuilder builder = new StringBuilder();
         for (int i = 0; i < message.length(); i++) {
             boolean isAppended = false;
-            for(int x = 0; x < abcCyr.length; x++ ) {
-                if (message.charAt(i) == abcCyr[x]) {
-                    builder.append(abcLat[x]);
+            for(int x = 0; x < CYR_ABC.length; x++ ) {
+                if (message.charAt(i) == CYR_ABC[x]) {
+                    builder.append(LAT_ABC[x]);
                     isAppended = true;
                     break;
                 }
