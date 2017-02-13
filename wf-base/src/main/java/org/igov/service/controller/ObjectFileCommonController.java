c133e47b778c05947470385063125ff2e857c9c2
it200687kov <9379992privat>
2017-02-13 19:48:09 +0200

    fix

--- wf-base/src/main/java/org/igov/service/controller/ObjectFileCommonController.java
+++ wf-base/src/main/java/org/igov/service/controller/ObjectFileCommonController.java
@@ -821,7 +821,7 @@ public class ObjectFileCommonController {
     private String convetToWin(String str) throws UnsupportedEncodingException{
         //return new String(str.getBytes("UTF-8"), "windows-1251");
         LOG.info("str: " + str);
-        LOG.info("Cp-1251: " + new String(str.getBytes("UTF-8"), "Cp-1251"));
+        LOG.info("windows-1251: " + new String(str.getBytes("UTF-8"), "windows-1251"));
         LOG.info("Cp1251: " + new String(str.getBytes("UTF-8"), "Cp1251"));
         LOG.info("no: " + new String(str.getBytes("UTF-8")));
         
