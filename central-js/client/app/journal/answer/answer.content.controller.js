ab4cb194c5f058bbc4973db151b15d07accfc7b3
antonlegkiy <anton.legkiy@gmail.com>
2017-02-15 22:15:40 +0200

    try

--- central-js/client/app/journal/answer/answer.content.controller.js
+++ central-js/client/app/journal/answer/answer.content.controller.js
@@ -12,6 +12,7 @@
     vm.redirectUrl = $state.href('index.journal.answer', {error: ''});
 
     vm.parseDate = function (date) {
+      console.warn('ДАТА!!!Контроллер - '+ date);
       if(date) return date.replace(/^(\d{2})(\d{2})(\d{4})$/, '$1.$2.$3');
     }
   }
