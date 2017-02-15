534f52b3e2088bd45990b18564255f411149ab26
antonlegkiy <anton.legkiy@gmail.com>
2017-02-15 21:08:30 +0200

    try fix

--- central-js/client/app/journal/answer/answer.content.controller.js
+++ central-js/client/app/journal/answer/answer.content.controller.js
@@ -10,5 +10,9 @@
     vm.signedFileID = $state.params.signedFileID;
     vm.fileName = $state.params.fileName;
     vm.redirectUrl = $state.href('index.journal.answer', {error: ''});
+
+    vm.parseDate = function (date) {
+      if(date) return date.replace(/^(\d{2})(\d{2})(\d{4})$/, '$1.$2.$3');
+    }
   }
 })();
