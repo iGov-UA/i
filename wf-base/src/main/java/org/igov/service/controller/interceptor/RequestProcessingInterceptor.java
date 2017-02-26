ebaa41b0d50727cb8a7e3db501a84ad76823c97b
Kovylin Yegor <egor.kovylin@privatbank.ua>
2017-02-26 14:21:57 +0200

    realization 1.1. for #1504

--- wf-base/src/main/java/org/igov/service/controller/interceptor/RequestProcessingInterceptor.java
+++ wf-base/src/main/java/org/igov/service/controller/interceptor/RequestProcessingInterceptor.java
@@ -292,7 +292,7 @@ public class RequestProcessingInterceptor extends HandlerInterceptorAdapter impl
             throws Exception {
         
         LOG.info("saveNewTaskInfo started in " + new SimpleDateFormat("yyyy-MM-dd HH:mm").format(new Date()));
-        LOG.info("sRequestBody is {} , sResponseBody is {}",  sResponseBody, mParamRequest);
+        LOG.info("sRequestBody is {} , sResponseBody is {}",  sRequestBody, sResponseBody);
         
         if (sResponseBody == null) {
             LOG.warn("sResponseBody=null!!! (sRequestBody={},mParamRequest={})", sRequestBody, mParamRequest);
