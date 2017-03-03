28604d83f6a3b561b7319b0d6a1eaaeba15e5798
Kovylin Yegor <egor.kovylin@privatbank.ua>
2017-03-03 03:08:18 +0200

    add edit logic for #1504

--- wf-base/src/main/java/org/igov/model/action/event/HistoryEventType.java
+++ wf-base/src/main/java/org/igov/model/action/event/HistoryEventType.java
@@ -78,7 +78,7 @@ public enum HistoryEventType {
             "DocumentChanged",
             "Внесены изменения в документ или задачу (Слушатель SetTask)",
             HistoryEventMessage.FIO + " - документ відредаговано:\n\n" +  
-            "<table width=\"30%\" cellspacing=\"0\" cellpadding=\"4\" border=\"1\"><tr><th>Було</th><th>Cтало</th></tr>" +         
+            "<table style=\"width:30%\"><tr><th>Було</th><th>Cтало</th></tr>" +         
             HistoryEventMessage.OLD_DATA + "\n" 
             + HistoryEventMessage.NEW_DATA + "</table>"),         
     CREATE_SUBDOCUMENT(15L,
