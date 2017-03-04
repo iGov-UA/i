dfb2878da1d6d7565bb6f9bd72756f1f756eae77
it200687kov <9379992privat>
2017-03-04 15:33:58 +0200

    fix

--- wf-base/src/main/java/org/igov/service/controller/ExportCommonController.java
+++ wf-base/src/main/java/org/igov/service/controller/ExportCommonController.java
@@ -4,7 +4,6 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 
 import org.igov.service.business.export.AgroholdingService;
-import org.igov.service.business.export.AgroholdingService_oData;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,26 +21,14 @@ public class ExportCommonController {
 
     @Autowired
     AgroholdingService agroholdingService;
-    
-    @Autowired
-    AgroholdingService_oData agroholdingService_oData;
 
     @ApiOperation(value = "/agroholding/transferDocumentVacation", notes = "##### Экспорт документа о отпуске в агрофирму. Примеры:\n"
             + "https://alpha.test.igov.org.ua/wf/service/export/agroholding/transferDocumentVacation")
     @RequestMapping(value = "/agroholding/transferDocumentVacation", method = RequestMethod.GET)
-    public @ResponseBody String transferDocumentVacation() throws Exception {
+    public @ResponseBody
+    String transferDocumentVacation() throws Exception {
         LOG.info("transferDocumentVacation...");
         String result = agroholdingService.transferDocumentVacation();
-        LOG.info("transferDocumentVacation result = " + result);
-        return result;
-    }
-    
-    @ApiOperation(value = "/agroholding/transferDocumentVacation_oData", notes = "##### Экспорт документа о отпуске в агрофирму. Примеры:\n"
-            + "https://alpha.test.igov.org.ua/wf/service/export/agroholding/transferDocumentVacation_oData")
-    @RequestMapping(value = "/agroholding/transferDocumentVacation_oData", method = RequestMethod.GET)
-    public @ResponseBody String transferDocumentVacation_oData() throws Exception {
-        LOG.info("transferDocumentVacation...");
-        String result = agroholdingService_oData.transferDocumentVacation();
         LOG.info("transferDocumentVacation result = " + result);
         return result;
     }
