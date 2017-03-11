ded1a3c443f159dab89bfce2beb2c0eb8790eac3
Elena <elena.shebaldenkova@gmail.com>
2017-03-11 23:08:50 +0200

    #1563

--- wf-base/src/main/java/org/igov/service/business/subject/SubjectRightBPService.java
+++ wf-base/src/main/java/org/igov/service/business/subject/SubjectRightBPService.java
@@ -44,10 +44,10 @@ public class SubjectRightBPService {
 		}
 		LOG.info("In the method getSubjectRightBPs sLogin={}, asID_Group={}", sLogin, asID_Group);
 
-		List<SubjectRightBP> aSubjectRightBP = subjectRightBPDao.findAllByInValues("sID_Group_Activiti", asID_Group);
+		List<SubjectRightBP> aSubjectRightBP = subjectRightBPDao.findAllByInValues("sID_Group", asID_Group);
 		LOG.info("In the method getSubjectRightBPs aSubjectRightBP {}", aSubjectRightBP);
 		
-		List<SubjectRightBP> aSubjectRightBP1 = subjectRightBPDao.findAll();
+		/*List<SubjectRightBP> aSubjectRightBP1 = subjectRightBPDao.findAll();
 		
 		LOG.info("In the method getSubjectRightBPs aSubjectRightBP findAll = {}", aSubjectRightBP1);
 
@@ -57,7 +57,7 @@ public class SubjectRightBPService {
 			oSubjectRightBP.getsID_Place_UA();
 			LOG.info("sID_BP = {} sID_Group = {}  sID_Place_UA = {}" , oSubjectRightBP.getsID_BP(), oSubjectRightBP.getsID_Group(), oSubjectRightBP.getsID_Place_UA());
 			
-		}
+		}*/
 		
 		SubjectRightBP oFindedSubjectRightBP = null;
 
