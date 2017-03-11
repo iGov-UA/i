e6b6b977b63da9992075bd9aa800622cfb57ca77
Kovylin Yegor <egor.kovylin@privatbank.ua>
2017-03-11 16:29:14 +0200

    add documnet submit intercepting

--- wf-base/src/main/java/org/igov/service/controller/interceptor/RequestProcessingInterceptor.java
+++ wf-base/src/main/java/org/igov/service/controller/interceptor/RequestProcessingInterceptor.java
@@ -101,7 +101,7 @@ public class RequestProcessingInterceptor extends HandlerInterceptorAdapter impl
         LOG_BIG.info("(getMethod()={}, getRequestURL()={})", oRequest.getMethod().trim(), oRequest.getRequestURL().toString());
         oRequest.setAttribute("startTime", startTime);
         protocolize(oRequest, response, false);
-        documentHistoryProcessing(oRequest, response);
+        //documentHistoryProcessing(oRequest, response);
         return true;
 }
 
