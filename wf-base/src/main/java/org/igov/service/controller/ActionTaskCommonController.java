0743aa44ef8b4e8a9001163f955d19617fcf276e
Kovylin Yegor <egor.kovylin@privatbank.ua>
2017-03-25 00:48:09 +0200

    fix attach history error

--- wf-base/src/main/java/org/igov/service/controller/ActionTaskCommonController.java
+++ wf-base/src/main/java/org/igov/service/controller/ActionTaskCommonController.java
@@ -706,9 +706,9 @@ public class ActionTaskCommonController {//extends ExecutionBaseResource
                 response.put("aMessage", oMessageService.gerOrderMessagesByProcessInstanceID(nID_Process));
             } catch (Exception oException) {
                 LOG.error("Can't get: {}", oException.getMessage());
-                //throw new CommonServiceException(
+                /*throw new CommonServiceException(
                         ExceptionCommonController.BUSINESS_ERROR_CODE,
-                        "Can't get: " + oException.getMessage(), oException, HttpStatus.FORBIDDEN);
+                        "Can't get: " + oException.getMessage(), oException, HttpStatus.FORBIDDEN);*/
             }
         }
         if (bIncludeProcessVariables.equals(Boolean.TRUE) && nID_Process != null) {
