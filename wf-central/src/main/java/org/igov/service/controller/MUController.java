92bb37fe9e3b601c7fd067ddcfd0541a94d0dcf4
askosyr <askosyr@gmail.com>
2017-04-02 19:44:21 +0300

    https://github.com/e-government-ua/i/issues/1593 Added logic to pass files as parameters to MU web service

--- wf-central/src/main/java/org/igov/service/controller/MUController.java
+++ wf-central/src/main/java/org/igov/service/controller/MUController.java
@@ -1,21 +1,19 @@
 package org.igov.service.controller;
 
-import java.rmi.RemoteException;
-import java.util.List;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.xml.rpc.ServiceException;
-
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+
+import java.rmi.RemoteException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.xml.rpc.ServiceException;
 
 import org.igov.bjust.IService;
 import org.igov.bjust.ResultModel;
 import org.igov.bjust.ServiceLocator;
 import org.igov.bjust.Signs;
 import org.igov.bjust.UploadSignsModel;
-import org.igov.model.object.ObjectEarthTarget;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -36,14 +34,26 @@ public class MUController {
     @RequestMapping(value = "/setOrder", method = RequestMethod.POST)
     public @ResponseBody
     String getObjectEarthTargets(
-            @ApiParam(value = "Документ для передачи в веб сервис министерства юстиции", required = false) @RequestBody String order,
+    		@ApiParam(value = "application_id", required = false) 
+    		@RequestParam(value = "application_id", required = false) String sApplicationId,
+    		@ApiParam(value = "sessionId", required = false) 
+    		@RequestParam(value = "sessionId", required = false) String sSessionId,
+    		@ApiParam(value = "fileName", required = false) 
+    		@RequestParam(value = "fileName", required = false) String sFileName,
+            @ApiParam(value = "Документ для передачи в веб сервис министерства юстиции", required = false) @RequestParam("file_sign") byte[] file_sign,
+            @ApiParam(value = "Подпись документа для передачи в веб сервис министерства юстиции", required = false) 
+    		@RequestParam("file_stamp") byte[] file_stamp,
             HttpServletRequest request) throws ServiceException, RemoteException {
 		ServiceLocator serviceLocator = new ServiceLocator();
 		IService service = serviceLocator.getBinding_IService();
-		LOG.info("Got web service locator. Length of parameter of the method: " + order.length());
+		LOG.info("Got web service locator. Length of parameters of the method: " + file_sign != null ? String.valueOf(file_sign.length) : "-1" + ":" + 
+				file_stamp != null ? String.valueOf(file_stamp.length) : "-1");
 		UploadSignsModel uploadSignsModel = new UploadSignsModel();
 		
 		Signs signs = new Signs();
+		signs.setFile_name(sFileName);
+		signs.setFile_sign(String.valueOf(file_sign));
+		signs.setFile_stamp(String.valueOf(file_stamp));
 		Signs[] params = new Signs[1];
 		params[0] = signs;
 		uploadSignsModel.setFiles(params);
