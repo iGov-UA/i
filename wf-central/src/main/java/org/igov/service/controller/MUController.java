c30c6ed6f7e355a76b581ae3a2d557a5b5e1cd84
askosyr <askosyr@gmail.com>
2017-04-05 12:18:13 +0300

    https://github.com/e-government-ua/i/issues/1593 Refactoring

--- wf-central/src/main/java/org/igov/service/controller/MUController.java
+++ wf-central/src/main/java/org/igov/service/controller/MUController.java
@@ -17,7 +17,6 @@ import org.igov.bjust.UploadSignsModel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -68,7 +67,7 @@ public class MUController {
     }
 	
 	@ApiOperation(value = "")
-    @RequestMapping(value = "/test", method = RequestMethod.POST)
+    @RequestMapping(value = "/test", method = RequestMethod.GET)
     public @ResponseBody
     String getObjectEarthTargets(
     		@ApiParam(value = "service_code", required = true) 
