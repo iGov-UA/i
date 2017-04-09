d629df9ff1b56a2c09c952931503fd19380f3473
askosyr <askosyr@gmail.com>
2017-04-09 16:48:59 +0300

    https://github.com/e-government-ua/i/issues/1575 (not real one) Refactoring

--- wf-base/src/main/java/org/igov/service/controller/ActionTaskCommonController.java
+++ wf-base/src/main/java/org/igov/service/controller/ActionTaskCommonController.java
@@ -1292,13 +1292,14 @@ public class ActionTaskCommonController {//extends ExecutionBaseResource
         List<HistoricTaskInstance> foundHistoricResults = historicQuery
                 .listPage(nRowStart, nRowsMax);
 
-        String header = oActionTaskService.formHeader(saFields, foundHistoricResults, saFieldsCalc);
-        String[] headers = header.split(";");
-
         if ("*".equals(saFields)){
         	saFields = null;
         	LOG.info("Resetting saFields to null in order to get all the fields values");
         }
+        String header = oActionTaskService.formHeader(saFields, foundHistoricResults, saFieldsCalc);
+        String[] headers = header.split(";");
+
+        
         saFields = oActionTaskService.processSaFields(saFields, foundHistoricResults);
 
         LOG.info("!!!!!!!!!!!!!!!!!!!saFields!!!!!!!!!!!!!!!!!" + saFields);
