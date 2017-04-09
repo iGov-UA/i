a08b89c44dd2e8e77ac39329d1bdfab018ab47f5
olga <olga@192.168.0.102>
2017-04-09 15:21:29 +0300

    add information about countSubmited

--- wf-base/src/main/java/org/igov/service/controller/DocumentCommonController.java
+++ wf-base/src/main/java/org/igov/service/controller/DocumentCommonController.java
@@ -46,7 +46,7 @@ public class DocumentCommonController {
     @RequestMapping(value = "/isDocumentStepSubmitedAll", method = RequestMethod.GET)
     @Transactional
     public @ResponseBody
-    Map<String, Boolean> isDocumentStepSubmitedAll(
+    Map<String, Object> isDocumentStepSubmitedAll(
             @ApiParam(value = "ИД процесс-активити", required = false) @RequestParam(required = false, value = "nID_Process") String nID_Process,
             @ApiParam(value = "Логин подписанта", required = false) @RequestParam(required = false, value = "sLogin") String sLogin,
             @ApiParam(value = "Ключ шага документа", required = false) @RequestParam(required = false, value = "sKey_Step") String sKey_Step,
