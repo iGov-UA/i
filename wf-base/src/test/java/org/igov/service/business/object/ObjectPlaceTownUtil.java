0930994e5cfc9265f2c97204c5aeb88dbd182971
kr110666kai@gmail.com < ..>
2017-04-21 14:34:27 +0300

    fix Utils for 1511

--- wf-base/src/test/java/org/igov/service/business/object/ObjectPlaceTownUtil.java
+++ wf-base/src/test/java/org/igov/service/business/object/ObjectPlaceTownUtil.java
@@ -1,6 +1,7 @@
 package org.igov.service.business.object;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -30,6 +31,8 @@ public class ObjectPlaceTownUtil {
     private static final String URL_LIST_ADDRESS = URL_SERVICE_ROOT + "/listAddressByType.do";
 
     public static void main(String[] args) {
+	System.out.printf("получение обастей:\n");
+
 	Map<ObjectPlaceLang, List<Address>> mAddress = new HashMap<ObjectPlaceLang, List<Address>>();
 
 	// Получаем области
@@ -40,34 +43,29 @@ public class ObjectPlaceTownUtil {
 	mAddress.put(ObjectPlaceLang.RUS, lAddress);
 
 	// Получаем адреса по областям
-	Map<ObjectPlaceLang, List<Address>> mTown = new HashMap<ObjectPlaceLang, List<Address>>();
+	List<Address> lTowns = new ArrayList<>();
 	for (ObjectPlaceLang lang : mAddress.keySet()) {
 	    lAddress = mAddress.get(lang);
 	    System.out.printf("Язык: %s\n", lang);
 
-	    mTown.put(lang, new ArrayList<>());
-
 	    for (Address address : lAddress) {
-		System.out.printf("Область: %s\n", address.code);
-		mTown.get(lang).addAll(getListAddress(address));
+		System.out.printf("Область: %s\n", address.getCode());
+		lTowns.addAll(getListAddress(address));
 	    }
+
+	    lTowns.addAll(lAddress);
 	}
 
-	// Вывод областей в файлы
-	for (ObjectPlaceLang lang : mAddress.keySet()) {
-	    WriteToCSV("ObjectPlaceOblast_", lang, mAddress.get(lang));
-	}
-	
-	// Вывод адресов в файлы
-	for (ObjectPlaceLang lang : mTown.keySet()) {
-	    WriteToCSV("ObjectPlaceTown_", lang, mTown.get(lang));
-	}
+	lTowns.sort(Comparator.comparing(Address::getNodeTypeCode).thenComparing(Address::getCodeParent)
+		.thenComparing(Address::getCode));
+
+	WriteToCSV(lTowns);
 
 	System.out.println("Работа успешно завершена");
     }
 
-    private static void WriteToCSV(String sFileNameRoot, ObjectPlaceLang lang, List<Address> lAddress) {
-	String fileName = System.getProperty("user.home") + File.separator + sFileNameRoot + lang.name() + ".csv";
+    private static void WriteToCSV(List<Address> lAddress) {
+	String fileName = System.getProperty("user.home") + File.separator + "ObjectAddress.csv";
 	System.out.printf("Вывод данных в файл: %s\n", fileName);
 
 	// Определяем файл
@@ -92,25 +90,25 @@ public class ObjectPlaceTownUtil {
 		    sb.append(";");
 		    sb.append(address.codeParent);
 		    sb.append(";");
-		    sb.append(address.code);
+		    sb.append(address.getCode());
 		    sb.append(";");
-		    sb.append(address.desc);
+		    sb.append(address.getDesc());
 		    sb.append(";");
-		    sb.append(address.innerId);
+		    sb.append(address.getInnerId());
 		    sb.append(";");
-		    sb.append(address.langCode);
+		    sb.append(address.getLangCode());
 		    sb.append(";");
-		    sb.append(address.nodeTypeCode);
+		    sb.append(address.getNodeTypeCode());
 		    sb.append(";");
-		    sb.append(address.nodeTypeId);
+		    sb.append(address.getNodeTypeId());
 		    sb.append(";");
-		    sb.append(address.nodeTypeName);
+		    sb.append(address.getNodeTypeName());
 		    sb.append(";");
-		    sb.append(address.nodeTypeSName);
+		    sb.append(address.getNodeTypeSName());
 		    sb.append(";");
-		    sb.append(address.typeCodeId);
+		    sb.append(address.getTypeCodeId());
 		    sb.append(";");
-		    sb.append(address.zip);
+		    sb.append(address.getZip());
 		    sb.append("\n");
 
 		    out.print(sb);
@@ -123,7 +121,7 @@ public class ObjectPlaceTownUtil {
 
     // https://service-street.tech.igov.org.ua/AddressReference/address/listAddressByType.do?id=UA40773&type=4&&language=UAN
     private static List<Address> getListAddress(Address oblast) {
-	String sUrl = URL_LIST_ADDRESS + "?id=" + oblast.code + "&language=" + oblast.langCode + "&type=4";
+	String sUrl = URL_LIST_ADDRESS + "?id=" + oblast.getCode() + "&language=" + oblast.getLangCode() + "&type=4";
 	String ret = "";
 
 	HttpURLConnection oHttpURLConnection = null;
@@ -162,7 +160,7 @@ public class ObjectPlaceTownUtil {
 
 	// System.out.printf("url=%s\n%s\n", sUrl, ret);
 
-	return getListAddressFromString(oblast.code, ret);
+	return getListAddressFromString(oblast.getCode(), ret);
     }
 
     private static List<Address> getListAddressFromString(String codeRoot, String sAddr) {
@@ -264,8 +262,6 @@ public class ObjectPlaceTownUtil {
 	    }
 	}
 
-//	System.out.printf("url=%s\n%s\n", sUrl, ret);
-
 	return getListAddressFromString("UA40773", ret);
     }
 
@@ -274,16 +270,16 @@ public class ObjectPlaceTownUtil {
 class Address {
     String codeParent = null;
 
-    String code = "";
-    String desc = "";
-    String innerId = "";
-    String langCode = "";
-    String nodeTypeCode = "";
-    String nodeTypeId = "";
-    String nodeTypeName = "";
-    String nodeTypeSName = "";
-    String typeCodeId = "";
-    String zip = "";
+    private String code = "";
+    private String desc = "";
+    private String innerId = "";
+    private String langCode = "";
+    private String nodeTypeCode = "";
+    private String nodeTypeId = "";
+    private String nodeTypeName = "";
+    private String nodeTypeSName = "";
+    private String typeCodeId = "";
+    private String zip = "";
 
     public Address(String codeRoot, String code, String desc, String innerId, String langCode, String nodeTypeCode,
 	    String nodeTypeId, String nodeTypeName, String nodeTypeSName, String typeCodeId, String zip) {
@@ -301,4 +297,49 @@ class Address {
 	this.zip = zip;
 
     }
+
+    public String getCode() {
+	return code;
+    }
+
+    public String getCodeParent() {
+	return codeParent;
+    }
+
+    public String getDesc() {
+	return desc;
+    }
+
+    public String getInnerId() {
+	return innerId;
+    }
+
+    public String getLangCode() {
+	return langCode;
+    }
+
+    public String getNodeTypeCode() {
+	return nodeTypeCode;
+    }
+
+    public String getNodeTypeId() {
+	return nodeTypeId;
+    }
+
+    public String getNodeTypeName() {
+	return nodeTypeName;
+    }
+
+    public String getNodeTypeSName() {
+	return nodeTypeSName;
+    }
+
+    public String getTypeCodeId() {
+	return typeCodeId;
+    }
+
+    public String getZip() {
+	return zip;
+    }
+
 }
