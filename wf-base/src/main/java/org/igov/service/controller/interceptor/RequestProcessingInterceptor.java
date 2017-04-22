13ba5631d6f4fbf218e58ab9b04eeaa154724e05
Kovylin Yegor <egor.kovylin@privatbank.ua>
2017-04-22 22:03:36 +0300

    realization for #1628

--- wf-base/src/main/java/org/igov/service/controller/interceptor/RequestProcessingInterceptor.java
+++ wf-base/src/main/java/org/igov/service/controller/interceptor/RequestProcessingInterceptor.java
@@ -972,7 +972,8 @@ public class RequestProcessingInterceptor extends HandlerInterceptorAdapter impl
         return POST.equalsIgnoreCase(oRequest.getMethod().trim())
                 && (((sResponseBody == null || "".equals(sResponseBody))
                 && oRequest.getRequestURL().toString().indexOf(FORM_FORM_DATA) > 0)
-                || TAG_PATTERN_PREFIX.matcher(oRequest.getRequestURL()).find());
+                || TAG_PATTERN_PREFIX.matcher(oRequest.getRequestURL()).find()
+                || oRequest.getRequestURL().toString().indexOf(SERVICE_CANCELTASK) > 0);
     }
 
     private boolean isSaveTask(HttpServletRequest oRequest, String sResponseBody) {
