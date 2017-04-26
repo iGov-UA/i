9c308f708fe8dda639aa7e88155020b0fee15f7a
admin <admin@inna>
2017-04-26 12:19:52 +0300

    edit Signed-off-by: admin <admin@inna>

--- wf-base/src/main/java/org/igov/service/business/action/task/systemtask/mail/Abstract_MailTaskCustom.java
+++ wf-base/src/main/java/org/igov/service/business/action/task/systemtask/mail/Abstract_MailTaskCustom.java
@@ -812,7 +812,7 @@ public abstract class Abstract_MailTaskCustom extends AbstractModelTask implemen
         if (oTaskFormData != null
                 && oTaskFormData.getFormProperties() != null) {
             for (FormProperty oFormProperty : oTaskFormData.getFormProperties()) {
-            	if(oFormProperty.getValue()!=null&&oFormProperty.getValue().contains("sID_StorageType")) {
+            	if(oFormProperty.getValue()!=null&&oFormProperty.getValue().startsWith("{\"sID_StorageType\":\"Mongo\"")) {
                 aFormPropertyReturn.put("HTML", oFormProperty.getValue());
                 LOG.info("Matching property (Value={})",
                         oFormProperty.getValue());
