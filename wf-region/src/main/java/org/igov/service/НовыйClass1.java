50eb9078735675713459ca7ff70db2b8fec49c10
it200687kov <olga@olga-pc>
2017-05-11 17:18:31 +0300

    fix

---
+++ wf-region/src/main/java/org/igov/service/НовыйClass1.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.igov.service;
+
+/**
+ *
+ * @author olga
+ */
+public class НовыйClass1 {
+    
+    public static void main(){
+    String s = "sDoc1::${sDoc1};;sDateRegistration::${sDateRegistration};;sDateDoc::${sDateDoc};;sLogin_AutorResolution::${sLogin_AutorResolution};;sDateExecution::${sDateExecution};;sContent::${sContent};;sName_AutorResolution::${sName_AutorResolution};;sTypeDoc::${sTypeDoc};;sID_Custom_GovPublic::${sID_Custom_GovPublic};;sApplicant::${sApplicant};;nCountAttach::${nCountAttach};;sNote::${sNote};;asUrgently::${asUrgently};;asTypeResolution::${asTypeResolution};;sTextResolution::${sTextResolution}";
+    }
+}
