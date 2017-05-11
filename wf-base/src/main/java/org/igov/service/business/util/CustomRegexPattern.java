54a69bd49f0219dde1772d4a9cce7b111007dee5
Kovylin Yegor <egor.kovylin@privatbank.ua>
2017-05-11 18:17:41 +0300

    add ukraine symbols to transliteration

--- wf-base/src/main/java/org/igov/service/business/util/CustomRegexPattern.java
+++ wf-base/src/main/java/org/igov/service/business/util/CustomRegexPattern.java
@@ -50,5 +50,5 @@ public interface CustomRegexPattern {
         "Ja", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", 
         "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", 
         "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", 
-        "S", "T", "U", "V", "W", "X", "Y", "Z", "ji", "JI", "g", "G", "i", "I", "\'", "je", "JE"};
+        "S", "T", "U", "V", "W", "X", "Y", "Z", "ji", "Ji", "g", "G", "i", "I", "\'", "je", "Je"};
 }
