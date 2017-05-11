b9d4698a5c5acd696337e60bd6585a78b1c488ec
Kovylin Yegor <egor.kovylin@privatbank.ua>
2017-05-11 18:00:36 +0300

    test process instance id setting in multiinstance

--- wf-base/src/main/java/org/igov/service/business/process/ProcessSubjectTaskService.java
+++ wf-base/src/main/java/org/igov/service/business/process/ProcessSubjectTaskService.java
@@ -142,6 +142,7 @@ public class ProcessSubjectTaskService {
     }*/
     
     public List<String> getProcessSubjectLoginsWithoutTask(String snID_Process_Activiti, String sFilterLoginRole) throws RecordInmemoryException, org.json.simple.parser.ParseException{
+        LOG.info("getProcessSubjectLoginsWithoutTask started...");
         String sKeyRedis = (String)oRuntimeService.getVariable(snID_Process_Activiti, "sID_File_StorateTemp");
         byte[] aByteTaskBody = oBytesDataInmemoryStorage.getBytes(sKeyRedis);
         List<Task> aTaskActive = oTaskService.createTaskQuery().processInstanceId(snID_Process_Activiti).active().list();
