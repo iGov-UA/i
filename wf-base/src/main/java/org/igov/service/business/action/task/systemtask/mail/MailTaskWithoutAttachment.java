73a2f7d7847b2113b1cfb4b32b6e4f54d4890daa
admin <admin@inna>
2017-05-13 12:13:56 +0300

    Not send mail clerk Signed-off-by: admin <admin@inna>

--- wf-base/src/main/java/org/igov/service/business/action/task/systemtask/mail/MailTaskWithoutAttachment.java
+++ wf-base/src/main/java/org/igov/service/business/action/task/systemtask/mail/MailTaskWithoutAttachment.java
@@ -3,7 +3,9 @@ package org.igov.service.business.action.task.systemtask.mail;
 import org.activiti.engine.delegate.DelegateExecution;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.igov.io.GeneralConfig;
 import org.igov.io.mail.Mail;
 
 /**
@@ -13,33 +15,44 @@ import org.igov.io.mail.Mail;
  */
 @Component("MailTaskWithoutAttachment")
 public class MailTaskWithoutAttachment extends Abstract_MailTaskCustom {
+	
+	 @Autowired
+	 GeneralConfig generalConfig;
     
     private final static Logger LOG = LoggerFactory.getLogger(MailTaskWithoutAttachment.class);
 
     @Override
     public void execute(DelegateExecution oExecution) throws Exception {
     	
-    	Mail oMail = null;
-    	String sJsonMongo = loadFormPropertyFromTaskHTMLText(oExecution);
-    	LOG.info("sJsonMongo: {}", sJsonMongo);
-    	String sBodyFromMongoResult = getHtmlTextFromMongo(sJsonMongo); 
-    	LOG.info("sBodyFromMongoResult: {}", sBodyFromMongoResult);
-    	if(sBodyFromMongoResult!=null){
-	        try {
-	        	oMail = sendToMailFromMongo(oExecution);
-	        } catch (Exception ex) {
-	            LOG.error("MailTaskWithoutAttachment: ", ex);
-	        }
-    	} else{
-    		try {
-            	oMail = Mail_BaseFromTask(oExecution);
-            } catch (Exception ex) {
-                LOG.error("MailTaskWithoutAttachment: ", ex);
-            }
-    	}
-    	
-    	 sendMailOfTask(oMail, oExecution);
-    	 LOG.info("MailTaskWithoutAttachment ok!");
+	    	Mail oMail = null;
+	    	String sJsonMongo = loadFormPropertyFromTaskHTMLText(oExecution);
+	    	String sBodyFromMongoResult = getHtmlTextFromMongo(sJsonMongo); 
+	    	if(sBodyFromMongoResult!=null){
+		        try {
+		        	oMail = sendToMailFromMongo(oExecution);
+		        } catch (Exception ex) {
+		            LOG.error("MailTaskWithoutAttachment: ", ex);
+		        }
+	    	} else{
+	    		try {
+	            	oMail = Mail_BaseFromTask(oExecution);
+	            } catch (Exception ex) {
+	                LOG.error("MailTaskWithoutAttachment: ", ex);
+	            }
+	    	}
+	    	//если тестовый сервер - не отправлять письмо чиновнику
+	    	if(generalConfig.isSelfTest()) {
+	    		if(oMail.getBody()!=null && !oMail.getBody().contains("Шановний колего!")) {
+	    			sendMailOfTask(oMail, oExecution);
+	    			LOG.info("MailTaskWithoutAttachment ok!");
+	    		}else {
+	    			LOG.info("did not send to clerk body: {}",oMail.getBody());
+	    		}
+	    		
+	    	}else {
+	    		sendMailOfTask(oMail, oExecution);
+    			LOG.info("MailTaskWithoutAttachment ok!");
+	    	}
     }
     
 }
