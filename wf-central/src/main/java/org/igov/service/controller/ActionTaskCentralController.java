8fa3498c69a7ea1d2df771518b0963f12a002f7d
Elena <elena.shebaldenkova@gmail.com>
2017-05-16 20:52:37 +0300

    add log

--- wf-central/src/main/java/org/igov/service/controller/ActionTaskCentralController.java
+++ wf-central/src/main/java/org/igov/service/controller/ActionTaskCentralController.java
@@ -104,6 +104,7 @@ public class ActionTaskCentralController {
             Map<String, String> mParam = new HashMap<String, String>();
             mParam.put("nID_Process", processId);
             mParam.put("saField", saField);
+            LOG.info("saField ", saField);
             LOG.info(" mParam={} ", mParam);
             String sReturnRegion = httpRequester.getInside(sURL, mParam);
             LOG.info("(sReturnRegion={})", sReturnRegion);
