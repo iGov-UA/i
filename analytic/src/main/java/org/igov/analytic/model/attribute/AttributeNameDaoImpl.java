73a16f4a86cdeffb51027df655522e94a9ff903b
dpekach <dImA1995>
2017-05-18 19:09:40 +0300

    #1419: implementing AccessGroup/AccessUser

--- analytic/src/main/java/org/igov/analytic/model/attribute/AttributeNameDaoImpl.java
+++ analytic/src/main/java/org/igov/analytic/model/attribute/AttributeNameDaoImpl.java
@@ -3,10 +3,12 @@ package org.igov.analytic.model.attribute;
 import org.apache.log4j.Logger;
 import org.igov.analytic.model.core.GenericEntityDaoAnalytic;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * Created by dpekach on 18.05.17.
  */
+@Transactional("transactionManagerAnalytic")
 @Repository
 public class AttributeNameDaoImpl extends GenericEntityDaoAnalytic<Long, AttributeName> implements AttributeNameDao {
     private static final Logger log = Logger.getLogger(AttributeNameDaoImpl.class);
