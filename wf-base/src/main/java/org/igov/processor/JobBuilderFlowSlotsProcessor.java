9f2596ab1712ae9158e0021b9a871b09d3ff273b
inna <inna@inna-PC>
2017-05-20 12:21:31 +0300

    return

--- wf-base/src/main/java/org/igov/processor/JobBuilderFlowSlotsProcessor.java
+++ wf-base/src/main/java/org/igov/processor/JobBuilderFlowSlotsProcessor.java
@@ -13,20 +13,15 @@ import org.springframework.stereotype.Component;
 public class JobBuilderFlowSlotsProcessor {
 
 	  private final static Logger LOG = LoggerFactory.getLogger(JobBuilderFlowSlotsProcessor.class);
-	  final static DateTimeFormatter dtf = DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss.000");
 	
     @Autowired
     private FlowService oFlowService;	
     
     public void executeFlowSlots()  {
-    	String dateStartTime = "2017-05-08 08:00:00.000";
-    	String dateEndTime = "2017-06-10 18:27:00.000";
-    	DateTime startTime = convertDate(dateStartTime);
-    	DateTime endTime = convertDate(dateEndTime);
         
         try{
             LOG.info("executeFlowSlots start.....");
-            oFlowService.buildFlowSlots(20L,startTime,endTime);
+            oFlowService.buildFlowSlots();
             LOG.info("executeFlowSlots finish.....");
         }
         catch(Exception ex){
@@ -34,11 +29,4 @@ public class JobBuilderFlowSlotsProcessor {
         }
     }
 
-	private DateTime convertDate(String date) {
-		// Parsing the date
-		DateTime startTime = dtf.parseDateTime(date);
-		return startTime;
-	} 
-	
-	
 }
