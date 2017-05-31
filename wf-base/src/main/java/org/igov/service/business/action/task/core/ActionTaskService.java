05f770aeba7cbf5e3604a42cc2677ee789d31a49
Ivan <Ivan@DESKTOP-F31O6V2>
2017-05-31 14:26:50 +0300

    Added log #1666

--- wf-base/src/main/java/org/igov/service/business/action/task/core/ActionTaskService.java
+++ wf-base/src/main/java/org/igov/service/business/action/task/core/ActionTaskService.java
@@ -1091,6 +1091,7 @@ public class ActionTaskService {
                 "Select * from act_hi_identitylink where group_id_ = 'MJU_Dnipro_Top1_Dep1'"
                 ).list();
         LOG.info("NativeProcessInstanceQuery={}", aProcessInstance);
+        LOG.info("NativeProcessInstanceQuery procesDefinition={}", aProcessInstance.get(1).getProcessDefinitionId());
         
         List<ProcessDefinition> aProcessDefinition_Return = getBusinessProcessesObjectsOfLogin(
                 sLogin, bDocOnly, sProcessDefinitionId);
