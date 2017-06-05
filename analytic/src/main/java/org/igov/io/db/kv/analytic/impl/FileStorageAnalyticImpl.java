236dd8d28554743898c50c4cf1925002b7a094a7
dpekach <dImA1995>
2017-06-05 18:57:25 +0300

    #1419: refactoring: started to implement 'Strategy' pattern

--- analytic/src/main/java/org/igov/io/db/kv/analytic/impl/FileStorageAnalyticImpl.java
+++ analytic/src/main/java/org/igov/io/db/kv/analytic/impl/FileStorageAnalyticImpl.java
@@ -1,68 +1,23 @@
 package org.igov.io.db.kv.analytic.impl;
 
 
-import org.igov.io.db.kv.analytic.FileStorageAnalytic;
-import org.igov.io.db.kv.statical.exceptions.RecordNotFoundException;
-import org.igov.io.db.kv.statical.impl.FileStorage;
-import org.igov.io.db.kv.statical.model.UploadedFile;
-import org.igov.io.db.kv.statical.model.UploadedFileMetadata;
+import org.igov.io.db.kv.analytic.SaveToMongoStorage;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
+
 import org.springframework.data.mongodb.gridfs.GridFsTemplate;
-import org.springframework.web.multipart.MultipartFile;
 
-import javax.annotation.PostConstruct;
-import java.io.InputStream;
 
-public class FileStorageAnalyticImpl
-        implements FileStorageAnalytic {
+public class FileStorageAnalyticImpl extends SaveToMongoStorage {
+
+    private final GridFsTemplate gridAnalyticTemplate;
 
     @Autowired
-    private FileStorage fileStorage;
-
-
-    @Autowired
-    @Qualifier("gridAnalyticTemplate")
-    private GridFsTemplate gridAnalyticTemplate;
-
-    @PostConstruct
-    private void init() {
-
+    public FileStorageAnalyticImpl(GridFsTemplate gridAnalyticTemplate) {
+        this.gridAnalyticTemplate = gridAnalyticTemplate;
     }
 
     @Override
-    public String createFile(MultipartFile file) {
-        return fileStorage.createFile(file);
-    }
-
-    @Override
-    public boolean saveFile(String key, MultipartFile file) {
-        fileStorage.setoGridFsTemplate(gridAnalyticTemplate);
-        return fileStorage.saveFile(key, file);
-    }
-
-    @Override
-    public boolean remove(String key) {
-        return fileStorage.remove(key);
-    }
-
-    @Override
-    public boolean keyExists(String key) {
-        return fileStorage.keyExists(key);
-    }
-
-    @Override
-    public UploadedFile getFile(String key) {
-        return fileStorage.getFile(key);
-    }
-
-    @Override
-    public UploadedFileMetadata getFileMetadata(String key) {
-        return fileStorage.getFileMetadata(key);
-    }
-
-    @Override
-    public InputStream openFileStream(String key) throws RecordNotFoundException {
-        return fileStorage.openFileStream(key);
+    public GridFsTemplate getGridTemplate() {
+        return this.gridAnalyticTemplate;
     }
 }
