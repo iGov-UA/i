81479d1cb5b6159d227cfa8840550ffac7d3fbbb
BW <bw@bw-laptop>
2017-06-07 02:04:53 +0300

    getSessionId

--- wf-base/src/main/java/org/igov/service/business/promin/ProminSession_Singleton.java
+++ wf-base/src/main/java/org/igov/service/business/promin/ProminSession_Singleton.java
@@ -120,10 +120,10 @@ public class ProminSession_Singleton {
         //String xmlResponse = new RestRequest().post(uriSid, xml, MediaType.TEXT_XML,
         //        StandardCharsets.UTF_8, String.class, httpHeaders);
         try {
-            String xmlResponse = httpRequester.postInside(uriSid, null, xml,
-                    "text/xml; charset=utf-8", null, null);
-            LOG.info("Response from SID generator: {}", xmlResponse);
-            sessionId = getSidFromXml(xmlResponse);
+//=            String xmlResponse = httpRequester.postInside(uriSid, null, xml,
+//=                    "text/xml; charset=utf-8", null, null);
+//=            LOG.info("Response from SID generator: {}", xmlResponse);
+//=            sessionId = getSidFromXml(xmlResponse);
         } catch (Exception ex) {
             LOG.error("", ex);
         }
