09adf45b93bbeaf0a969075c60a7474bdd45bb67
Kovylin Yegor <egor.kovylin@privatbank.ua>
2017-06-08 14:33:01 +0300

    realize dictionary processing

--- wf-base/src/main/java/org/igov/service/buisness/dictionary/DictionaryService.java
+++ wf-base/src/main/java/org/igov/service/buisness/dictionary/DictionaryService.java
@@ -20,7 +20,7 @@ import org.springframework.stereotype.Component;
  *
  * @author Kovilin
  */
-@Component("documentStepService")
+@Component("DictionaryService")
 @Service
 public class DictionaryService {
     
