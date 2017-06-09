5311e30254f9e67de26faf6931cb7024c957a34e
dpekach <dImA1995>
2017-06-09 19:37:22 +0300

    #1419: file-name patch

--- wf-region/src/main/java/org/igov/service/migration/MigrationServiceImpl.java
+++ wf-region/src/main/java/org/igov/service/migration/MigrationServiceImpl.java
@@ -365,7 +365,7 @@ public class MigrationServiceImpl implements MigrationService {
                 type = attributeTypeDao.findById(7L).get();
                 Attribute_File fileAttribute = new Attribute_File();
                 Map<String, String> fileValuesMap = parseJsonStringToMap(string);
-                fileAttribute.setsFileName(fileValuesMap.get("sFileNameAndExt").split("\\.")[0]);
+                fileAttribute.setsFileName(fileValuesMap.get("sFileNameAndExt"));
                 fileAttribute.setsExtName(fileValuesMap.get("sFileNameAndExt").split("\\.")[1]);
                 fileAttribute.setsContentType(fileValuesMap.get("sContentType"));
                 String newKey = transferAttachment("Mongo-" + fileValuesMap.get("sKey"), fileValuesMap.get("sFileNameAndExt"), fileValuesMap.get("sContentType"));
