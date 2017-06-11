73ff03d1758d426f28b1053174ebe8c577ca0e20
inna <inna@inna-PC>
2017-06-11 20:50:17 +0300

    fix

--- wf-base/src/main/java/org/igov/service/business/util/DateUtilFormat.java
+++ wf-base/src/main/java/org/igov/service/business/util/DateUtilFormat.java
@@ -11,7 +11,7 @@ import com.google.common.collect.Lists;
  */
 public interface DateUtilFormat {
 	public final static List<String> formats = Lists.newArrayList("yyyy-MM-dd", "dd-MM-yyyy", "yyyy/MM/dd", "dd/MM/yyyy", "yyyy.MM.dd", "yyyyMMdd",
-			"dd.MM.yyyy", "yyyyy-MM-dd HH:mm:ss", "yyyyy/MM/dd HH:mm:ss", "yyyyy.MM.dd HH:mm:ss", "EEE MMM dd HH:mm:ss zzz yyyy");
+			"dd.MM.yyyy", "yyyy-MM-dd HH:mm:ss", "yyyy/MM/dd HH:mm:ss", "yyyy.MM.dd HH:mm:ss", "EEE MMM dd HH:mm:ss zzz yyyy");
 	public final static String DATE_FORMAT_yyyyMMdd = "yyyyMMdd";
     public final static String DATE_FORMAT_yyyy_MM_dd = "yyyy-MM-dd";
     public final static String DATE_FORMAT_dd_SLASH_MM_SLASH_yyyy = "dd/MM/yyyy";
