e9b4f9f314e54b7bd5fd8b8e6559acba6dc68505
Oleksii Khalikov <oleksii.khalikov@gmail.com>
2017-06-13 07:02:30 +0300

    https://github.com/e-government-ua/i/issues/1673 add scanner

--- dashboard-js/client/app/tasks/form-fields/file.directive.js
+++ dashboard-js/client/app/tasks/form-fields/file.directive.js
@@ -1,5 +1,5 @@
 'use strict';
-angular.module('dashboardJsApp').directive('fileField', function() {
+angular.module('dashboardJsApp').directive('fileField', function($modal, $http, generationService) {
   return {
     require: 'ngModel',
     restrict: 'E',
@@ -15,20 +15,47 @@ angular.module('dashboardJsApp').directive('fileField', function() {
       fileField.bind('click', function(e) {
         e.stopPropagation();
       });
-      element.bind('click', function(e) {
+      element.find('#upload-button').bind('click', function(e) {
         e.preventDefault();
         fileField[0].click();
       });
+
+
+
+      scope.openScanModal = function (item) {
+        $http.get('http://127.0.0.1:9005/TWAIN@Web/').success(function (data) {
+          if(data){
+            scanDocument();
+          } else {
+            alert('Для роботи зі сканером необхідно встановити программу TWAIN@Web')
+          }
+        }).error(function (err) {
+          alert('Для роботи зі сканером необхідно встановити программу TWAIN@Web')
+        });
+
+      };
+
+      function scanDocument() {
+        var modalInstance = $modal.open({
+          animation: true,
+          templateUrl: 'components/scanner/scan-modal.html',
+          controller: 'ScannerModalCtrl',
+          resolve: {}
+        });
+
+        modalInstance.result.then(function (oScanResult) {
+          var aFiles = [];
+          var base64content = oScanResult.downloadFiles["0"].base64;
+
+          var oScannedFile = generationService.getFileFromBase64(base64content, oScanResult.downloadFiles["0"].file, 'image/jpeg');
+
+          aFiles.push(oScannedFile);
+          scope.upload(aFiles, attrs.name);
+
+        });
+      }
     },
-    template: '<form>' +
-                '<button type="button" ng-disabled="isFileProcessUploading.bState" ng-class="{\'btn-igov\':field && field.value, \'btn-link attach-btn\':!field, \'btn-default\':field && !field.value}" class="btn">' +
-                  '<span ng-disabled="isFormPropertyDisabled(item)">{{field && field.value ? "Завантажити iнший файл" : "Завантажити файл"}}</span>' +
-                  '<input type="file" style="display:none" ng-disabled="isFormPropertyDisabled(item)">' +
-                '</button>' +
-                '<span ng-if="item.fileName || field.fileName">Файл: <label>{{item.fileName || field.fileName}}</label></span>' +
-                '<br>' +
-                '<span ng-if="field.signInfo">Пiдпис: <label>{{field.signInfo.customer.signatureData.name || field.signInfo.name}}</label></span>' +
-              '</form>',
+    templateUrl: 'app/tasks/form-fields/file-field.html',
     replace: true,
     transclude: true
   };
