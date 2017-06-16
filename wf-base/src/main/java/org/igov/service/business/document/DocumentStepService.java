dd15b14a70916b713f84ca90f8e40b997a6b931b
Ivan <Ivan@DESKTOP-F31O6V2>
2017-06-16 09:15:27 +0300

    Added log

--- wf-base/src/main/java/org/igov/service/business/document/DocumentStepService.java
+++ wf-base/src/main/java/org/igov/service/business/document/DocumentStepService.java
@@ -1938,6 +1938,7 @@ public class DocumentStepService {
                         HistoricProcessInstance oHistoricProcessInstance = historyService
                                 .createHistoricProcessInstanceQuery().processInstanceId(snID_Process_Activiti)
                                 .singleResult();
+                        LOG.info("oHistoricProcessInstance.id={}, snID_Process_Activiti={}", oHistoricProcessInstance.getId(), snID_Process_Activiti);
 
                         if (oHistoricProcessInstance != null) {
                             ProcessDefinition oProcessDefinition = repositoryService.createProcessDefinitionQuery()
