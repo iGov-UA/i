5289cca86807ca6d72569030d55e9bbdd20add99
antonlegkiy <anton.legkiy@gmail.com>
2017-06-16 16:19:00 +0300

    fix

--- central-js/client/app/service/built-in/controllers/servicebuiltinbankid.controller.js
+++ central-js/client/app/service/built-in/controllers/servicebuiltinbankid.controller.js
@@ -1287,7 +1287,7 @@ angular.module('app').controller('ServiceBuiltInBankIDController', ['$sce', '$st
 
                 openUrl(res.ascUrl, {pareq: res.pareq, md: res.md, TermUrl: callbackUrl}, '_self');
               } else if(res.secure && res.secure === 'otp') {
-                $scope.phoneVerify.otpIsConfirmed = false;
+                $scope.phoneVerify.otpIsConfirmed = true;
                 $scope.otpErrorMsg = null;
                 $scope.checkoutConfirm.status = 'confirm';
                 $scope.checkoutData.payment= {otpToken: res.token, otpCode: '', invoice: res.invoice, pmt_id: res.pmt_id};
