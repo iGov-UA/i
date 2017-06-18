ebdad6b7c904651c5bdb11ab11feb27264b6385a
iDoc-2 <iDoc-2@bt-09-03.btsol.local>
2017-06-18 14:35:11 +0300

    refactoring

--- wf-base/src/main/java/org/igov/util/ToolLuna.java
+++ wf-base/src/main/java/org/igov/util/ToolLuna.java
@@ -1,11 +1,13 @@
 package org.igov.util;
 
-import org.slf4j.Logger;import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Random;
 import org.igov.service.exception.CRCInvalidException;
 
 public class ToolLuna {
+
     private static final Logger LOG = LoggerFactory.getLogger(ToolLuna.class);
 
     private static int getLastDigit(Long inputNumber) {
@@ -31,21 +33,6 @@ public class ToolLuna {
         return sum;
     }
 
-    // Calculate the sum of digits while it became one digit
-    private static int sumDigits(int sum) {
-        int result;
-        do {
-            result = 0;
-            while (sum != 0) {
-                result += sum % 10;
-                sum /= 10;
-            }
-            sum = result;
-        }
-        while (result > 9);
-        return result;
-    }
-
     public static long getProtectedNumber(long inputNumber) {
         return getCheckSumLastDigit(inputNumber) + inputNumber * 10;
     }
@@ -59,8 +46,8 @@ public class ToolLuna {
 
         LOG.info("inputNumber / 10={}", originalNumber);
         LOG.info("(inputNumber={})", inputNumber);
-        LOG.info("getLastDigit(inputNumber)={}",  getLastDigit(inputNumber));
-        LOG.info("getCheckSumLastDigit(inputNumber / 10)={}",  getCheckSumLastDigit(originalNumber));
+        LOG.info("getLastDigit(inputNumber)={}", getLastDigit(inputNumber));
+        LOG.info("getCheckSumLastDigit(inputNumber / 10)={}", getCheckSumLastDigit(originalNumber));
         return getCheckSumLastDigit(originalNumber) == getLastDigit(inputNumber);
     }
 
@@ -75,6 +62,15 @@ public class ToolLuna {
             throw new CRCInvalidException(errorMessage);
         }
     }
+    
+    public static String getProtectedString(Long inputNumber, Long nID_subject, String sID_status) {
+        return "" + inputNumber;
+    }
+
+    public static long getValidatedOriginalNumber(long protectedNumber) throws CRCInvalidException {
+        validateProtectedNumber(protectedNumber);
+        return getOriginalNumber(protectedNumber);
+    }
 
     public static void main(String[] args) {
         Random random = new Random();
@@ -85,49 +81,17 @@ public class ToolLuna {
         System.out.println("Long.MAX_VALUE=" + Long.MAX_VALUE);
         for (int i = 0; i < arrSize; i++) {
             currValue = random.nextLong();
-            while (currValue < 0 || currValue > Long.MAX_VALUE / 10)
+            while (currValue < 0 || currValue > Long.MAX_VALUE / 10) {
                 currValue = random.nextLong();
+            }
             testArray[i] = currValue;
             testProtectedArray[i] = getProtectedNumber(testArray[i]);
             System.out.println(">>test " + i
                     + ":   nID=" + testArray[i]
                     + ",   checkSum=" + getCheckSumLastDigit(testArray[i])
                     + ",   controlDigit=" + getCheckSumLastDigit(testArray[i] * 10)
-
                     + ",   nID_Protected=" + testProtectedArray[i]
                     + ",   checkResult=" + checkProtectedNumber(testProtectedArray[i]));
         }
-    }
-
-    public static String getProtectedString(Long inputNumber, Long nID_subject, String sID_status) {
-         /*поле нИД генерируется автоматом
-поле сИД --  генерируется (используя алг.Луна) на основе нИД И (если есть сИД_Статус) на основе поля сИД_Статус.
- еще в основу и вкладываем часть данных как логин и часть как пароль...
-на самом деле все просто)
-Вот калькулятор: http://planetcalc.ru/2464/
-а) допустим у нас есть в nID число "12345678"
-по алгоритму получаем:
-Последний разряд контрольной суммы: 4
-Следующая проверочная цифра: 2
-б) так-же берем nID_Subject (как логин), например 16
-по алгоритму получаем:
-Последний разряд контрольной суммы: 8
-Следующая проверочная цифра: 6
-в) "собираем воедино все цифры":
-12345678 & 4 & 2 & 16 & 8 & 6
-т.е.: 12345678421686
-г) превращаем это десятиричное число в 27+27+10=64-ричное, символьное из латинских больших(27) и маленьких(27) букв и цифр(10) 64-тиричная (0..9a..zA..Z)
-получим что-то вроде G8hi3 (т.е. 5 символов)
-д) остается еще 5 свободных (до 10-ти обозначенных в задании), для случаянно-сгенерированного пароля
-например, 7Klg2
-е) в итоге получаем уникальный sID="G8hi37Klg2", которы никогда не повторится, если nID и nID_Subject будут другими нежели в нашем случае (даже если когдато сгенерится такой-же пароль)
-* */
-
-        return "" + inputNumber;
-    }
-
-    public static long getValidatedOriginalNumber(long protectedNumber) throws CRCInvalidException {
-        validateProtectedNumber(protectedNumber);
-        return getOriginalNumber(protectedNumber);
     }
 }
--- wf-region/src/main/java/org/igov/service/controller/ProcessController.java
+++ wf-region/src/main/java/org/igov/service/controller/ProcessController.java
@@ -35,6 +35,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
 import java.util.*;
+import org.igov.model.action.task.core.ProcessIdCover;
 
 /**
  * @author olga
@@ -73,11 +74,9 @@ public class ProcessController {
         migrationService.migrateOldRecords(processId);
     }
 
-
     @ApiOperation(value = "/backup", notes = "##### Process - сохранение процесса #####\n\n")
     @RequestMapping(value = "/backup", method = RequestMethod.GET)
-    public
-    @ResponseBody
+    public @ResponseBody
     void backup() throws Exception {
         LOG.info("/backup!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! :)");
         archiveService.archiveData();
@@ -112,20 +111,17 @@ public class ProcessController {
         List<HistoricVariableInstance> variablesList = historyService.createHistoricVariableInstanceQuery().processInstanceId(currentProcessInstanceId).list();
         archiveProcess.setaAttribute(createAttributeForArchiveProcess(variablesList));
 
-
         return archiveProcess;
     }
 
     //TODO write this method up to the end, it doesn't work now, ask Olga
     private List<Attribute> createAttributeForArchiveProcess(List<HistoricVariableInstance> variableInstances) {
         List<Attribute> resultList = new LinkedList<>();
-
         for (HistoricVariableInstance instance : variableInstances) {
             Attribute attribute = new Attribute();
         }
         return null;
     }
-
 
     private CustomProcess createNewCustomArchiveProcess(HistoricProcessInstance instance) {
         CustomProcess archiveCustomProcess = new CustomProcess();
@@ -148,8 +144,7 @@ public class ProcessController {
     //http://localhost:8080/wf-region/service/analytic/process/getProcesses?sID_=1
     @ApiOperation(value = "/getProcesses", notes = "##### Process - получение процесса #####\n\n")
     @RequestMapping(value = "/getProcesses", method = RequestMethod.GET, headers = {JSON_TYPE})
-    public
-    @ResponseBody
+    public @ResponseBody
     List<Process> getProcesses(
             @ApiParam(value = "внутренний ид заявки", required = true) @RequestParam(value = "sID_") String sID_,
             @ApiParam(value = "ид источника", required = false) @RequestParam(value = "nID_Source", required = false) Long nID_Source) {
@@ -159,104 +154,24 @@ public class ProcessController {
             LOG.info("/getProcess!!!!!!!!!!!!!!!!!!!!string sID_: " + sID_.trim());
             sID_ = sID_.trim().toUpperCase();
             if (sID_.length() >= 3) {
+                if (sID_.indexOf("-") == 1) { //передали sID_Order вместо nID_Process
+                    Long nID_Process = new ProcessIdCover(sID_, null, null, null).nID_Process();
+                    sID_ = String.valueOf(nID_Process);
+                }
                 List<Process> processes = processDao.findAllBy("sID_", sID_);
                 LOG.info("processes: " + processes.size());
                 result.addAll(processes);
             }
         } catch (Exception ex) {
             LOG.error("ex: ", ex);
-            Process process = createStub();
-            process.setsID_(ex.getMessage());
-            result.add(process);
         }
         return result;
-    }
-
-    private Process createStub() {
-        LOG.info("/createStub!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! :)");
-        Process process = new Process();
-        ProcessTask processTask = new ProcessTask();
-        Attribute attribute = new Attribute();
-        Attribute attribute1 = new Attribute();
-        Attribute_StringShort attribute_StringShort = new Attribute_StringShort();
-        Attribute_File attribute_File = new Attribute_File();
-        AccessGroup accessGroup = new AccessGroup();
-        AccessUser accessUser = new AccessUser();
-        SourceDB sourceDB = new SourceDB();
-        AttributeType attributeType = new AttributeType();
-        AttributeType attributeType1 = new AttributeType();
-        //---------------------------
-        process.setId(new Long(1));
-        process.setoDateStart(new DateTime());
-        process.setoDateFinish(new DateTime());
-        process.setoSourceDB(sourceDB);
-        process.setsID_("test");
-        process.setsID_Data("test");
-
-        List<ProcessTask> tasks = new ArrayList();
-        tasks.add(processTask);
-        process.setaProcessTask(tasks);
-
-        List<Attribute> attributes = new ArrayList();
-        attributes.add(attribute);
-        attributes.add(attribute1);
-        process.setaAttribute(attributes);
-        //process.setaAttribute(attributes);
-        //-------------------------------
-        processTask.setId(new Long(1));
-        processTask.setoDateStart(new DateTime());
-        processTask.setoDateFinish(new DateTime());
-        processTask.setsID_("test");
-        Set<AccessGroup> accessGroups = new HashSet<>();
-        accessGroups.add(accessGroup);
-        processTask.setaAccessGroup(accessGroups);
-        Set<AccessUser> accessUsers = new HashSet<>();
-        accessUsers.add(accessUser);
-        processTask.setaAccessUser(accessUsers);
-        //------------------------------
-        attribute.setId(new Long(1));
-        attribute.setoAttributeType(attributeType);
-        //attribute.setoAttribute_StingShort(attribute_StringShort);
-        attribute.setoAttribute_File(attribute_File);
-        attribute.setsID_("test");
-        attribute.setName("test");
-
-        attribute1.setId(new Long(2));
-        attribute1.setoAttributeType(attributeType1);
-        attribute1.setoAttribute_StringShort(attribute_StringShort);
-        //attribute.setoAttribute_File(attribute_File);
-        attribute1.setsID_("test");
-        attribute1.setName("test");
-        //------------------------------
-        attribute_StringShort.setId(new Long(1));
-        attribute_StringShort.setsValue("attribute_StringShort");
-        //------------------------------
-        attribute_File.setId(new Long(1));
-        attribute_File.setsID_Data("test");
-        attribute_File.setsFileName("test");
-        attribute_File.setsContentType("pdf");
-        attribute_File.setsExtName("txt");
-        //-------------------------------
-        accessGroup.setId(new Long(1));
-        accessGroup.setsID("test");
-        //--------------------------------
-        accessUser.setId(new Long(1));
-        accessUser.setsID("test");
-        //--------------------------------
-        sourceDB.setId(new Long(1));
-        sourceDB.setName("Gorsovet");
-        attributeType.setId(new Long(7));
-        attributeType.setName("File");
-        attributeType1.setId(new Long(3));
-        attributeType1.setName("StingShort");
-        return process;
     }
 
     //http://localhost:8080/wf-region/service/analytic/process/getFile?nID_Attribute_File=1
     @ApiOperation(value = "/getFile", notes = "##### File - получение контента файла #####\n\n")
     @RequestMapping(value = "/getFile", method = RequestMethod.GET, headers = {JSON_TYPE})
-    public
-    @ResponseBody
+    public @ResponseBody
     byte[] getFile(
             @ApiParam(value = "внутренний ид заявки", required = true) @RequestParam(value = "nID_Attribute_File") Long nID_Attribute_File,
             HttpServletResponse httpResponse) throws RecordNotFoundException {
@@ -272,22 +187,19 @@ public class ProcessController {
                         file.getsFileName(), file.getsFileName() + "." + file.getsExtName(), file.getsContentType());
                 httpResponse.setCharacterEncoding("UTF-8");
                 httpResponse.setHeader("Content-disposition", "attachment; filename=" + multipartFile.getName());
-                //httpResponse.setHeader("Content-Type", "application/octet-stream");
                 httpResponse.setHeader("Content-Type", multipartFile.getContentType());
                 httpResponse.setContentLength(multipartFile.getBytes() != null ? multipartFile.getBytes().length : 0);
             }
             LOG.info("multipartFile: " + multipartFile);
-            return ((multipartFile != null && multipartFile.getBytes() != null) ?
-                    multipartFile.getBytes() :
-                    "".getBytes());
+            return ((multipartFile != null && multipartFile.getBytes() != null)
+                    ? multipartFile.getBytes()
+                    : "".getBytes());
         } catch (Exception ex) {
             LOG.error("!!!Error: ", ex);
             httpResponse.setCharacterEncoding("UTF-8");
             httpResponse
                     .setHeader("Content-disposition", "attachment; filename=fileNotFound.txt"); //"Content-Disposition"
-            //httpResponse.setHeader("Content-Type", "application/octet-stream");
             httpResponse.setHeader("Content-Type", "application/octet-stream; charset=UTF-8");
-            //httpResponse.setContentLength(10);
             return ("error: " + ex.getMessage()).getBytes();
         }
     }
