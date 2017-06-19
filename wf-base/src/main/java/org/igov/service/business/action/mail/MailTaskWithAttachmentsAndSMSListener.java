823f8b2879157db0f1cd7537e529b49ffc2ca213
Kovylin Yegor <egor.kovylin@privatbank.ua>
2017-06-20 00:24:15 +0300

    rollback and add mail listener

--- wf-base/src/main/java/org/igov/service/business/action/mail/MailTaskWithAttachmentsAndSMSListener.java
+++ wf-base/src/main/java/org/igov/service/business/action/mail/MailTaskWithAttachmentsAndSMSListener.java
@@ -1,43 +1,43 @@
 package org.igov.service.business.action.mail;
 
-import org.springframework.stereotype.Service;
 import org.activiti.engine.ActivitiObjectNotFoundException;
 import org.activiti.engine.delegate.DelegateExecution;
 import org.activiti.engine.delegate.Expression;
 import org.activiti.engine.task.Attachment;
 import org.apache.commons.mail.ByteArrayDataSource;
+import org.springframework.stereotype.Component;
 import org.igov.io.mail.Mail;
 
 import javax.activation.DataSource;
 
 import static org.igov.util.ToolLuna.getProtectedNumber;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import java.io.InputStream;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import static org.igov.service.business.action.task.core.AbstractModelTask.getStringFromFieldExpression;
 import org.igov.service.business.action.task.systemtask.mail.Abstract_MailTaskCustom;
+import static org.igov.util.ToolLuna.getProtectedNumber;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.web.multipart.MultipartFile;
 
 /**
- *
- * @author Kovylin
+ * @author BW
  */
-@Service
-public class MailTaskWithAttachmentsAndSMSService extends Abstract_MailTaskCustom {
-
-    private final static Logger LOG = LoggerFactory.getLogger(MailTaskWithAttachmentsAndSMSService.class);
+@Component("MailTaskWithAttachmentsAndSMSListener")
+public class MailTaskWithAttachmentsAndSMSListener extends Abstract_MailTaskCustom {
 
     private Expression saAttachmentsForSend;
     protected Expression sPhone_SMS;
     protected Expression sText_SMS;
-
-    public void MailTaskWithAttachmentsAndSMS(DelegateExecution oExecution) throws Exception {
+    
+    private final static Logger LOG = LoggerFactory.getLogger(MailTaskWithAttachmentsAndSMSListener.class);
+    
+    @Override
+    public void execute(DelegateExecution oExecution) throws Exception {
 
         Mail oMail = Mail_BaseFromTask(oExecution);
 
@@ -157,4 +157,5 @@ public class MailTaskWithAttachmentsAndSMSService extends Abstract_MailTaskCusto
             throw ex;
         }
     }
+
 }
