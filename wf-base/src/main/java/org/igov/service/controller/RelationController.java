cda9f27c7cd971cdd4eb96bf1db3ad79d92e2615
Kovylin Yegor <egor.kovylin@privatbank.ua>
2017-06-19 12:45:54 +0300

    test

--- wf-base/src/main/java/org/igov/service/controller/RelationController.java
+++ wf-base/src/main/java/org/igov/service/controller/RelationController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.transaction.annotation.Transactional;
-
+ 
 
 /**
  *
