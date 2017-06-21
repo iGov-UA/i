b7bd70c922dcb4ddc352ce718f82ef86a6808cc1
Kovylin Yegor <egor.kovylin@privatbank.ua>
2017-06-21 11:27:39 +0300

    find error for #1693

--- wf-base/src/main/java/org/igov/service/controller/ActionTaskCommonController.java
+++ wf-base/src/main/java/org/igov/service/controller/ActionTaskCommonController.java
@@ -2983,7 +2983,7 @@ public class ActionTaskCommonController {//extends ExecutionBaseResource
     //save curretn values to Form
     @ApiOperation(value = "saveForm", notes = "saveForm")
     @RequestMapping(value = "/saveForm", method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
-    public HttpServletRequest saveForm(
+    public String saveForm(
             @ApiParam(value = "проперти формы", required = false) @RequestBody String sParams, HttpServletRequest req)
             throws ParseException, CommonServiceException, IOException {
         
