852be7255c5ddb43e9aee41d9ef0f810b5c0a42d
Valery.Stavitsky <Valery.Stavitsky@gmail.com>
2017-07-11 14:40:13 +0300

    накат альфы на гамму не собиралось, потому были дополнительно удалены файлы wf-base\src\main\java\org\igov\service\business\action\task\systemtask\mail\MailTaskWithAttachment.java wf-base\src\main\java\org\igov\service\business\action\task\systemtask\mail\MailTaskWithoutAttachmentBase.java

---
+++ wf-base/src/main/java/org/igov/model/arm/ArmDao.java
@@ -0,0 +1,18 @@
+package org.igov.model.arm;
+
+import java.util.List;
+
+public interface ArmDao {
+	
+	public List<DboTkModel> getDboTkByOutNumber(String outNumber);
+	
+	public DboTkResult createDboTk(DboTkModel dboTkModel);
+	
+	public DboTkResult updateDboTk(DboTkModel dboTkModel);
+	
+	public DboTkResult updateDboTkByExpert(DboTkModel dboTkModel);
+	
+	public Integer getMaxValue();
+	
+	public Integer getMaxValue442(); 
+}
