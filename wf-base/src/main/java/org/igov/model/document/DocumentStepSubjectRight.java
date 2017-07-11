5b975d2b5618c543f6aeddce413e84deed2aec32
Ivan <Ivan@DESKTOP-F31O6V2>
2017-07-11 13:50:50 +0300

    Rollback changes

--- wf-base/src/main/java/org/igov/model/document/DocumentStepSubjectRight.java
+++ wf-base/src/main/java/org/igov/model/document/DocumentStepSubjectRight.java
@@ -34,7 +34,7 @@ public class DocumentStepSubjectRight extends AbstractEntity {
 	private static final transient Logger LOG = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
 	@JsonIgnore
-	@ManyToOne(fetch = FetchType.EAGER)
+	@ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
 	@JoinColumn(name = "nID_DocumentStep")
 	private DocumentStep documentStep;
 
