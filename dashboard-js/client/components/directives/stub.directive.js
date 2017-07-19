b77a04da11a020eddfcaa7b5b897b1ea2e8c8064
antonlegkiy <anton.legkiy@gmail.com>
2017-07-19 22:13:05 +0300

    you can customize text in stub directive

--- dashboard-js/client/components/directives/stub.directive.js
+++ dashboard-js/client/components/directives/stub.directive.js
@@ -1,10 +1,10 @@
 angular.module('dashboardJsApp').directive('stub', function ($timeout) {
   return {
     restrict: 'A',
-    link: function (scope, elem) {
+    link: function (scope, elem, as) {
       $timeout(function() {
         if(!elem.context.children[0]) {
-          angular.element(elem).append('<span>Відпрацювати</span>')
+          angular.element(elem).append('<span>' + elem.context.attributes.stub.nodeValue + '</span>')
         }
       })
     }
