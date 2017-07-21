f7291c5e009239995a9ccfa595f452847124d1be
iDoc-2 <iDoc-2@bt-09-03.btsol.local>
2017-07-21 17:32:54 +0300

    fix

--- wf-base/src/main/java/org/igov/service/processUtil/ProcessUtilService.java
+++ wf-base/src/main/java/org/igov/service/processUtil/ProcessUtilService.java
@@ -1,57 +1,66 @@
 package org.igov.service.processUtil;
+
 import java.util.List;
 import java.util.Map;
 import java.util.HashMap;
 import org.activiti.engine.HistoryService;
 import org.activiti.engine.TaskService;
 import org.activiti.engine.history.HistoricTaskInstance;
+import org.activiti.engine.task.Task;
+import org.igov.service.business.action.task.core.ActionTaskService;
+import org.igov.service.exception.CRCInvalidException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
-
 @Component("processUtilService")
 @Service
 public class ProcessUtilService {
-    
+
     @Autowired
     private HistoryService historyService;
-    
+
     @Autowired
     private TaskService taskService;
-    
-    public Map<String, Object> getmID_TaskAndProcess(String nID_Process){
-        
-        Map <String, Object> resulMap = new HashMap<>();
-        
-        resulMap.put("nID_Process", nID_Process);
-        
-        try{
-            resulMap.put("nID_Task_Active", taskService.createTaskQuery().processInstanceId(nID_Process).active().list().get(0).getId());
-        }catch (Exception ex){
-            resulMap.put("nID_Task_Active", null);
+
+    @Autowired
+    private ActionTaskService oActionTaskService;
+
+    public Map<String, Object> getmID_TaskAndProcess(Long nID_Order, String nID_Process) throws CRCInvalidException {
+
+        if (nID_Order != null) {
+            nID_Process = oActionTaskService.getOriginalProcessInstanceId(nID_Order);
         }
-        
-        try{
-           
+
+        Map<String, Object> resulMap = new HashMap<>();
+
+        resulMap.put("nID_Process", nID_Process);
+        String nID_Task_Active = null;
+        List<Task> aTask = taskService.createTaskQuery().processInstanceId(nID_Process).active().list();
+        if (aTask.size() > 0) {
+            nID_Task_Active = aTask.get(0).getId();
+        }
+        resulMap.put("nID_Task_Active", nID_Task_Active);
+
+        try {
+
             List<HistoricTaskInstance> aHistoricTaskInstance = historyService.createHistoricTaskInstanceQuery().processInstanceId(nID_Process).orderByHistoricTaskInstanceEndTime().desc().list();
-            
-            if(resulMap.get("nID_Task_Active") == null){
-               resulMap.put("nID_Task_HistoryLast", aHistoricTaskInstance.get(0).getId()); 
-            }
-            else{
-                if(aHistoricTaskInstance.size() > 1){
+
+            if (resulMap.get("nID_Task_Active") == null) {
+                resulMap.put("nID_Task_HistoryLast", aHistoricTaskInstance.get(0).getId());
+            } else {
+                if (aHistoricTaskInstance.size() > 1) {
                     resulMap.put("nID_Task_HistoryLast", aHistoricTaskInstance.get(1).getId());
-                }else{
+                } else {
                     resulMap.put("nID_Task_HistoryLast", null);
-                }  
+                }
             }
-            
-        }catch (Exception ex){
+
+        } catch (Exception ex) {
             resulMap.put("nID_Task_HistoryLast", null);
         }
-        
+
         return resulMap;
     }
-    
+
 }
