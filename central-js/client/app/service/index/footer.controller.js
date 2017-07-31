ad96e6180f9c431cdd4dbe5eed17cdcddd12420e
Oleksii Khalikov <oleksii.khalikov@gmail.com>
2017-07-31 18:48:05 +0300

    https://github.com/e-government-ua/i/issues/1675 fix snap footer
    (cherry picked from commit 28f7fd3)

--- central-js/client/app/service/index/footer.controller.js
+++ central-js/client/app/service/index/footer.controller.js
@@ -37,21 +37,29 @@ angular.module('app').controller('FooterController', function ($scope, SponsorsL
     var oHeader = $(".main-header");
     var oMain = $(".main");
     var oFooter = $(".main-footer");
-    if(oFooter[0] && oFooter[0].scrollHeight){
-      var nFooterHeight = oFooter[0].scrollHeight;
-      var nHeaderHeight = 0;
-      var nMainHeight = 0;
-      if(oHeader[0] && oHeader[0].scrollHeight){
-        nHeaderHeight = oHeader[0].scrollHeight;
-      }
-      if(oMain[0] && oMain[0].scrollHeight){
-        nMainHeight = oMain[0].scrollHeight;
-      }
+    var sStateName = $state.current.name;
+    if(sStateName === "index.journal" || sStateName === "index.service.general.place"){
+      if(oFooter[0] && oFooter[0].scrollHeight){
+        var nFooterHeight = oFooter[0].scrollHeight;
+        var nHeaderHeight = 0;
+        var nMainHeight = 0;
+        if(oHeader[0] && oHeader[0].scrollHeight){
+          nHeaderHeight = oHeader[0].scrollHeight;
+        }
+        if(oMain[0] && oMain[0].scrollHeight){
+          nMainHeight = oMain[0].scrollHeight;
+        }
 
+        oFooter.css({
+          top: Math.max(nHeaderHeight + nMainHeight, this.innerHeight - nFooterHeight),
+          left: (this.innerWidth - oFooter[0].scrollWidth)/2 < 0 ? 0 : (this.innerWidth - oFooter[0].scrollWidth)/2,
+          position:'absolute'
+        });
+      }
+    } else {
       oFooter.css({
-        top: Math.max(nHeaderHeight + nMainHeight, this.innerHeight - nFooterHeight),
         left: (this.innerWidth - oFooter[0].scrollWidth)/2 < 0 ? 0 : (this.innerWidth - oFooter[0].scrollWidth)/2,
-        position:'absolute'
+        position:'relative'
       });
     }
   }
