71b948eb825341d12d78db592459b67e87539e14
Kovylin Yegor <egor.kovylin@privatbank.ua>
2017-12-20 23:47:01 +0200

    realization for PLATFORM-44

--- wf-base/src/main/java/org/igov/service/business/flow/FlowService.java
+++ wf-base/src/main/java/org/igov/service/business/flow/FlowService.java
@@ -33,6 +33,8 @@ import javax.xml.datatype.Duration;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import org.activiti.engine.FormService;
 import org.activiti.engine.form.FormProperty;
 import org.igov.run.schedule.JobBuilderFlowSlots;
@@ -1105,5 +1107,18 @@ public class FlowService implements ApplicationContextAware {
         nStartDay += DAYS_IN_MONTH;
         boolean bEnoughFreeDays = nStartDay < DAYS_IN_HALF_YEAR;
         LOG.info(" bEnoughFreeDays = {}", bEnoughFreeDays);
+    }
+    
+    public Boolean checkFlowSessionPermition(HttpServletRequest oRequest){
+        HttpSession oHttpSession = oRequest.getSession();
+        LOG.info("Session {}", oHttpSession);
+        
+        if(oHttpSession != null && oHttpSession.getAttribute("sINN") != null){
+            LOG.info("Session sINN is {}", oHttpSession.getAttribute("sINN"));
+            LOG.info("Session ID is {}", oHttpSession.getId());
+            return true;
+        }else{
+            return false;
+        }
     }
 }
