cce245aa10ff13cfa12b4c6a1007ff99f9044298
Kovylin Yegor <egor.kovylin@privatbank.ua>
2018-01-18 10:29:00 +0200

    move SRVCSGOV-43 to master

--- wf-base/src/main/java/org/igov/service/business/action/task/systemtask/ReleaseTicketsOfQueue.java
+++ wf-base/src/main/java/org/igov/service/business/action/task/systemtask/ReleaseTicketsOfQueue.java
@@ -29,39 +29,41 @@ public class ReleaseTicketsOfQueue extends AbstractModelTask implements JavaDele
         LOG.info("(asFieldID={})", asFieldID.toString());
         List<String> asFieldValue = getVariableValues(oExecution, asFieldID);
         LOG.info("(asFieldValue={})", asFieldValue.toString());
-        if (!asFieldValue.isEmpty()) {
-            String sValue = asFieldValue.get(0);
-            LOG.info("(sValue={})", sValue);
-            long nID_FlowSlotTicket = 0;
+        
+        for(String sFieldValue : asFieldValue){
+            if (!sFieldValue.equals("null")) {
+                String sValue = sFieldValue;
+                LOG.info("(sValue={})", sValue);
+                long nID_FlowSlotTicket = 0;
 
-            Map<String, Object> m = QueueDataFormType.parseQueueData(sValue);
-            nID_FlowSlotTicket = QueueDataFormType.get_nID_FlowSlotTicket(m);
-            LOG.info("(nID_FlowSlotTicket={})", nID_FlowSlotTicket);
-            String sDate = (String) m.get(QueueDataFormType.sDate);
-            LOG.info("(sDate={})" + sDate);
+                Map<String, Object> m = QueueDataFormType.parseQueueData(sValue);
+                nID_FlowSlotTicket = QueueDataFormType.get_nID_FlowSlotTicket(m);
+                LOG.info("(nID_FlowSlotTicket={})", nID_FlowSlotTicket);
+                String sDate = (String) m.get(QueueDataFormType.sDate);
+                LOG.info("(sDate={})" + sDate);
 
-			try {
+                            try {
 
-				long nID_Task_Activiti = 1; // TODO set real ID!!!
-				try {
-					nID_Task_Activiti = Long.valueOf(oExecution.getProcessInstanceId());
-					LOG.info("nID_Task_Activiti:Ok!");
-				} catch (NumberFormatException oException) {
-					LOG.error(oException.getMessage());
-				}
-				LOG.info("(nID_Task_Activiti={})", nID_Task_Activiti);
+                                    long nID_Task_Activiti = 1; // TODO set real ID!!!
+                                    try {
+                                            nID_Task_Activiti = Long.valueOf(oExecution.getProcessInstanceId());
+                                            LOG.info("nID_Task_Activiti:Ok!");
+                                    } catch (NumberFormatException oException) {
+                                            LOG.error(oException.getMessage());
+                                    }
+                                    LOG.info("(nID_Task_Activiti={})", nID_Task_Activiti);
 
-				if (!oFlowSlotTicketDao.unbindFromTask(nID_FlowSlotTicket)) {
-					LOG.error("nID_Task_Activiti is empty for oFlowSlotTicket with ID {}", nID_FlowSlotTicket);
-				}
+                                    if (!oFlowSlotTicketDao.unbindFromTask(nID_FlowSlotTicket)) {
+                                            LOG.error("nID_Task_Activiti is empty for oFlowSlotTicket with ID {}", nID_FlowSlotTicket);
+                                    }
 
-			} catch (Exception oException) {
-				LOG.error("Error: {}",oException.getMessage());
-                LOG.trace("FAIL:", oException);
-			}
-
+                            } catch (Exception oException) {
+                                    LOG.error("Error: {}",oException.getMessage());
+                    LOG.trace("FAIL:", oException);
+                            }
+                break;            
+            }
         }
-
         LOG.info("ReleaseTicketsOfQueue:execute end");
     }
 
