d5041298b005a450f75cd06d630b95149cf94bd5
Alex <alkomarenko@gmail.com>
2018-03-07 16:40:51 +0200

    https://idoc-develop.atlassian.net/browse/SRVCSGOV-58 change url to iGov

--- wf-central/src/main/java/org/igov/service/controller/SubjectMessageController.java
+++ wf-central/src/main/java/org/igov/service/controller/SubjectMessageController.java
@@ -123,10 +123,10 @@ public class SubjectMessageController {
         SubjectMessage message = subjectMessagesDao.getMessage(nID);
         return JsonRestUtils.toJsonResponse(message);
     }
-    
-    
-    
-    
+
+
+
+
     @RequestMapping(value = "/getSID_Auth_PB_SMS", method = RequestMethod.GET,
             produces = MediaType.APPLICATION_JSON_VALUE, headers = {"Accept=application/json"})
     public
@@ -135,7 +135,7 @@ public class SubjectMessageController {
             @ApiParam(value = "", required = false) @RequestParam(value = "nID") Long nID) {
         return prominSession_Singleton.getSID_Auth_PB_SMS();
     }
-    
+
 
     @ApiOperation(value = "Сохранение сообщение ", notes = ""
             + "При заданных параметрах sID_Order или nID_Protected с/без nID_Server и sID_Rate - обновляется поле nRate в записи сущности HistoryEvent_Service, которая находится по sID_Order или nID_Protected с/без nID_Server (подробнее тут, при этом приходящее значение из параметра sID_Rate должно содержать число от 1 до 5. т.е. возможные ошибки:\n\n"
@@ -399,12 +399,12 @@ public class SubjectMessageController {
             }
             aSubjectMessage = subjectMessagesDao.getMessages(nID_HistoryEvent_Service);
             aoSubjectMessage.addAll(aSubjectMessage);
-            
+
             if(isRegion){
                 aoSubjectMessage.addAll(historyEventDao.getHistoryEvents(null, nID_HistoryEvent_Service, false));
             }
-            
-        
+
+
         } catch (Exception e) {
             LOG.error("FAIL: {}", e);
             //LOG.trace("FAIL:", e);
@@ -453,7 +453,7 @@ public class SubjectMessageController {
             if (bAuth) {
                 actionEventService.checkAuth(oHistoryEvent_Service, nID_Subject, sToken);
             }
-        
+
             if (isNotBlank(sID_File)) {
                 LOG.info("sID_File={}, sFileName={}", sID_File, sFileName);
                     byte[] aByte_FileContent = null;
@@ -480,7 +480,7 @@ public class SubjectMessageController {
                     }
                     sID_DataLink = accessDataDao.setAccessData(aByte_FileContent);   //accessDataService//sKey
                     sID_DataLinkSource="Central";
-                    LOG.info("Saved to Mongo! (sID_DataLink={},aByte_FileContent.length={})", sID_DataLink, aByte_FileContent.length);                    
+                    LOG.info("Saved to Mongo! (sID_DataLink={},aByte_FileContent.length={})", sID_DataLink, aByte_FileContent.length);
             }
 
             if (isNotBlank(sID_DataLink)) {
@@ -496,14 +496,14 @@ public class SubjectMessageController {
                 o.put("sFileContentType", sFileContentType);
                  o.put("sKey", sID_DataLink); //TODO: заменить на клиенте использование на одноименный параметр сущности
                 //o.put("sID_DataLink", sID_DataLink);
-                o.put("sID_DataLinkSource", sID_DataLinkSource); //TODO: заменить на сервере и клиенте использование на одноименный параметр сущности 
+                o.put("sID_DataLinkSource", sID_DataLinkSource); //TODO: заменить на сервере и клиенте использование на одноименный параметр сущности
                 o.put("sID_DataLinkAuthor", sID_DataLinkAuthor); //TODO: заменить на сервере и клиенте использование на одноименный параметр сущности
                 //sID_FileAuthor//SFS
                 oaFile.put(o);
                 sData = new JSONObject().put("aFile", oaFile).toString();
-                LOG.info("sData={}", sData);                
+                LOG.info("sData={}", sData);
             }
-            
+
             String sHost = null;
             int nID_Server = oHistoryEvent_Service.getnID_Server();
             Optional<Server> oOptionalServer = serverDao.findById(Long.valueOf(nID_Server + ""));
@@ -521,7 +521,7 @@ public class SubjectMessageController {
             mergeParams.put("key", "saTaskStatus");
 
             LOG.info("mergeParams={}, mergeUrl=", mergeParams, mergeUrl);
-            
+
             if (nID_SubjectMessageType == 8L) { //citizen's comment or question
                 mergeParams.put("insertValues", "GotUpdate");
                 //request to get clerk mail from userTask
@@ -532,6 +532,7 @@ public class SubjectMessageController {
                 String sMailClerk = httpRequester.getInside(sTaskDataUrl, requestParams);
                 LOG.info("Searched sMail={}", sMail);
                 String sBodyClerk = "Заявка " + sID_Order.split("-")[1] + ", отримала запитання від заявника.";
+                String sURL_Region = sHost.replaceAll("/wf/", "");
                 oNotificationPatterns.sendTaskClientFeedbackMessageEmail(sHead, sO(sBodyClerk), sMailClerk, sID_Order, sHost);
             }
             if (nID_SubjectMessageType == 9L) { //officer's comment or question
@@ -875,7 +876,7 @@ public class SubjectMessageController {
             HistoryEvent_Service oHistoryEvent_Service = historyEventServiceDao.getOrgerByID(sID_Order);
             if (oHistoryEvent_Service != null) {
                 if (oHistoryEvent_Service.getsToken() != null && oHistoryEvent_Service.getsToken().equals(sToken)) {
-                    
+
                     SubjectMessage oSubjectMessage_Feedback = oSubjectMessageService.createSubjectMessage(
                             sMessageHead(nID_SubjectMessageType, sID_Order), "", oHistoryEvent_Service.getnID_Subject(),
                             "", "", "", nID_SubjectMessageType);//2l
@@ -884,7 +885,7 @@ public class SubjectMessageController {
                     LOG.info("No SubjectMessage records found, create new!");
                     oHistoryEvent_Service.setsToken("");
                     historyEventServiceDao.saveOrUpdate(oHistoryEvent_Service);
-                    
+
                 } else {
                     LOG.warn("Skipping history event service from processing as it contains wrong token: {}", oHistoryEvent_Service.getsToken());
                     throw new CommonServiceException(
