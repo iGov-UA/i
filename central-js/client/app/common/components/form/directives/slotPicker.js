1fcffc8699682bda34b16304b01a57ad2c2979f8
joulis1derful <joulis1derful@gmail.com>
2018-03-18 23:47:50 +0200

    SRVCSGOV-83 changed request and added error catcher

--- central-js/client/app/common/components/form/directives/slotPicker.js
+++ central-js/client/app/common/components/form/directives/slotPicker.js
@@ -195,28 +195,29 @@ angular.module('app').directive('slotPicker', function($http, dialogs, ErrorsFac
           }
         } else if (isQueueDataType.QLogic){
           if(newValue){
-            // var baseURL = '/api/service/flow/Qlogic/setSlot';
-            // var setFlowUrl = baseURL + newValue.nID + '?nID_Server=' + scope.serviceData.nID_Server;
-            // if (nSlotsParam) {
-            //   var nSlots = parseInt(nSlotsParam.value) || 0;
-            //   if (nSlots > 1)
-            //     setFlowUrl += '&nSlots=' + nSlots;
-            // }
+            var URL = 'api/service/flow/Qlogic/setSlot';
 
-            // $http.post(setFlowUrl).then(function (response) {
-              scope.ngModel = JSON.stringify({
-                sOrganizatonGuid: scope.formData.params[sOrganizatonGuid_ID].value,
-                sServiceCenterId: scope.formData.params[sServiceCenterId_ID].value,
-                sServiceId: scope.formData.params[sServiceId_ID].value,
-                sPhone: scope.formData.params.phone.value || '',
-                sEmail: scope.formData.params.email.value || '',
-                sName: scope.formData.params.bankIdlastName.value + ' ' + scope.formData.params.bankIdfirstName.value + (scope.formData.params.bankIdmiddleName.value ? ' ' + scope.formData.params.bankIdmiddleName.value : ''),
-                sDate: scope.selected.date.sDate,
-                sTime: scope.selected.slot.sTime
+            scope.ngModel = JSON.stringify({
+              sOrganizatonGuid: scope.formData.params[sOrganizatonGuid_ID].value,
+              sServiceCenterId: scope.formData.params[sServiceCenterId_ID].value,
+              sServiceId: scope.formData.params[sServiceId_ID].value,
+              sPhone: scope.formData.params.phone.value || '',
+              sEmail: scope.formData.params.email.value || '',
+              sName: scope.formData.params.bankIdlastName.value + ' ' + scope.formData.params.bankIdfirstName.value + (scope.formData.params.bankIdmiddleName.value ? ' ' + scope.formData.params.bankIdmiddleName.value : ''),
+              sDate: scope.selected.date.sDate,
+              sTime: scope.selected.slot.sTime
+            });
+
+            $http({
+              method: 'POST',
+              url: URL,
+              data: scope.ngModel
+            })
+              .then(function (response) {
+                console.log('ok');
+              }, function (error) {
+                dialogs.error('Помилка', 'Неможливо вибрати час. Спробуйте обрати інший або пізніше, будь ласка');
               });
-            // }, function () {
-            //   dialogs.error('Помилка', 'Неможливо вибрати час. Спробуйте обрати інший або пізніше, будь ласка');
-            // });
           }
 
         }
