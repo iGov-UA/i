3dec2b8b6b7db23860cb568ba35622c11de22b2f
dolinets <dolinets123@gmail.com>
2018-08-22 15:33:51 +0300

    https://idoc-develop.atlassian.net/browse/SRVCSGOV-105

--- central-js/server/api/mobileid/mobileid.controller.js
+++ central-js/server/api/mobileid/mobileid.controller.js
@@ -2,44 +2,43 @@ var request = require('request')
   , config = require('../../config/environment')
 
 module.exports.mobileid= function(req, res) {
-    res.send(config.mobileid.login + config.mobileid.password + config.mobileid.IP);
-    res.end();
 
-    // var callback = function(error, response, body) {
-    //     console.log(resultServices);
-    //     res.send(body);
-    //     res.end();
-    // };
 
-    // var apInstant = new Date();
+    var callback = function(error, response, body) {
+        console.log(resultServices);
+        res.send(body);
+        res.end();
+    };
 
-    // var resultServices = request.post({
+    var apInstant = new Date();
 
-    //     'url': config.mobileid.IP + "/MSSP/restapi/services/service_ds/formats/PKCS7/signTextTransaction",
+    var resultServices = request.post({
+
+        'url': config.mobileid.IP + "/MSSP/restapi/services/service_ds/formats/PKCS7/signTextTransaction",
+        'auth': {
+            'username': config.mobileid.login,
+            'password': config.mobileid.password
+        },
+        'qs': {
+            //"msisdn": req.body.msisdn,
+            "msisdn": "+380672340261",
+            "dtbs":"Kari test",
+            "apTransactionID": "_222228888888888888888",
+            "apInstant": apInstant
+        }
+
+    }, callback);
+    // var resultRestapi = request.get({
+
+    //     'url': "https://81.23.16.246:8080//MSSP/restapi/status/222228888888888888888",
     //     'auth': {
-    //         'username': config.mobileid.login,
-    //         'password': config.mobileid.password
-    //     },
-    //     'qs': {
-    //         //"msisdn": req.body.msisdn,
-    //         "msisdn": "+380672340261",
-    //         "dtbs":"Kari test",
-    //         "apTransactionID": "_222228888888888888888",
-    //         "apInstant": apInstant
-    //     }
+    //         'username': "kyivstar",
+    //         'password': "kyivstar"
+    //       }
 
     // }, callback);
-    // // var resultRestapi = request.get({
 
-    // //     'url': "https://81.23.16.246:8080//MSSP/restapi/status/222228888888888888888",
-    // //     'auth': {
-    // //         'username': "kyivstar",
-    // //         'password': "kyivstar"
-    // //       }
-
-    // // }, callback);
-
-    // return resultServices; 
+    return resultServices; 
     
     
     
