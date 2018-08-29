14cb23b96c9f4b0804c9ce4f2a9268bdc085d9f6
dolinets <dolinets123@gmail.com>
2018-08-29 18:06:09 +0300

    https://idoc-develop.atlassian.net/browse/SRVCSGOV-105

--- central-js/server/api/mobileid/mobileid.controller.js
+++ central-js/server/api/mobileid/mobileid.controller.js
@@ -7,34 +7,34 @@ module.exports.mobileid= function(req, res) {
     var callback = function(error, response, body) {
         console.log('mobileid', error, response, body); 
         console.log('mobileid', 'resultServices', resultServices.body); 
-        
-
+        body.transactionID 
+ 
         res.send(body);
         res.end();
     };
   
-    var apInstant = new Date();
-    var apTransactionID ="trxID_" + Date.now();
+    // var apInstant = new Date();
+    // var apTransactionID ="trxID_" + Date.now();
 
-    var resultServices = request.post({
+    // var resultServices = request.post({
 
-        'url': config.mobileid.IP + "/MSSP/restapi/services/service_ds/formats/PKCS7/signTextTransaction",
-        'headers': {
-            'Content-Type': 'application/json; charset=utf-8'
-        }, 
-        'auth': {
-            'username': config.mobileid.login,
-            'password': config.mobileid.password
-        },
-        'json': true,
-        'body': {
-            "msisdn":"+380672340261",
-            "dtbd":"Test message",
-            "dtbs":apTransactionID,
-            "apTransactionID": apTransactionID,
-            "apInstant": apInstant
-        }
-    }, callback);
+    //     'url': config.mobileid.IP + "/MSSP/restapi/services/service_ds/formats/PKCS7/signTextTransaction",
+    //     'headers': {
+    //         'Content-Type': 'application/json; charset=utf-8'
+    //     }, 
+    //     'auth': {
+    //         'username': config.mobileid.login,
+    //         'password': config.mobileid.password
+    //     },
+    //     'json': true,
+    //     'body': {
+    //         "msisdn":"+380672340261",
+    //         "dtbd":"Test message",
+    //         "dtbs":apTransactionID,
+    //         "apTransactionID": apTransactionID,
+    //         "apInstant": apInstant
+    //     }
+    // }, callback);
 
     var pingCount = 0;
     var maxPings = 5;
@@ -52,7 +52,7 @@ module.exports.mobileid= function(req, res) {
         } else {
             pingCount++;
             request.get({
-                'url': config.mobileid.IP + "/MSSP/restapi/status/26459",
+                'url': config.mobileid.IP + "/MSSP/restapi/status/26478",
                 'headers': {
                     'Content-Type': 'application/json; charset=utf-8'
                 }, 
