5c82a4bf37aaaab21d3c5a15cc9abad201cff9bf
dolinets <dolinets123@gmail.com>
2018-08-30 15:09:50 +0300

    https://idoc-develop.atlassian.net/browse/SRVCSGOV-105

--- central-js/server/api/mobileid/mobileid.controller.js
+++ central-js/server/api/mobileid/mobileid.controller.js
@@ -3,67 +3,87 @@ var request = require('request')
 
 module.exports.mobileid= function(req, res) {
 
+var transactionID;
+var pingCount = 1;
 
     var callback = function(error, response, body) {
-        console.log('mobileid', error, response, body); 
-        console.log('mobileid', 'resultServices', resultServices.body); 
-        body.transactionID 
- 
-        res.send(body);
-        res.end();
+        console.log(body);  
+        if (body == null) {
+            res.send(body);   
+            res.end();   
+        }
+        else if (body.statusCode == "504" && pingCount < 100) {
+            transactionID = body.transactionID;
+            resultRestapi (transactionID, callback);
+        } else {
+            res.send(body);   
+            res.end();       
+        }        
+
     };
   
-    // var apInstant = new Date();
-    // var apTransactionID ="trxID_" + Date.now();
+    var apInstant = new Date();
+    var apTransactionID ="trxID_" + Date.now();
 
-    // var resultServices = request.post({
+    var resultServices = request.post({
 
-    //     'url': config.mobileid.IP + "/MSSP/restapi/services/service_ds/formats/PKCS7/signTextTransaction",
-    //     'headers': {
-    //         'Content-Type': 'application/json; charset=utf-8'
-    //     }, 
-    //     'auth': {
-    //         'username': config.mobileid.login,
-    //         'password': config.mobileid.password
-    //     },
-    //     'json': true,
-    //     'body': {
-    //         "msisdn":"+380672340261",
-    //         "dtbd":"Test message",
-    //         "dtbs":apTransactionID,
-    //         "apTransactionID": apTransactionID,
-    //         "apInstant": apInstant
-    //     }
-    // }, callback);
+        'url': config.mobileid.IP + "/MSSP/restapi/services/service_ds/formats/PKCS7/signTextTransaction",
+        //'url': "https://httpstat.us/200",
+        'headers': {
+            'Content-Type': 'application/json; charset=utf-8'
+        }, 
+        'auth': {
+            'username': config.mobileid.login,
+            'password': config.mobileid.password
+        },
+        'json': true,
+        'body': {
+            "msisdn":"+380672340261",
+            "dtbd":"Test message",
+            "dtbs":apTransactionID,
+            "apTransactionID": apTransactionID,
+            "apInstant": apInstant
+        }
+    }, callback);
 
-    var pingCount = 0;
-    var maxPings = 5;
+
+
+  function resultRestapi () {
+
+    // var pingCount = 1;
+    // var maxPings = 3;
+
+    // console.log ("ид транзакции", transactionID); 
+    // var pingMobileId = setInterval(pingResultRestapi, 3000);
+
   
-    var pingMobileId = setInterval(resultRestapi, 30000);
+    // function pingResultRestapi (){
 
-  
-    function resultRestapi (){
-
-        if (pingCount > maxPings) {
-            clearInterval(pingMobileId);
-            console.log ('mobileid', 'пинг закончен');
-            console.log ('mobileid', resultRestapi.statusCode);
-            return resultRestapi;  
-        } else {
+        // if (pingCount > maxPings) {
+        //     clearInterval(pingMobileId);
+        //     console.log ("Пинг закончен");
+            
+            //return req;  
+        // } else {
             pingCount++;
             request.get({
-                'url': config.mobileid.IP + "/MSSP/restapi/status/26484",
+                'url': config.mobileid.IP + "/MSSP/restapi/status/" + transactionID,
+                //'url': "https://httpstat.us/202",
                 'headers': {
                     'Content-Type': 'application/json; charset=utf-8'
                 }, 
                 'auth': {
                     'username': config.mobileid.login,
                     'password': config.mobileid.password
-                }
+                },
+                'json': true
 
-        }, callback)
-    };
-}
+        }, callback) 
+    }; 
+// }
         
     
-};
+       
+//    };
+    
+}; 
