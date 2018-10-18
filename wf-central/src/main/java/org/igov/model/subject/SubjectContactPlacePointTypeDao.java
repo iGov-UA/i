119f820653bd15adb67283a20c466b0bc21532d7
Oleksandr Gurtovyi <gurtik7@gmail.com>
2018-10-18 09:22:54 +0300

    PLATFORM-2312 fix

--- wf-central/src/main/java/org/igov/model/subject/SubjectContactPlacePointTypeDao.java
+++ wf-central/src/main/java/org/igov/model/subject/SubjectContactPlacePointTypeDao.java
@@ -3,5 +3,4 @@ package org.igov.model.subject;
 import org.igov.model.core.EntityDao;
 
 public interface SubjectContactPlacePointTypeDao extends EntityDao<Long, SubjectContactPlacePointType> {
-    public SubjectContactPlacePointType findByNameEN(String sName_EN);
 }
--- wf-central/src/main/java/org/igov/model/subject/SubjectContactPlacePointTypeDaoImpl.java
+++ wf-central/src/main/java/org/igov/model/subject/SubjectContactPlacePointTypeDaoImpl.java
@@ -11,12 +11,4 @@ public class SubjectContactPlacePointTypeDaoImpl extends GenericEntityDao<Long, 
         super(SubjectContactPlacePointType.class);
     }
 
-    @Override
-    public SubjectContactPlacePointType findByNameEN(String sName_EN) {
-        Criteria criteria = getSession().createCriteria(SubjectContactPlacePointType.class);
-        if (sName_EN != null) {
-            criteria.add(Restrictions.eq("sName_EN", sName_EN)).setMaxResults(1).uniqueResult();
-        }
-        return (SubjectContactPlacePointType) criteria;
-    }
 }
