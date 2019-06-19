e32b11b4f82b1cbebc6b6ac20fba3b0522499806
Mykola <mykolabaraniuk@gmail.com>
2019-06-19 12:38:09 +0300

    PLATFORM-3565 minor link fix

--- central-js/client/app/common/components/form/directives/extend-a-tag.js
+++ central-js/client/app/common/components/form/directives/extend-a-tag.js
@@ -5,17 +5,17 @@ angular.module('linkdir', []).directive('a', [function() {
 		link: function link(scope, elem, attrs, ngModel) {
 			var element = elem[0];
 			var sCurrHost = location.hostname;
-			var sLinkHref = element.href;
-			var sLinkHost = element.hostname;
 
-			if (sCurrHost !== sLinkHost && sLinkHref.indexOf('igov') > -1) {
-                element.href = '/api/service/setAuthForURL?sURL=' + encodeURIComponent(sLinkHref);
-                
-                element.addEventListener('click', function(event) {
+			var sLinkHref = attrs.ngHref ? attrs.ngHref : (attrs.href ? attrs.href : element.href);
+
+			if (sLinkHref.indexOf(sCurrHost) === -1  && sLinkHref.indexOf('igov') > -1) {
+				element.href = '/api/service/setAuthForURL?sURL=' + encodeURIComponent(sLinkHref);
+
+				element.addEventListener('click', function(event) {
 					event.preventDefault();
 					location = element.href;
 				});
 			}
 		}
 	};
-}]); 
+}]);
