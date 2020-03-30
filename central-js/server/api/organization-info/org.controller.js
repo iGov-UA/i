92a631a6ba41b4913808f97ceb290a9b4aa85dc2
gorob4ik <danilgorobec4@gmail.com>
2020-03-30 10:52:36 +0300

    GOVREG-459

--- central-js/server/api/organization-info/org.controller.js
+++ central-js/server/api/organization-info/org.controller.js
@@ -3,7 +3,7 @@ var request = require('request'),
     config = require('../../config/environment');
 
 module.exports.getOrganization = function (req, res) {
-  var downloadURL = 'https://opendatabot.com/api/v1/company/' + req.query.code;
+  var downloadURL = 'https://opendatabot.com/api/v2/company/' + req.query.code;
   var callback = function (error, response, body) {
     res.send(body);
     res.end();
